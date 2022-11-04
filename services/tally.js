598129831243a68a80ee88497c10b9428df065ec
Paige Andrews <paige.andrews@modolabs.com>
2022-11-04 15:27:41 -0400

    Add fate tally logic

--- services/tally.js
+++ services/tally.js
@@ -3,5 +3,9 @@
 module.exports = {
   sum(resultSets) {
     return resultSets.map((set) => set.reduce((a, b) => a+b, 0))
+  },
+
+  fate(resultSets) {
+    return resultSets.map((set) => set.reduce((a, b) => a + b - 2, 0))
   }
 }
--- services/tally.test.js
+++ services/tally.test.js
@@ -18,3 +18,40 @@ describe("sum", () => {
     expect(sums).toEqual(expectedSums)
   })
 })
+
+describe("fate", () => {
+  it("generates one number per array of the result set", () => {
+    const resultSets = [[1,1,2,2], [2,1,3,2], [1,3,1,3]]
+
+    const fateSums = tallyService.fate(resultSets)
+
+    expect(fateSums.length).toEqual(3)
+  })
+
+  it("subtracts 1 for every 1 result", () => {
+    const resultSets = [[1,2,2,2]]
+    const expectedSums = [-1]
+
+    const fateSums = tallyService.fate(resultSets)
+
+    expect(fateSums).toEqual(expectedSums)
+  })
+
+  it("ignores every 2 result", () => {
+    const resultSets = [[2,2,2,2]]
+    const expectedSums = [0]
+
+    const fateSums = tallyService.fate(resultSets)
+
+    expect(fateSums).toEqual(expectedSums)
+  })
+
+  it("adds one for every 3 result", () => {
+    const resultSets = [[3,2,2,2]]
+    const expectedSums = [1]
+
+    const fateSums = tallyService.fate(resultSets)
+
+    expect(fateSums).toEqual(expectedSums)
+  })
+})
