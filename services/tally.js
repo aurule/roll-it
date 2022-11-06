703b5cba2e5225643e08d80b17ec1a3f4d010b46
Paige Andrews <paige.andrews@modolabs.com>
2022-11-05 21:33:50 -0400

    Document other tally methods

--- services/tally.js
+++ services/tally.js
@@ -1,10 +1,27 @@
 "use strict"
 
 module.exports = {
+  /**
+   * Add up the dice of result sets
+   *
+   * @param  {Array<Array<Int>>} resultSets Nested array representing one or more sets of dice rolls
+   * @return {Array<Int>}                   Array of sums from each result set
+   */
   sum(resultSets) {
     return resultSets.map((set) => set.reduce((prev, curr) => prev + curr, 0))
   },
 
+  /**
+   * Add up d3 rolls as fudge dice
+   *
+   * Each int must be from 1 to 3. The rules are:
+   * - Any die that equals 1 subtracts one from the total
+   * - Any die that equals 2 is ignored
+   * - Any die that equals 3 adds one to the total
+   *
+   * @param  {Array<Array<Int>>} resultSets Nested array representing one or more sets of dice rolls
+   * @return {Array<Int>}                   Array of sums from each result set
+   */
   fate(resultSets) {
     return resultSets.map((set) =>
       set.reduce((prev, curr) => prev + curr - 2, 0)
@@ -20,17 +37,17 @@ module.exports = {
   /**
    * Count successes using World of Darkness 20th Anniversary rules
    *
-   * The rules are:
+   * Each int must be from 1 to 10. The rules are:
    * - Any die that meets or exceeds threshold adds one success
    * - Any die that rolls a 1 subtracts one success
    * - If `double` is true, then any die that equals a 10 adds an additional success
    * - If the final tally is negative, but one or more successes occurred, then the tally becomes zero
    * - If the final tally is negative, and no successes ocurred at all, then the negative tally stands
    *
-   * @param  {Array<Array<Int>>}  resultSets  Array of arrays of ints representing die roll results
-   * @param  {Int}                threshold   Number a die must meet or exceed to add one success
-   * @param  {Boolean}            double      Whether to add a second success when a die is 10
-   * @return {Array<Int>}                     Array of ints representing the success tallies of each resultSet
+   * @param  {Array<Array<Int>>} resultSets Nested array representing one or more sets of dice rolls
+   * @param  {Int}               threshold  Number a die must meet or exceed to add one success
+   * @param  {Boolean}           double     Whether to add a second success when a die is 10
+   * @return {Array<Int>}                   Array of ints representing the success tallies of each resultSet
    */
   wod20(resultSets, threshold, double = false) {
     return resultSets.map((set) => {
