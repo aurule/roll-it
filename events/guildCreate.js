57f8ae2f291e51a32e122533488e95caccc938b2
Paige Andrews <pmandrews@gmail.com>
2023-12-05 23:13:52 -0500

    Push guild commands on guild add

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,11 +1,16 @@
+const commandService = require("../services/command-deploy");
+
 module.exports = {
   name: "guildCreate",
   async execute(discord_guild) {
+    const guildFlake = discord_guild.id
     if (
       (process.env.NODE_ENV != "development") ==
-      process.env.DEV_GUILDS.includes(discord_guild.id)
+      process.env.DEV_GUILDS.includes(guildFlake)
     ) {
       return
     }
+
+    await commandService.deployGuild(guildFlake)
   },
 }
