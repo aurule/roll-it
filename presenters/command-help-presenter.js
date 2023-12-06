7eb0420319abd7ca7d07708ffc9bea7829966aed
Paige Andrews <pmandrews@gmail.com>
2023-12-05 23:33:41 -0500

    Properly format help for commands without args

--- presenters/command-help-presenter.js
+++ presenters/command-help-presenter.js
@@ -10,23 +10,26 @@ module.exports = {
    */
   present: (command) => {
     const command_data = command.data()
+    const command_options = command_data.options
     const command_name = CommandNamePresenter.present(command)
 
     let lines = [
       `Showing help for ${command_name}: ${command.description}`,
-      "",
-      "Args:",
     ]
 
-    lines = lines.concat(
-      command_data.options.map((opt) => {
-        let opt_lines = [`\t${inlineCode(opt.name)}:`]
-        if (opt.required) opt_lines.push("(required)")
-        opt_lines.push(opt.description)
+    if(command_options.length) {
+      lines.push("")
+      lines.push("Args:")
+      lines = lines.concat(
+        command_data.options.map((opt) => {
+          let opt_lines = [`\t${inlineCode(opt.name)}:`]
+          if (opt.required) opt_lines.push("(required)")
+          opt_lines.push(opt.description)
 
-        return opt_lines.join(" ")
-      })
-    )
+          return opt_lines.join(" ")
+        })
+      )
+    }
 
     lines.push("")
     lines.push(command.help({ command_name }))
--- presenters/command-help-presenter.test.js
+++ presenters/command-help-presenter.test.js
@@ -20,6 +20,16 @@ const test_command = {
   help: ({ command_name }) => "test help output",
 }
 
+const test_command_bare = {
+  name: "test-command",
+  description: "A fake command for testing",
+  data: () =>
+    new SlashCommandBuilder()
+      .setName("test-command-bare")
+      .setDescription("A fake command for testing without options"),
+  help: ({ command_name }) => "test bare help output",
+}
+
 describe("present", () => {
   it("names the command in the output", () => {
     const result = CommandHelpPresenter.present(test_command)
@@ -28,22 +38,32 @@ describe("present", () => {
     expect(result).toMatch(test_command.name)
   })
 
-  it("shows the options", () => {
-    const result = CommandHelpPresenter.present(test_command)
-
-    expect(result).toMatch("Title description")
-    expect(result).toMatch("Subtitle description")
-  })
-
-  it("marks required options", () => {
-    const result = CommandHelpPresenter.present(test_command)
-
-    expect(result).toMatch("(required) Title description")
-  })
-
   it("shows the help output for the command", () => {
     const result = CommandHelpPresenter.present(test_command)
 
     expect(result).toMatch(test_command.help({ command_name: "test-command" }))
+  })
+
+  describe("with options", () => {
+    it("shows the options", () => {
+      const result = CommandHelpPresenter.present(test_command)
+
+      expect(result).toMatch("Title description")
+      expect(result).toMatch("Subtitle description")
+    })
+
+    it("marks required options", () => {
+      const result = CommandHelpPresenter.present(test_command)
+
+      expect(result).toMatch("(required) Title description")
+    })
+  })
+
+  describe("without options", () => {
+    it("skips the Args section", () => {
+      const result = CommandHelpPresenter.present(test_command_bare)
+
+      expect(result).not.toMatch("Args")
+    })
   })
 })
