a075a6fb6b0efed389943f7b975b8b796e37fff3
Paige Andrews <paige.andrews@modolabs.com>
2024-05-29 16:30:35 -0400

    Switch to standard sha1 for fast options hash

--- services/command-deploy.js
+++ services/command-deploy.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 
 const { logger } = require("../util/logger")
 const { REST, Routes } = require("discord.js")
+const crypto = require('crypto')
 
 const commandFetch = require("./command-fetch")
 
@@ -45,22 +46,10 @@ function buildGuildCommandJSON(command_names = null) {
  * @return {string} Hash of the JSON data about global slash commands
  */
 function hashGlobalCommandJSON() {
-  // Quick hash function courtesy of https://stackoverflow.com/users/815680/bryc found
-  // at https://stackoverflow.com/a/52171480
-  const cyrb53 = function(str, seed = 0) {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-      ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 2654435761);
-      h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return (h2>>>0).toString(16).padStart(8,0)+(h1>>>0).toString(16).padStart(8,0);
-  };
+  const hasher = crypto.createHash('sha1')
 
   // use fn from module.exports so we can mock it out in tests
-  return cyrb53(JSON.stringify(module.exports.buildGlobalCommandJSON()))
+  return hasher.update(JSON.stringify(module.exports.buildGlobalCommandJSON())).digest('hex')
 }
 
 /**
