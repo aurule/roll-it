57fc48264068d7196e65a51bd4195c0bd9992c0b
Paige Andrews <paige.andrews@modolabs.com>
2024-05-31 15:11:37 -0400

    Add default field to command options

--- transformers/command-select-transformer.js
+++ transformers/command-select-transformer.js
@@ -3,14 +3,16 @@ module.exports = {
    * Transforms an array of command objects into an array of select menu option
    * objects suitable for the options of a Discord String Select Menu.
    * @param  {Array}        commands  The commands to transform
+   * @param  {Array}        deployed  Command names which should be set as default selections (optional)
    * @return {Array[Object]}          Array of select choices
    */
-  transform: (commands) => {
+  transform: (commands, deployed = []) => {
     return commands.map((command) => {
       return {
         label: `${command.name}`,
         description: `${command.description}`,
         value: `${command.name}`,
+        default: deployed.includes(command.name),
       }
     })
   },
--- transformers/command-select-transformer.test.js
+++ transformers/command-select-transformer.test.js
@@ -19,7 +19,27 @@ it("creates an array of suitable objects", () => {
   const data = transform(commands)
 
   expect(data).toEqual([
-    {label: "test 1", description: "The first test", value: "test 1"},
-    {label: "test 2", description: "The second test", value: "test 2"}
+    {label: "test 1", description: "The first test", value: "test 1", default: false},
+    {label: "test 2", description: "The second test", value: "test 2", default: false}
   ])
+})
+
+it("sets default property based on deployed arg", () => {
+  const commands = [
+    {
+      name: "test 1",
+      description: "The first test",
+      id: 1,
+    },
+    {
+      name: "test 2",
+      description: "The second test",
+      id: 2,
+    },
+  ]
+  const deployed = ["test 1"]
+
+  const data = transform(commands, deployed)
+
+  expect(data[0].default).toBeTruthy()
 })
