8e179f2ab7df3da350d099382cda1391455ab76c
Paige Andrews <paige.andrews@modolabs.com>
2024-06-06 14:01:12 -0400

    Add advantage handling to d20 presenter

---
+++ presenters/d20-results-presenter.js
@@ -0,0 +1,107 @@
+const { bold, strikethrough, userMention } = require("discord.js")
+
+/**
+ * Create a string describing the results of a rock-paper-scissors roll
+ *
+ * @param  {Int}       options.modifier     Number to add to the roll's summed result
+ * @param  {String}    options.description  Text describing the roll
+ * @param  {Array<Array<Int>>} options.raw  An array of one array with one numeric value for the die
+ * @param  {obj}       options.picked       Object of results and indexes after picking highest or lowest
+ * @param  {Snowflake} options.userFlake    Snowflake ID of the user who made the roll
+ * @return {String}                         String describing this roll
+ */
+function presentOne({ modifier, description, raw, picked, userFlake }) {
+  let content = [
+    userMention(userFlake),
+    "rolled",
+    detail(raw[0], picked[0].indexes, modifier)
+  ]
+  // with dis/advantage
+  if (description) content.push(`for "${description}"`)
+  return content.join(" ")
+}
+
+
+/**
+ * Create a string describing the results of many rock-paper-scissors rolls
+ *
+ * @param  {Int}       options.modifier     Number to add to the roll's summed result
+ * @param  {String}    options.description  Text describing the roll
+ * @param  {Array<Array<Int>>} options.raw  An array of one array with one numeric value for the die
+ * @param  {obj[]}     options.picked       Array of objects of results and indexes after picking highest or lowest
+ * @param  {Snowflake} options.userFlake    Snowflake ID of the user who made the roll
+ * @return {String}                         String describing this roll
+ */
+function presentMany({ modifier, description, raw, picked, userFlake }) {
+  let content = [
+    userMention(userFlake),
+    "rolled",
+  ]
+  if (description) {
+    content.push(`"${description}"`)
+  }
+  // with dis/advantage
+  content.push(raw.length)
+  content.push("times:")
+  return content
+    .concat(
+      raw.map((result, idx) => `\n\t${detail(result, picked[idx].indexes, modifier)}`)
+    )
+    .join(" ")
+}
+
+/**
+ * Describe a single roll result
+ *
+ * @param  {Int[]} result   Array of raw die numbers
+ * @param  {int[]} indexes  Array of indexes kept after rolling
+ * @param  {Int} modifier Number to add to the raw die
+ * @return {string}       Description of the result and modifier
+ */
+function detail(result, indexes, modifier) {
+  const die = result[indexes[0]]
+  const content = [bold(die + modifier)]
+
+  let selection = `${die}`
+  if (result.length > 1) {
+    const nums = result.map((res, idx) => {
+      if (indexes.includes(idx)) {
+        return `${res}`
+      } else {
+        return strikethrough(res)
+      }
+    }).join(", ")
+    selection = `[${nums}]`
+  }
+
+  let breakdown = ""
+  if(modifier) {
+    breakdown = `(${selection} + ${modifier})`
+    content.push(breakdown)
+  } else if (result.length > 1) {
+    content.push(selection)
+  }
+  return content.join(" ")
+}
+
+module.exports = {
+  /**
+   * Present one or more results from a singleton roll command
+   *
+   * This is meant to present the output for commands like d20 and d100, where there is only ever a single die
+   * result.
+   *
+   * @param  {Int}        options.rolls       Total number of rolls to show
+   * @param  {...[Array]} options.rollOptions The rest of the options, passed to presentOne or presentMany
+   * @return {String}                         String describing the roll results
+   */
+  present: ({ rolls, ...rollOptions }) => {
+    if (rolls == 1) {
+      return presentOne(rollOptions)
+    }
+    return presentMany(rollOptions)
+  },
+  presentOne,
+  presentMany,
+  detail
+}
---
+++ presenters/d20-results-presenter.test.js
@@ -0,0 +1,115 @@
+const d20Presenter = require("./d20-results-presenter")
+
+const { simpleflake } = require("simpleflakes")
+
+describe("presentOne", () => {
+  const defaultArgs = {
+    modifier: 0,
+    description: "test roll",
+    raw: [[1]],
+    picked: [{indexes: [0]}],
+    userFlake: simpleflake(),
+  }
+
+  it("mentions the user", () => {
+    const result = d20Presenter.presentOne(defaultArgs)
+
+    expect(result).toMatch(defaultArgs.userFlake.toString())
+  })
+
+  it("includes description if present", () => {
+    const result = d20Presenter.presentOne(defaultArgs)
+
+    expect(result).toMatch(`"${defaultArgs.description}"`)
+  })
+})
+
+describe("presentMany", () => {
+  const defaultArgs = {
+    modifier: 0,
+    description: "test roll",
+    raw: [[1], [2]],
+    picked: [{indexes: [0]}, {indexes: [0]}],
+    userFlake: simpleflake(),
+  }
+
+  it("mentions the user", () => {
+    const result = d20Presenter.presentMany(defaultArgs)
+
+    expect(result).toMatch(defaultArgs.userFlake.toString())
+  })
+
+  it("includes description if present", () => {
+    const result = d20Presenter.presentMany(defaultArgs)
+
+    expect(result).toMatch(`"${defaultArgs.description}"`)
+  })
+})
+
+describe("detail", () => {
+  describe("single die", () => {
+    describe("when modifier is zero", () => {
+      it("bolds the final result", () => {
+        const result = d20Presenter.detail([5], [0], 0)
+
+        expect(result).toMatch("**5**")
+      })
+
+      it("has no breakdown", () => {
+        const result = d20Presenter.detail([5], [0], 0)
+
+        expect(result).not.toMatch("(")
+      })
+    })
+
+    describe("when modifier is non-zero", () => {
+      it("bolds the final result", () => {
+        const result = d20Presenter.detail([5], [0], 3)
+
+        expect(result).toMatch("**8**")
+      })
+
+      it("includes breakdown", () => {
+        const result = d20Presenter.detail([5], [0], 3)
+
+        expect(result).toMatch("(")
+      })
+    })
+  })
+
+  describe("two dice", () => {
+    describe("when modifier is zero", () => {
+      it("bolds the final result", () => {
+        const result = d20Presenter.detail([5, 18], [1], 0)
+
+        expect(result).toMatch("**18**")
+      })
+
+      it("shows the rejected die", () => {
+        const result = d20Presenter.detail([5, 18], [1], 0)
+
+        expect(result).toMatch("~~5~~")
+      })
+    })
+
+    describe("when modifier is non-zero", () => {
+      it("bolds the final result", () => {
+        const result = d20Presenter.detail([5, 18], [1], 5)
+
+        expect(result).toMatch("**23**")
+      })
+
+      it("shows the rejected die", () => {
+        const result = d20Presenter.detail([5, 18], [1], 5)
+
+        expect(result).toMatch("~~5~~")
+      })
+
+      it("shows a breakdown with modifier", () => {
+        const result = d20Presenter.detail([5, 18], [1], 5)
+
+        expect(result).toMatch("+ 5")
+      })
+    })
+  })
+})
