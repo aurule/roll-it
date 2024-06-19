9f753f186bd1a37f524a5037f70c653bfa0cf4d7
Peter Andrews <pmandrews@gmail.com>
2024-06-19 15:32:20 -0400

    Use different databases based on env

--- db/index.js
+++ db/index.js
@@ -2,8 +2,23 @@ const { logger } = require("../util/logger")
 
 const Database = require('better-sqlite3');
 
-const db = new Database('../roll-it.db', { verbose: logger.debug });
+require("dotenv").config()
+
+function pickDatabaseFile() {
+  switch(process.env.NODE_ENV) {
+    case "development":
+      return "../roll-it.dev.db"
+    case "test":
+    case "ci":
+      return ":memory:"
+    case "production":
+      return "../roll-it.prod.db"
+  }
+}
+
+const db = new Database(pickDatabaseFile(), { verbose: logger.debug });
 
 module.exports = {
-  db
+  db,
+  pickDatabaseFile
 }
