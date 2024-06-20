3167f2239fc301d693a5e50a168160b90daf3c23
Paige Andrews <paige.andrews@modolabs.com>
2024-06-20 17:07:47 -0400

    WIP sketch out rollable object

--- db/rollable.js
+++ db/rollable.js
@@ -1,15 +1,5 @@
 const { db } = require("./db")
 
-// api needs
-// * class that saves the guildId
-// * get rollable name and ID pairs for every table in a guild
-// * get count of rollables for a guild
-// guild class needs
-// * create(name, desc, contents array)
-// * get name and description pairs for all tables, maybe with counts
-// * get count for id
-// * get result for id and line number
-// * update name, description, or contents
 // schema
 // * guildId
 // * name
@@ -17,6 +7,44 @@ const { db } = require("./db")
 // * die (derived when contents changes)
 // * contents: json array
 
+class Rollables {
+  constructor(guildId) {
+    this.guildId = guildId
+  }
+
+  create(name, description, contents) {
+    // make a new table with the given data
+    // derive die from contents.length
+    // validate contents.length is > 1
+    // it's caller's job to make sure the name is unique, or to catch the database error that follows
+  }
+
+  list() {
+    // get name, description, and die for all tables
+  }
+
+  length(id) {
+    // get the die for the given ID
+  }
+
+  result(id, lineNo) {
+    // get the string in contents[lineNo]
+  }
+
+  update(id, name, description, contents) {
+    // alter the given data pieces
+    // update die to eq new contents.length
+  }
+
+  count() {
+    // return simple count of all rollables for the guild
+  }
+
+  options() {
+    // return name, description and id for every table
+  }
+}
+
 module.exports = {
-  //
+  Rollables
 }
