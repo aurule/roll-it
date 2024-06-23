863f311ee7721b93823efb5df0fca5740b6224b6
Paige Andrews <pmandrews@gmail.com>
2024-06-23 17:31:36 -0400

    Implement table name completer

---
+++ completers/table-completers.js
@@ -0,0 +1,29 @@
+/**
+ * Autocomplete functions used by the /table family of commands
+ *
+ * These are invoked by that command's `autcomplete` function. Their code lives
+ * here for easier testing.
+ */
+module.exports = {
+  /**
+   * Completer for looking up existing tables
+   *
+   * @param  {str}   partialText The user's typed text
+   * @param  {obj[]} tables      Array of table info objects, as returned by GuildRollables.all()
+   * @return {obj[]}             Array of choice objects
+   */
+  table(partialText, tables) {
+    const search = partialText.toLowerCase()
+
+    const matches = tables
+      .filter(t => t.name.toLowerCase().startsWith(search))
+      .map(t => {
+        return {
+          name: t.name.substring(0, 100),
+          value: t.id.toString()
+        }
+      })
+
+    return matches
+  },
+}
