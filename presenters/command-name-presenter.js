f3724bd1e7a0ee88f0c6cb84fd0d7e58bc5dd131
Paige Andrews <paige.andrews@modolabs.com>
2024-06-28 15:28:34 -0400

    Expand command name presenter to handle subcommands

--- presenters/command-name-presenter.js
+++ presenters/command-name-presenter.js
@@ -1,13 +1,51 @@
 const { inlineCode, italic } = require("discord.js")
 
+/**
+ * Return a formatted version of the command's name
+ *
+ * @param  {Command} command  The command object to present
+ * @return {String}           Markdown-formatted string of the command's name
+ */
+function present(command) {
+  return command.type == "menu" ? italic(command.name) : inlineCode(`/${command.name}`)
+}
+
+/**
+ * Return a formatted version of the subcommand's name
+ *
+ * @param  {Command}    parent Command object that is the entry point to the subcommand
+ * @param  {Subcommand} child  Subcommand object to present
+ * @return {String}            Markdown-formatted string of the subcommand's fully qualified name
+ */
+function presentSub(parent, child) {
+  return inlineCode(`/${parent.name} ${child.name}`)
+}
+
+/**
+ * Make a text list of all commands
+ *
+ * @param  {Collection} all_commands Optional collection of Command objects. Defaults to the `commands` module.
+ * @return {str}                     Markdown-formatted string of all commands and their subcommands
+ */
+function list(all_commands) {
+  const commands = all_commands ?? require("../commands")
+  return commands
+    .filter(c => c.type !== "menu")
+    .map(cmd => {
+      let content = `* ${present(cmd)} - ${cmd.description}`
+      if (cmd.subcommands) {
+        const subcontent = cmd.subcommands
+          .map((subc, _k) => `  - ${presentSub(cmd, subc)} - ${subc.description}`)
+          .join("\n")
+        content += `\n${subcontent}`
+      }
+      return content
+    })
+    .join("\n")
+}
+
 module.exports = {
-  /**
-   * Return a formatted version of the command's name
-   *
-   * @param  {Command} command  The command object to present
-   * @return {String}           Markdown-formatted string of the command's name
-   */
-  present: (command) => {
-    return command.type == "menu" ? italic(command.name) : inlineCode(`/${command.name}`)
-  },
+  present,
+  presentSub,
+  list,
 }
--- presenters/command-name-presenter.test.js
+++ presenters/command-name-presenter.test.js
@@ -1,8 +1,16 @@
+const { Collection } = require("discord.js")
+const commands = require("../commands")
 const CommandNamePresenter = require("./command-name-presenter")
+
+const test_subcommand = {
+  name: "sub",
+  description: "a test subcommand",
+}
 
 const test_command = {
   name: "test-command",
-  help: () => "test help output",
+  description: "a test command",
+  subcommands: new Collection([["sub", test_subcommand]])
 }
 
 describe("present", () => {
@@ -19,5 +27,43 @@ describe("present", () => {
     const result = CommandNamePresenter.present(test_command)
 
     expect(result).toEqual(`\`/${test_command.name}\``)
+  })
+})
+
+describe("presentSub", () => {
+  it("shows the parent name", () => {
+    const result = CommandNamePresenter.presentSub(test_command, test_subcommand)
+
+    expect(result).toMatch(test_command.name)
+  })
+
+  it("shows the child name", () => {
+    const result = CommandNamePresenter.presentSub(test_command, test_subcommand)
+
+    expect(result).toMatch(test_subcommand.name)
+  })
+
+  it("separates with a space", () => {
+    const result = CommandNamePresenter.presentSub(test_command, test_subcommand)
+
+    expect(result).toMatch(`${test_command.name} ${test_subcommand.name}`)
+  })
+})
+
+describe("list", () => {
+  it("shows commands", () => {
+    const commands = new Collection([["test-command", test_command]])
+
+    const result = CommandNamePresenter.list(commands)
+
+    expect(result).toMatch(test_command.name)
+  })
+
+  it("shows subcommands", () => {
+    const commands = new Collection([["test-command", test_command]])
+
+    const result = CommandNamePresenter.list(commands)
+
+    expect(result).toMatch(test_subcommand.name)
   })
 })
