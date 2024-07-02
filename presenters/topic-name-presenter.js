51764dc7158ce98efcb026c941c6bb84163ecb30
Paige Andrews <paige.andrews@modolabs.com>
2024-07-02 13:45:15 -0400

    Add presenter for help topics

---
+++ presenters/topic-name-presenter.js
@@ -0,0 +1,18 @@
+const { italic } = require("discord.js")
+
+/**
+ * Make a text list of all topics
+ *
+ * @param  {Collection} all_topics Optional collection of Topic objects. Defaults to the `help` module.
+ * @return {str}                   Markdown-formatted string of all topics
+ */
+function list(all_topics) {
+  const topics = all_topics ?? require("../help")
+  return topics
+    .map((t) => `* ${italic(t.title)} - ${t.description}`)
+    .join("\n")
+}
+
+module.exports = {
+  list,
+}
---
+++ presenters/topic-name-presenter.test.js
@@ -0,0 +1,26 @@
+const {Collection} = require("discord.js")
+const Presenter = require("./topic-name-presenter")
+
+const test_topic = {
+  name: "test",
+  title: "A Test Topic",
+  description: "A topic to test to"
+}
+
+describe("list", () => {
+  it("shows the topic names", () => {
+    const topics = new Collection([[test_topic.name, test_topic]])
+
+    const result = Presenter.list(topics)
+
+    expect(result).toMatch(test_topic.name)
+  })
+
+  it("shows the topic descriptions", () => {
+    const topics = new Collection([[test_topic.name, test_topic]])
+
+    const result = Presenter.list(topics)
+
+    expect(result).toMatch(test_topic.description)
+  })
+})
