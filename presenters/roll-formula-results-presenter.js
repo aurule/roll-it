45fd96384ebc2546697ffebd370a429f988143b5
Paige Andrews <paige.andrews@modolabs.com>
2024-07-11 14:10:05 -0400

    Make formula parsing safer

--- presenters/roll-formula-results-presenter.js
+++ presenters/roll-formula-results-presenter.js
@@ -1,6 +1,20 @@
 const { bold, userMention, inlineCode } = require("discord.js")
 const { evaluate } = require("mathjs")
 
+const { create, all } = require('mathjs')
+
+const math = create(all)
+const limitedEvaluate = math.evaluate
+
+math.import({
+  'import':     function () { throw new Error('Function import is disabled') },
+  'createUnit': function () { throw new Error('Function createUnit is disabled') },
+  'evaluate':   function () { throw new Error('Function evaluate is disabled') },
+  'parse':      function () { throw new Error('Function parse is disabled') },
+  'simplify':   function () { throw new Error('Function simplify is disabled') },
+  'derivative': function () { throw new Error('Function derivative is disabled') }
+}, { override: true })
+
 module.exports = {
   /**
    * Present a result from the formula roll command
@@ -15,7 +29,7 @@ module.exports = {
    * @return {String}                           String describing the roll results
    */
   present: ({ formula, rolledFormula, pools, raw, summed, description, userFlake }) => {
-    const finalSum = evaluate(rolledFormula)
+    const finalSum = limitedEvaluate(rolledFormula)
 
     let content = [userMention(userFlake), "rolled", bold(finalSum), "on"]
     if (description) content.push(`"${description}"`)
