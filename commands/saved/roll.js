3ad24f7fccacfa060d1e339b62b2e0d9263ce709
Paige Andrews <paige.andrews@modolabs.com>
2024-07-30 17:05:57 -0400

    Flesh out saved roller

--- commands/saved/roll.js
+++ commands/saved/roll.js
@@ -4,6 +4,19 @@ const presenter = require("../../presenters/saved-roll-presenter")
 const { longReply } = require("../../util/long-reply")
 const { UserSavedRolls } = require("../../db/saved_rolls")
 const commonOpts = require("../../util/common-options")
+const present_command = require("../../presenters/command-name-presenter").present
+const { injectMention } = require("../../util/inject-user")
+const { oneLine } = require("common-tags")
+
+function change_target(bonus, change, savable) {
+  if (!bonus) return undefined
+  if (change) return change
+  for (const opt of ["modifier", "pool"]) {
+    if (savable.includes(opt)) {
+      return opt
+    }
+  }
+}
 
 module.exports = {
   name: "roll",
@@ -15,51 +28,114 @@ module.exports = {
       .setDescription(module.exports.description)
       .addStringOption((option) =>
         option
-          .setName("roll")
+          .setName("name")
           .setDescription("Name of the saved roll to use")
           .setRequired(true)
           .setAutocomplete(true),
       )
-      .addStringOption(commonOpts.description) // maybe with a default?
-      // TODO
-      // optional pool
-      // optional modifier
+      .addStringOption((option) =>
+        option
+        .setName("description")
+        .setDescription("A word or two about this roll. Defaults to the saved description.") // or should it be the saved name?
+        .setMaxLength(1500)
+      )
+      .addIntegerOption((option) =>
+        option.setName("bonus").setDescription("A number to add or subtract from the roll"),
+      )
+      .addStringOption((option) =>
+        option.setName("change")
+          .setDescription("Choose where to apply the bonus. Default is based on the saved command.")
+          .setChoices(
+            { name: "Modifier", value: "modifier" },
+            { name: "Pool", value: "pool" },
+            { name: "Difficulty", value: "difficulty" },
+          )
+      )
       .addBooleanOption(commonOpts.secret),
+  change_target,
   async execute(interaction) {
-    // get the roll
-    // get its options
-    // look up the correct command object and send it to the perform() method
-    // inject and reply
-    //
-    // warn if invalid
-    // warn if incomplete
-    const tables = new GuildRollables(interaction.guildId)
+    const saved_rolls = new UserSavedRolls(interaction.guildId, interaction.user.id)
 
-    const rolls = interaction.options.getInteger("rolls") ?? 1
-    const roll_description = interaction.options.getString("description") ?? ""
-    const secret = interaction.options.getBoolean("secret") ?? false
-    const table_name = interaction.options.getString("table") ?? "0"
-    const table_id = parseInt(table_name)
+    const roll_name = interaction.options.getString("name") ?? ""
+    const roll_id = parseInt(roll_name)
 
-    const results = Array.from({ length: rolls }, (v) => tables.random(table_id, table_name))
-
-    if (results[0] === undefined) {
+    const roll_detail = saved_rolls.detail(roll_id, roll_name)
+    if(roll_detail === undefined) {
       return interaction.reply({
         content:
-          "That table does not exist. Check spelling, capitalization, or choose one of the suggested tables.",
+          "That roll does not exist. Check spelling, capitalization, or choose one of the suggested rolls.",
         ephemeral: true,
       })
     }
 
-    const detail = tables.detail(table_id, table_name)
+    if (roll_detail.invalid) {
+      return interaction.reply({
+        content: oneLine`
+          The saved options for that roll are not valid. You'll have to update them before you can use this
+          saved roll.
+        `,
+        ephemeral: true,
+      })
+    }
 
-    const full_text = present({
-      userFlake: interaction.user.id,
-      rolls,
-      tableName: detail.name,
-      results,
-      description: roll_description,
-    })
+    if (roll_detail.incomplete) {
+      return interaction.reply({
+        content: "This roll is not finished. You have to save some options before you can use it.",
+        ephemeral: true,
+      })
+    }
+
+    const description = interaction.options.getString("description") ?? roll_detail.description // or should this be the name?
+    roll_detail.options.description = description
+
+    const bonus = interaction.options.getInteger("bonus") ?? 0
+    const change = interaction.options.getString("change")
+    const secret = interaction.options.getBoolean("secret") ?? false
+
+    const savable_commands = require("../index").savable()
+    const command = savable_commands.get(roll_detail.command)
+    const target = change_target(bonus, change, command.savable)
+
+    if (target) {
+      if (!command.savable.includes(target)) {
+        return interaction.reply({
+          content: oneLine`
+            Cannot change option ${inlineCode(target)}, since it does not exist for
+            ${present_command(command)}.
+          `,
+          ephemeral: true,
+        })
+      }
+
+      const old_number = roll_detail.options[target] ?? 0
+      roll_detail.options[target] = old_number + bonus
+
+    }
+
+    try {
+      await command.schema.validateAsync(roll_detail.options)
+    } catch (err) {
+      if (target) {
+        return interaction.reply({
+          content: oneLine`
+            This roll can no longer be made after changing the ${target}. The error is:\n* ${err.details[0].message}
+          `,
+          ephemeral: true,
+        })
+      } else {
+        saved_rolls.update(roll_detail.id, {invalid: true})
+        return interaction.reply({
+          content: oneLine`
+            The saved options for this roll are no longer valid. You'll have to update them before you can use
+            this saved roll.
+          `,
+          ephemeral: true,
+        })
+      }
+    }
+
+    const partial_message = command.perform(roll_detail.options)
+    const full_text = injectMention(partial_message, interaction.user.id)
     return longReply(interaction, full_text, { separator: "\n\t", ephemeral: secret })
   },
   async autocomplete(interaction) {
@@ -68,7 +144,7 @@ module.exports = {
     const partialText = focusedOption.value
 
     switch (focusedOption.name) {
-      case "roll":
+      case "name":
         return Completers.saved_roll(partialText, saved_rolls.all())
     }
   },
--- commands/saved/roll.test.js
+++ commands/saved/roll.test.js
@@ -5,9 +5,189 @@ const { test_secret_option } = require("../../testing/shared/execute-secret")
 const saved_roll_command = require("./roll")
 
 describe("execute", () => {
-  it.todo("warns on missing roll")
-  it.todo("warns on invalid")
-  it.todo("warns on incomplete")
-  it.todo("executes the roll")
-  it.todo("warns and sets invalid if the roll errors")
+  var interaction
+  var saved_rolls
+
+  beforeEach(() => {
+    interaction = new Interaction()
+    saved_rolls = new UserSavedRolls(interaction.guildId, interaction.user.id)
+  })
+
+  it("warns on unknown roll", async () => {
+    interaction.command_options.name = "nope"
+
+    await saved_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("does not exist")
+  })
+
+  it("warns on invalid", async () => {
+    saved_rolls.create({
+      name: "test",
+      description: "test",
+      command: "roll",
+      options: {
+        pool: 0,
+        sides: 6,
+      },
+      invalid: true,
+    })
+    interaction.command_options.name = "test"
+
+    await saved_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("not valid")
+  })
+
+  it("warns on incomplete", async () => {
+    saved_rolls.create({
+      name: "test",
+      description: "test",
+      incomplete: true,
+    })
+    interaction.command_options.name = "test"
+
+    await saved_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("not finished")
+  })
+
+  it("executes the roll", async () => {
+    saved_rolls.create({
+      name: "test",
+      description: "test",
+      command: "roll",
+      options: {
+        pool: 1,
+        sides: 6,
+      },
+    })
+    interaction.command_options.name = "test"
+
+    await saved_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("rolled")
+  })
+
+  it("warns and marks the saved roll invalid if saved options are bad", async () => {
+    saved_rolls.create({
+      name: "test",
+      description: "test",
+      command: "roll",
+      options: {
+        pool: 0,
+        sides: 6,
+      },
+    })
+    interaction.command_options.name = "test"
+
+    await saved_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("no longer valid")
+    const detail = saved_rolls.detail(undefined, "test")
+    expect(detail.invalid).toBeTruthy()
+  })
+
+  describe("with a bonus", () => {
+    it("adds the bonus to the automatic option", async () => {
+      saved_rolls.create({
+        name: "test",
+        description: "test",
+        command: "roll",
+        options: {
+          pool: 1,
+          sides: 6,
+          modifier: 6,
+        },
+      })
+      interaction.command_options.name = "test"
+      interaction.command_options.bonus = 2
+
+      await saved_roll_command.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("+ 8")
+    })
+
+    it("adds the bonus to the chosen option", async () => {
+      saved_rolls.create({
+        name: "test",
+        description: "test",
+        command: "roll",
+        options: {
+          pool: 1,
+          sides: 6,
+          modifier: 6,
+        },
+      })
+      interaction.command_options.name = "test"
+      interaction.command_options.bonus = 2
+      interaction.command_options.change = "pool"
+
+      await saved_roll_command.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("3d6")
+    })
+
+    it("validates the modified options", async () => {
+      saved_rolls.create({
+        name: "test",
+        description: "test",
+        command: "roll",
+        options: {
+          pool: 1,
+          sides: 6,
+          modifier: 6,
+        },
+      })
+      interaction.command_options.name = "test"
+      interaction.command_options.bonus = -1
+      interaction.command_options.change = "pool"
+
+      await saved_roll_command.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("can no longer")
+    })
+  })
+})
+
+describe("change_target", () => {
+  it("is undefined with no bonus", () => {
+    const bonus = 0
+    const change = undefined
+    const savable = ["modifier"]
+
+    const result = saved_roll_command.change_target(bonus, change, savable)
+
+    expect(result).toBeUndefined()
+  })
+
+  it("returns the named option if provided", () => {
+    const bonus = 1
+    const change = "pool"
+    const savable = ["modifier", "pool"]
+
+    const result = saved_roll_command.change_target(bonus, change, savable)
+
+    expect(result).toMatch("pool")
+  })
+
+  it("returns modifier if supported", () => {
+    const bonus = 1
+    const change = undefined
+    const savable = ["modifier"]
+
+    const result = saved_roll_command.change_target(bonus, change, savable)
+
+    expect(result).toMatch("modifier")
+  })
+
+  it("returns pool if supported and modifier is not", () => {
+    const bonus = 1
+    const change = undefined
+    const savable = ["pool"]
+
+    const result = saved_roll_command.change_target(bonus, change, savable)
+
+    expect(result).toMatch("pool")
+  })
 })
