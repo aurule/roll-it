65bc6a8b4ddd243b0a7dc6e01233c76138335426
Paige Andrews <paige.andrews@modolabs.com>
2024-08-01 14:40:14 -0400

    Instantiate schemas outside of the execute function

--- commands/saved/set.js
+++ commands/saved/set.js
@@ -10,6 +10,64 @@ const commonSchemas = require("../../util/common-schemas")
 const MAX_UPLOAD_SIZE = 5_242_880
 const MAX_ENTRY_LENGTH = 1500
 
+/**
+ * Validate that the given value is not in use as a name for a saved roll.
+ *
+ * This helper **requires** that the validation be run with a context including the `saved_rolls` attribute.
+ *
+ * @param  {str} value   Name to check
+ * @param  {obj} helpers Joi helpers object with a context obj which includes saved_rolls
+ * @return {str}         The value that was passed in
+ */
+function name_taken_validator(value, helpers) {
+  if (helpers.prefs.context.saved_rolls.taken(value)) {
+    return helpers.error("any.custom.unique")
+  }
+
+  return value
+}
+
+/**
+ * Schema for validating incoming command options
+ *
+ * This repeats the validations performed by discord, with the addition of ensuring the name is not yet used
+ * for a saved roll by this user in this guild.
+ *
+ * @type {Joi.object}
+ */
+const options_schema = Joi.object({
+  name: Joi.string()
+    .trim()
+    .required()
+    .min(3)
+    .max(100)
+    .custom(name_taken_validator, "whether the name is in use")
+    .messages({
+      "any.custom.unique": oneLine`
+        You already have a saved roll named "{#value}". You can pick a different name, or use
+        ${inlineCode("/saved manage")} to remove the existing roll.
+      `,
+    }),
+  description: commonSchemas.description,
+  invocation: Joi.string()
+    .trim()
+    .optional()
+    .min(4)
+    .max(1500),
+})
+
+/**
+ * Minimal schema to validate presence. Correctness must be validated before we reach this point.
+ *
+ * @type {Joi.object}
+ */
+const saved_roll_presence_schema = Joi.object({
+  name: Joi.string(),
+  description: Joi.string(),
+  command: Joi.string(),
+  options: Joi.object(),
+}).unknown()
+
 module.exports = {
   name: "set",
   parent: "saved",
@@ -50,35 +108,6 @@ module.exports = {
     const savable_commands = require("../index").savable()
     const saved_rolls = new UserSavedRolls(interaction.guildId, interaction.user.id)
 
-    const name_taken_validator = (value, helpers) => {
-      if (saved_rolls.taken(value)) {
-        return helpers.error("any.custom.unique")
-      }
-
-      return value
-    }
-
-    const options_schema = Joi.object({
-      name: Joi.string()
-        .trim()
-        .required()
-        .min(3)
-        .max(100)
-        .custom(name_taken_validator, "whether the name is in use")
-        .messages({
-          "any.custom.unique": oneLine`
-            You already have a saved roll named "{#value}". You can pick a different name, or use
-            ${inlineCode("/saved manage")} to remove the existing roll.
-          `,
-        }),
-      description: commonSchemas.description,
-      invocation: Joi.string()
-        .trim()
-        .optional()
-        .min(4)
-        .max(1500),
-    })
-
     const raw_options = {
       name: interaction.options.getString("name"),
       description: interaction.options.getString("description"),
@@ -87,7 +116,7 @@ module.exports = {
 
     let command_options = {}
     try {
-      command_options = await options_schema.validateAsync(raw_options)
+      command_options = await options_schema.validateAsync(raw_options, {context: {saved_rolls}})
     } catch (err) {
       return interaction.editReply({
         content: err.details[0].message,
@@ -141,13 +170,6 @@ module.exports = {
     const saved_details = saved_rolls.detail(record_id)
 
     // see if the changes complete the saved roll
-    // Minimal schema to validate presence. Correctness must be validated before we reach this point.
-    const saved_roll_presence_schema = Joi.object({
-      name: Joi.string(),
-      description: Joi.string(),
-      command: Joi.string(),
-      options: Joi.object(),
-    }).unknown()
 
     try {
       await saved_roll_presence_schema.validateAsync(saved_details)
--- commands/table/add.js
+++ commands/table/add.js
@@ -7,6 +7,87 @@ const { fetchLines } = require("../../util/attachment-lines")
 const MAX_UPLOAD_SIZE = 5_242_880
 const MAX_ENTRY_LENGTH = 1500
 
+/**
+ * Validate that a given value is not in use as a name for a rollable in this server
+ *
+ *
+ * This helper **requires** that the validation be run with a context including the `tables` attribute.
+ *
+ * @param  {str} value   Name to check
+ * @param  {obj} helpers Joi helpers object with a context obj which includes tables
+ * @return {str}         The value that was passed in
+ */
+function name_taken_validator(value, helpers) {
+  if (helpers.prefs.context.tables.taken(value)) {
+    return helpers.error("any.custom.unique")
+  }
+
+  return value
+}
+
+/**
+ * Schema for validating incoming command options
+ *
+ * This repeats the validations performed by discord, with some additions:
+ * 1. The name is not yet used for a table in this guild.
+ * 2. The uploaded file is a text file and under 5MB
+ *
+ * @type {Joi.object}
+ */
+const options_schema = Joi.object({
+  table_name: Joi.string()
+    .trim()
+    .required()
+    .min(3)
+    .custom(name_taken_validator, "whether the name is in use")
+    .messages({
+      "any.custom.unique": oneLine`
+        There is already a table named "{#value}". You can pick a different name, or use
+        ${inlineCode("/table manage")} to remove the existing table.
+      `,
+    }),
+  description: Joi.string().trim().required().min(3),
+  secret: Joi.boolean(),
+  table_file: Joi.object({
+    contentType: Joi.string()
+      .valid("text/plain")
+      .required()
+      .messages({
+        "any.only": oneLine`
+          The file you uploaded does not look like a plain text file. Try again using a plain, basic text
+          file (save as ${inlineCode(".txt")}) with one result per line (when word wrap is turned off).
+        `,
+      }),
+    size: Joi.number()
+      .required()
+      .max(MAX_UPLOAD_SIZE)
+      .message("The file you uploaded is too large. Make sure your file is less than 5 MB."),
+    contents: Joi.string().optional(), //for testing
+  }).unknown(),
+})
+
+/**
+ * Schema for validating the uploaded file's contents
+ *
+ * It ensures that each line is under 1500 characters long and that there are at least two lines.
+ *
+ * @type {Joi.object}
+ */
+const contents_schema = Joi.array()
+  .items(
+    Joi.string()
+      .max(MAX_ENTRY_LENGTH)
+      .message(
+        `At least one of the table entries is too long. Keep each one below ${MAX_ENTRY_LENGTH} characters.`,
+      )
+      .trim(),
+  )
+  .min(2)
+  .message(
+    "The file you uploaded does not have enough lines. Ensure there are at least two lines of text in the file.",
+  )
+  .required()
+
 module.exports = {
   name: "add",
   parent: "table",
@@ -44,61 +125,6 @@ module.exports = {
     interaction.deferReply()
     const tables = new GuildRollables(interaction.guildId)
 
-    const name_taken_validator = (value, helpers) => {
-      if (tables.taken(value)) {
-        return helpers.error("any.custom.unique")
-      }
-
-      return value
-    }
-
-    const options_schema = Joi.object({
-      table_name: Joi.string()
-        .trim()
-        .required()
-        .min(3)
-        .custom(name_taken_validator, "whether the name is in use")
-        .messages({
-          "any.custom.unique": oneLine`
-            There is already a table named "{#value}". You can pick a different name, or use
-            ${inlineCode("/table manage")} to remove the existing table.
-          `,
-        }),
-      description: Joi.string().trim().required().min(3),
-      secret: Joi.boolean(),
-      table_file: Joi.object({
-        contentType: Joi.string()
-          .valid("text/plain")
-          .required()
-          .messages({
-            "any.only": oneLine`
-              The file you uploaded does not look like a plain text file. Try again using a plain, basic text
-              file (save as ${inlineCode(".txt")}) with one result per line (when word wrap is turned off).
-            `,
-          }),
-        size: Joi.number()
-          .required()
-          .max(MAX_UPLOAD_SIZE)
-          .message("The file you uploaded is too large. Make sure your file is less than 5 MB."),
-        contents: Joi.string().optional(), //for testing
-      }).unknown(),
-    })
-
-    const contents_schema = Joi.array()
-      .items(
-        Joi.string()
-          .max(MAX_ENTRY_LENGTH)
-          .message(
-            `At least one of the table entries is too long. Keep each one below ${MAX_ENTRY_LENGTH} characters.`,
-          )
-          .trim(),
-      )
-      .min(2)
-      .message(
-        "The file you uploaded does not have enough lines. Ensure there are at least two lines of text in the file.",
-      )
-      .required()
-
     const raw_options = {
       table_name: interaction.options.getString("name"),
       description: interaction.options.getString("description"),
@@ -108,7 +134,7 @@ module.exports = {
 
     let normalized_options
     try {
-      normalized_options = await options_schema.validateAsync(raw_options)
+      normalized_options = await options_schema.validateAsync(raw_options, {context: {tables}})
     } catch (err) {
       return interaction.editReply({
         content: err.details[0].message,
