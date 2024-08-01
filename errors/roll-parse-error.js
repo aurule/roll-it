0d7c0aef0a0450535094209d5e4bd6d29399ac56
Paige Andrews <paige.andrews@modolabs.com>
2024-08-01 14:42:23 -0400

    Document parse error class

--- errors/roll-parse-error.js
+++ errors/roll-parse-error.js
@@ -1,9 +1,19 @@
+/**
+ * Error class to throw within parser functions
+ *
+ * Unlike most errors, this expects to receive an array of messages.
+ */
 class RollParseError extends Error {
-  constructor(messages) {
+  constructor(...messages) {
     super()
     this.messages = messages
   }
 
+  /**
+   * Consolidate all messages into a single string
+   *
+   * @return {str} All of our error messages separated by newlines
+   */
   get message() {
     return this.messages.join("\n")
   }
