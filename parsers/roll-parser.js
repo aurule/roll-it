10de0561ac975ce7396764535bbc5689706400d7
Paige Andrews <paige.andrews@modolabs.com>
2024-08-02 15:46:38 -0400

    Add roll parser

---
+++ parsers/roll-parser.js
@@ -0,0 +1,57 @@
+const Joi = require("joi")
+const { RollParseError } = require("../errors/roll-parse-error")
+const command = require("../commands/roll")
+
+const pool_re = /(?<pool>\d+)d(?<sides>\d+)/
+const modifier_re = /(?<modifier>(?:- )?\d+)\)/
+const rolls_re = /(?<rolls>\d+) times/
+
+module.exports = {
+  name: "roll",
+  /**
+   * Parse the presented output of the /roll command
+   *
+   * @example
+   * ```js
+   * const content = '<@12345> rolled **7** "example roll" (1d6: [5] + 2)'
+   * parse(content)
+   * // {
+   * //   modifier: 4
+   * // }
+   * ```
+   *
+   * @param  {str} content Command execution output
+   * @return {obj}         Object of command options
+   *
+   * @throws RollParseError On an invalid content string or invalid options.
+   */
+  async parse(content) {
+    const stripped_content = content.replace(/".*"/, '')
+    const raw_options = {}
+
+    const pool_groups = pool_re.exec(stripped_content)?.groups
+    if (pool_groups) {
+      raw_options.pool = pool_groups.pool
+      raw_options.sides = pool_groups.sides
+    }
+
+    const modifier_groups = modifier_re.exec(stripped_content)?.groups
+    if (modifier_groups) {
+      raw_options.modifier = modifier_groups.modifier
+    }
+
+    const rolls_groups = rolls_re.exec(stripped_content)?.groups
+    if (rolls_groups) {
+      raw_options.rolls = rolls_groups.rolls
+    }
+
+    var validated_options
+    try {
+      validated_options = await command.schema.validateAsync(raw_options, {abortEarly: false})
+    } catch(err) {
+      throw new RollParseError(err.details.map(d => d.message))
+    }
+
+    return validated_options
+  },
+}
