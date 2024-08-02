f531d46a4542dc4350aff55040260c7a9f5d45ed
Paige Andrews <paige.andrews@modolabs.com>
2024-08-02 14:40:28 -0400

    Implement save this roll context command

--- commands/save-this-roll.js
+++ commands/save-this-roll.js
@@ -1,4 +1,20 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType, inlineCode, italic } = require("discord.js")
+const { oneLine } = require("common-tags")
+const Joi = require("joi")
+const { UserSavedRolls } = require("../db/saved_rolls")
+const CommandNamePresenter = require("../presenters/command-name-presenter")
+
+/**
+ * Minimal schema to validate presence. Correctness must be validated before we reach this point.
+ *
+ * @type {Joi.object}
+ */
+const saved_roll_presence_schema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  command: Joi.string().required(),
+  options: Joi.object().required(),
+}).unknown()
 
 module.exports = {
   name: "Save this roll",
@@ -10,15 +26,102 @@ module.exports = {
     new ContextMenuCommandBuilder()
       .setName(module.exports.name)
       .setType(ApplicationCommandType.Message),
-  execute(interaction) {
-    // error if command is missing or not savable
-    // parse message
-    // error if parse fails
-    // upsert the command and incomplete flag
-    // get details
-    // reply with instructions or success
+  async execute(interaction) {
+    const commands = require("./index")
+    const parsers = require("../parsers")
+
+    const message = interaction.targetMessage
+    const command_name = message.interaction.commandName
+    const command = commands.get(command_name)
+
+    if (!command) {
+      return interaction.reply({
+        content: "That message was not sent by a Roll It command.",
+        ephemeral: true,
+      })
+    }
+
+    if (!command.savable) {
+      return interaction.reply({
+        content: `The command ${inlineCode(command_name)} cannot be saved.`,
+        ephemeral: true,
+      })
+    }
+
+    const parser = parsers.get(command_name)
+    if (!parser) {
+      return interaction.reply({
+        content: oneLine`
+          You found a bug! There is no parser for the options of ${inlineCode(command_name)}, but there should be.
+          Please report this error if you can.
+        `,
+        ephemeral: true,
+      })
+    }
+
+    let parsed_options
+    try {
+      parsed_options = await parser.parse(message.content)
+    } catch(err) {
+      return interaction.reply({
+        content: `There was a problem saving ${inlineCode(command_name)}:\n` + err.message,
+        ephemeral: true,
+      })
+    }
+
+    const saved_roll_params = {
+      command: command_name,
+      options: parsed_options,
+      incomplete: true,
+    }
+
+    const saved_rolls = new UserSavedRolls(interaction.guildId, interaction.user.id)
+    const record_result = saved_rolls.upsert(saved_roll_params)
+    const record_id = record_result.lastInsertRowid
+    const saved_details = saved_rolls.detail(record_id)
+
+    // see if the changes complete the saved roll
+
+    try {
+      await saved_roll_presence_schema.validateAsync(saved_details)
+    } catch (err) {
+      return interaction.reply({
+        content: oneLine`
+          You've saved the command for a new roll. Now use ${inlineCode("/saved set")} to set its name and
+          description.
+        `,
+        ephemeral: true,
+      })
+    }
+
+    // the roll is finished
+    saved_rolls.update(record_id, {incomplete: false})
+
+    return interaction.reply({
+      content: oneLine`
+        You've saved the roll ${italic(saved_details.name)}! Try it out with
+        ${inlineCode("/saved roll name:" + saved_details.name)}.
+      `,
+      ephemeral: true,
+    })
   },
   help({ command_name }) {
-    return "WIP"
+    const savable_commands = require("./index").savable()
+    return [
+      oneLine`
+        ${command_name} helps create a new saved roll for you on this server by setting the command and
+        options. Then, you set the name and description using ${inlineCode("/saved set")}. Once you're
+        finished, you can use ${inlineCode("/saved roll")} to roll it!
+      `,
+      "",
+      oneLine`
+        You can only have one unfinished roll at a time. That can either be a roll you just created, or a roll
+        you are editing with ${inlineCode("/saved manage")}. Until you finish that roll's options, you will not
+        be able to use it, nor will you be able to edit or create another saved roll.
+      `,
+      "",
+      "Not all commands can be saved. Here is a list of the ones which can be used:",
+      CommandNamePresenter.list(savable_commands),
+    ].join("\n")
   }
 }
--- commands/save-this-roll.test.js
+++ commands/save-this-roll.test.js
@@ -1,26 +1,176 @@
+const { UserSavedRolls } = require("../db/saved_rolls")
+const { Interaction } = require("../testing/interaction")
+
 const save_roll_command = require("./save-this-roll")
 
 describe("execute", () => {
+  let interaction
+  let saved_rolls
+  beforeEach(() => {
+    interaction = new Interaction()
+    saved_rolls = new UserSavedRolls(interaction.guildId, interaction.user.id)
+  })
+
+  it("warns on unknown command", async () => {
+    interaction.targetMessage = {
+      interaction: {commandName: "gobbledegook"},
+      content: "lorem ipsum"
+    }
+
+    await save_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("not sent by a Roll It command")
+  })
+
+  it("warns on non-savable command", async () => {
+    interaction.targetMessage = {
+      interaction: {commandName: "chop"},
+      content: "lorem ipsum"
+    }
+
+    await save_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("cannot be saved")
+  })
+
+  it("warns on invalid options", async () => {
+    interaction.targetMessage = {
+      interaction: {commandName: "d10"},
+      content: "0 times"
+    }
+
+    await save_roll_command.execute(interaction)
+
+    expect(interaction.replyContent).toMatch("problem saving")
+  })
+
   describe("with no incomplete roll", () => {
-    it.todo("saves the command")
-    it.todo("saves the options")
-    it.todo("marks the record incomplete")
-    it.todo("responds with instructions to finish the saved roll")
+    beforeEach(() => {
+      interaction.targetMessage = {
+        interaction: {commandName: "d10"},
+        content: '<@12345> rolled **7** (3 + 4) for "a roll"',
+      }
+    })
+
+    it("saves the command", async () => {
+      await save_roll_command.execute(interaction)
+
+      const detail = saved_rolls.incomplete()
+      expect(detail.command).toMatch("d10")
+    })
+
+    it("saves the options", async () => {
+      await save_roll_command.execute(interaction)
+
+      const detail = saved_rolls.incomplete()
+      expect(detail.options).toMatchObject({modifier: 4})
+    })
+
+    it("marks the record incomplete", async () => {
+      await save_roll_command.execute(interaction)
+
+      const detail = saved_rolls.incomplete()
+      expect(detail.incomplete).toBeTruthy()
+    })
+
+    it("responds with instructions to finish the saved roll", async () => {
+      await save_roll_command.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("set its name and description")
+    })
   })
 
   describe("with an incomplete roll", () => {
     describe("with name and description", () => {
-      it.todo("saves the command")
-      it.todo("saves the options")
-      it.todo("marks the record complete")
-      it.todo("responds with instructions to use the saved roll")
+      var record_id
+
+      beforeEach(() => {
+        const created = saved_rolls.create({
+          name: "test",
+          description: "test",
+          incomplete: true,
+        })
+        record_id = created.lastInsertRowid
+
+        interaction.targetMessage = {
+          interaction: {commandName: "d10"},
+          content: '<@12345> rolled **7** (3 + 4) for "a roll"',
+        }
+      })
+
+      it("saves the command", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(undefined, "test")
+        expect(detail.command).toMatch("d10")
+      })
+
+      it("saves the options", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(undefined, "test")
+        expect(detail.options).toMatchObject({modifier: 4})
+      })
+
+      it("marks the record complete", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(undefined, "test")
+        expect(detail.incomplete).toBeFalsy()
+      })
+
+      it("responds with instructions to use the saved roll", async () => {
+        await save_roll_command.execute(interaction)
+
+        expect(interaction.replyContent).toMatch("saved the roll")
+      })
     })
 
     describe("with command and options", () => {
-      it.todo("overwrites the command")
-      it.todo("overwrites the options")
-      it.todo("leaves the record incomplete")
-      it.todo("responds with instructions to finish the saved roll")
+      var record_id
+
+      beforeEach(() => {
+        const created = saved_rolls.create({
+          command: "wod20",
+          options: {
+            pool: 7,
+          },
+          incomplete: true,
+        })
+        record_id = created.lastInsertRowid
+
+        interaction.targetMessage = {
+          interaction: {commandName: "d10"},
+          content: '<@12345> rolled **7** (3 + 4) for "a roll"',
+        }
+      })
+
+      it("overwrites the command", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(record_id)
+        expect(detail.command).toMatch("d10")
+      })
+
+      it("overwrites the options", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(record_id)
+        expect(detail.options).toMatchObject({modifier: 4})
+      })
+
+      it("leaves the record incomplete", async () => {
+        await save_roll_command.execute(interaction)
+
+        const detail = saved_rolls.detail(record_id)
+        expect(detail.incomplete).toBeTruthy()
+      })
+
+      it("responds with instructions to finish the saved roll", async () => {
+        await save_roll_command.execute(interaction)
+
+        expect(interaction.replyContent).toMatch("set its name and description")
+      })
     })
   })
 })
