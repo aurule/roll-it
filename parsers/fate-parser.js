fcfa3cafeb83bc2d135d79616a97fc93e1d1018c
Paige Andrews <paige.andrews@modolabs.com>
2024-08-02 15:09:46 -0400

    Add fate results parser

---
+++ parsers/fate-parser.js
@@ -0,0 +1,52 @@
+const Joi = require("joi")
+const { RollParseError } = require("../errors/roll-parse-error")
+const command = require("../commands/fate")
+
+const modifier_re = /> [-+] (?<modifier>[-]?\d+)/
+const rolls_re = /(?<rolls>\d+) times/
+
+module.exports = {
+  name: "fate",
+  /**
+   * Parse the presented output of the /fate command
+   *
+   * @example
+   * ```js
+   * const content = '<@12345> rolled **a Great (+3)** for "example roll": <:emoji:12345><:emoji:12345><:emoji:12345><:emoji:12345> + 3
+   * parse(content)
+   * // {
+   * //   modifier: 3
+   * // }
+   * ```
+   *
+   * @param  {str} content Command execution output
+   * @return {obj}         Object of command options
+   *
+   * @throws RollParseError On an invalid content string or invalid options.
+   */
+  async parse(content) {
+    // <@12345> rolled **a Great (+3)** for "description": 4x<:fateneg:1038147643836203018> + modifier
+
+    const stripped_content = content.replace(/".*"/, '')
+    const raw_options = {}
+
+    const modifier_groups = modifier_re.exec(stripped_content)?.groups
+    if (modifier_groups) {
+      raw_options.modifier = modifier_groups.modifier
+    }
+
+    const rolls_groups = rolls_re.exec(stripped_content)?.groups
+    if (rolls_groups) {
+      raw_options.rolls = rolls_groups.rolls
+    }
+
+    var validated_options
+    try {
+      validated_options = await command.schema.validateAsync(raw_options, {abortEarly: false})
+    } catch(err) {
+      throw new RollParseError(err.details.map(d => d.message))
+    }
+
+    return validated_options
+  },
+}
---
+++ parsers/fate-parser.test.js
@@ -0,0 +1,125 @@
+const { present } = require("../presenters/fate-results-presenter")
+
+const { parse } = require("./fate-parser")
+
+describe("with junk input", () => {
+  it("returns an empty object", async () => {
+    const result = await parse("this is some garbage")
+
+    expect(JSON.stringify(result)).toMatch("{}")
+  })
+})
+
+describe("minimal output", () => {
+  it("returns an empty object", async () => {
+    const content = present({
+      rolls: 1,
+      raw: [[1, 2, 3, 2]],
+      summed: [0],
+    })
+
+    const result = await parse(content)
+
+    expect(JSON.stringify(result)).toMatch("{}")
+  })
+})
+
+describe.each([
+  [
+    "single roll",
+    {
+      rolls: 1,
+      raw: [[1, 2, 3, 2]],
+      summed: [0],
+    },
+  ],
+  [
+    "multiple rolls",
+    {
+      rolls: 2,
+      raw: [
+        [1, 2, 3, 2],
+        [1, 2, 3, 3],
+      ],
+      summed: [0, 1],
+    },
+  ],
+])("%s", (_suite, raw_opts) => {
+  it("gets modifier if present", async () => {
+    const content = present({
+      ...raw_opts,
+      modifier: 2,
+    })
+
+    const result = await parse(content)
+
+    expect(result.modifier).toEqual(2)
+  })
+
+  it("skips modifier if not present", async () => {
+    const content = present(raw_opts)
+
+    const result = await parse(content)
+
+    expect(result.modifier).toBeUndefined()
+  })
+
+  it("ignores decoy modifier in description", async () => {
+    const content = present({
+      ...raw_opts,
+      modifier: 2,
+      description: "(I wanted a 5)"
+    })
+
+    const result = await parse(content)
+
+    expect(result.modifier).toEqual(2)
+  })
+})
+
+describe("single roll", () => {
+  it("skips rolls", async () => {
+    const content = present({
+      rolls: 1,
+      raw: [[1, 2, 3, 2]],
+      summed: [0],
+    })
+
+    const result = await parse(content)
+
+    expect(result.rolls).toBeUndefined()
+  })
+})
+
+describe("multiple rolls", () => {
+  it("gets rolls", async () => {
+    const content = present({
+      rolls: 2,
+      raw: [
+        [1, 2, 3, 2],
+        [1, 2, 3, 3],
+      ],
+      summed: [0, 1],
+    })
+
+    const result = await parse(content)
+
+    expect(result.rolls).toEqual(2)
+  })
+
+  it("ignores decoy rolls in description", async () => {
+    const content = present({
+      rolls: 2,
+      raw: [
+        [1, 2, 3, 2],
+        [1, 2, 3, 3],
+      ],
+      summed: [0, 1],
+      description: "5 times!"
+    })
+
+    const result = await parse(content)
+
+    expect(result.rolls).toEqual(2)
+  })
+})
