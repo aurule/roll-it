6cd3488991d633d8939dbf882f195514ab94a7cf
Paige Andrews <paige.andrews@modolabs.com>
2024-08-06 14:20:52 -0400

    Add script to mark invalid saved rolls

---
+++ scripts/mark-invalid-rolls.js
@@ -0,0 +1,35 @@
+const progress = require("cli-progress")
+
+const { GlobalSavedRolls } = require("../db/saved_rolls")
+const commands = require("../commands")
+const { logger } = require("../util/logger")
+
+logger.level = "warn"
+
+const saved_rolls = new GlobalSavedRolls()
+const all_rolls = saved_rolls.all()
+
+const bar = new progress.SingleBar({
+  hideCursor: true,
+  stopOnComplete: true,
+})
+
+process.stdout.write("Validating all saved rolls...\n")
+bar.start(all_rolls.length, 0)
+
+for (const roll_detail of all_rolls) {
+  const command = commands.get(roll_detail.command)
+
+  const validation_result = command.schema?.validate(roll_detail.options)
+  if (validation_result?.error) {
+    console.log(`bad roll`)
+    saved_rolls.update(roll_detail.id, {invalid: true})
+  }
+
+  if (!validation_result) {
+    console.log(`so that's weird`)
+    saved_rolls.update(roll_detail.id, {invalid: true})
+  }
+
+  bar.increment()
+}
