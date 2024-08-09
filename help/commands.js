220f2e3295c771e8cc6bfdde3db9a93cb9187225
Paige Andrews <paige.andrews@modolabs.com>
2024-08-09 14:55:34 -0400

    Tweak command help

--- help/commands.js
+++ help/commands.js
@@ -11,26 +11,26 @@ module.exports = {
     return [
       oneLine`
         Roll It uses slash commands to roll dice using different built-in dice mechanics. Slash commands are
-        started by typing a ${inlineCode("/")} character and then the name of the command. Discord will suggest
-        commands as you type. To accept a suggestion, click it from the list that pops up or hit the
+        started by typing a ${inlineCode("/")} character and then the name of the command. Discord will
+        suggest commands as you type. To accept a suggestion, click it from the list that pops up or hit the
         ${inlineCode("Tab")} key.
       `,
       "",
       oneLine`
         Commands usually accept one or more arguments. These are used to pass information to the command, like
         the size of a dice pool. Once you've selected a command, Discord will let you fill in
-        its required arguments. When you're done with an argument, press ${inlineCode("Tab")} to move to the
-        next one. After the required arguments have been filled out, Discord will show you the command's
-        optional arguments in a list, like it did for the command names. Hit ${inlineCode("Tab")} or select
-        the argument name to begin filling it in.
+        its required arguments. When you're done with an argument, press ${inlineCode("Tab")} on desktop to
+        move to the next one. After the required arguments have been filled out, Discord will show you the
+        command's optional arguments in a list, like it did for the command names. Press ${inlineCode("Tab")}
+        or select the argument name to begin filling it in.
       `,
       "",
       oneLine`
         Server managers can set which roll commands are available. See the help for the
-        ${inlineCode("/roll-chooser")} command for more.
+        ${inlineCode("/roll-chooser")} command to learn more.
       `,
       "",
-      "Here are all of the slash commands that Roll It knows:",
+      "Here are all of the commands that Roll It knows:",
       commands
         .filter((c) => c.type !== "menu")
         .map((c) => `• ${commandNamePresenter.present(c)} - ${c.description}`)
