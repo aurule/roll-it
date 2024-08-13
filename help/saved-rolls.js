7f4e099b31b0192d6fb6c474064778e62c02cf2e
Paige Andrews <paige.andrews@modolabs.com>
2024-08-13 16:38:14 -0400

    Improve saved roll help topic

--- help/saved-rolls.js
+++ help/saved-rolls.js
@@ -1,4 +1,4 @@
-const { inlineCode, italic, bold } = require("discord.js")
+const { inlineCode, italic, bold, heading, orderedList } = require("discord.js")
 const { oneLine } = require("common-tags")
 const CommandNamePresenter = require("../presenters/command-name-presenter")
 
@@ -10,6 +10,9 @@ module.exports = {
     const savable_commands = require("../commands").savable()
     const saved_set = inlineCode("/saved set")
     const save_roll = italic("Save this roll")
+    const roll = inlineCode("/saved roll")
+    const list = inlineCode("/saved list")
+    const manage = inlineCode("/saved manage")
 
     return [
       oneLine`
@@ -18,25 +21,33 @@ module.exports = {
         parts:
       `,
       "",
-      oneLine`
-        1. A name for the saved roll. This is how you will find the roll if you save more than one, so it has
-        to be unique.
-      `,
-      oneLine`
-        2. A description for the saved roll. This should describe what the roll does, and will be shown as the
-      default description each time you use that saved roll.
-      `,
-      oneLine`
-        3. The command and its options. This does the actual rolling, so it's super important.
-      `,
+      orderedList([
+        oneLine`
+          A name for the saved roll. This is how you will find it if you save more than one, so the name has
+          to be unique.
+        `,
+        oneLine`
+          A description for the saved roll. This should describe what the roll does, and will be shown as the
+          default description each time you use that saved roll.
+        `,
+        oneLine`
+          The command and its options. This does the actual rolling, so it's super important.
+        `,
+      ]),
       "",
       oneLine`
         In order to save all this, Roll It uses a pair of commands. The first is a normal slash command,
         ${saved_set}, which stores the name and description. The second is a context command (more on those in
-        a moment), ${save_roll}, which stores the command and its options. That's it! Once you've saved all
-        three parts, you're ready to use your saved roll.
+        a moment) named ${save_roll}, which stores the command and its options. That's it! Once you've saved
+        all three parts, you're ready to use your saved roll.
       `,
       "",
+      oneLine`
+        You can save the parts of a new roll in any order. You can start with ${save_roll} if you realize you
+        want to save the roll you just made, or with ${saved_set} if you have a name in mind first. Roll It
+        will save one incomplete command at a time for you to finish up later.
+      `,
+      heading("Starting With a Command", 2),
       oneLine`
         ${save_roll} is the first context command in Roll It, so it might need some explaining. A context
         command is a bot action that is not typed out, but is used through a menu within Discord. To get to
@@ -47,27 +58,58 @@ module.exports = {
       `,
       "",
       oneLine`
-        In order to save a command, you can use ${save_roll} on the message with that roll's results. It will
-        read that result message and save the command and options that were used to create it. For an example,
-        let's say you used ${inlineCode("/roll pool:2 sides:6")} and Roll It replied with
+        ${save_roll} lets you save a command that you've just used. To do so, use ${save_roll} on the message
+        with that command's results. It will read that result message and save the command and options that
+        were used to create it. Then, it will prompt you to take the next step and use ${saved_set} to finish
+        up your saved roll with a name and a description.
+      `,
+      "",
+      oneLine`
+        > For an example, let's say you used ${inlineCode("/roll pool:2 sides:6")} and Roll It replied with
         ${inlineCode("@you rolled 9 (2d6: [5,4])")}. By long pressing or right clicking on
         ${inlineCode("@you rolled 9 (2d6: [5,4])")}, you select ${inlineCode("Apps")} and then ${save_roll}.
         Roll It reads the message and determines that you used the ${inlineCode("/roll")} command with a
-        ${inlineCode("pool")} of 2 and ${inlineCode("sides")} of 6. It saves that. Once you have the name and
-        description set as well, you can use your roll!
+        ${inlineCode("pool")} of 2 and ${inlineCode("sides")} of 6. It saves that, and tells you to run
+        ${saved_set} to save the name and description. Once you do, you can use your roll!
+      `,
+      "",
+      oneLine`
+        Not all commands can be saved. Some do not roll actual dice (like ${inlineCode("/table roll")} or
+        ${inlineCode("/help")}), or don't have re-usable options (like ${inlineCode("/coin")}), or are so
+        simple that it's more effort to use a saved roll than to just use the options (like
+        ${inlineCode("/chop bomb:true")} vs ${inlineCode("/saved roll name:gimme ties")}). Here are the
+        commands that can be saved:
+      `,
+      CommandNamePresenter.list(savable_commands),
+      heading("Starting With a Name", 2),
+      oneLine`
+        As noted, you can also begin making a new saved roll with ${saved_set} and supplying a
+        ${inlineCode("name")} and ${inlineCode("description")}. ${saved_set} will reply with instructions to
+        use ${save_roll} to save the command and options. Once you do, your roll is ready to go!
       `,
       "",
       oneLine`
         The name of the roll has a single special limitation: you, personally, cannot have two rolls in the
         same server with the same name.
       `,
-      "",
+      heading("Rolling It"),
       oneLine`
-        You can save the parts of a saved roll in any order. You can start with ${save_roll} if that's more
-        convenient, or with ${saved_set} if you have a name in mind first. Roll It will save one incomplete
-        command at a time for you to finish up later.
+        Once you have a saved roll, you can start using it with ${roll}. It takes the name of a saved roll and
+        runs the command and options you stored. It also takes a ${inlineCode("bonus")}, which lets you add to
+        the modifier, pool, or difficulty on the fly. See the help for ${roll} to learn more.
       `,
       "",
+      oneLine`
+        You can see the rolls you've saved by using the ${list} command. That will also warn you if you have
+        an unfinished roll, or one whose options are no longer valid. In either case, you'll have to update
+        the roll in order to use it.
+      `,
+      "",
+      oneLine`
+        So how do you update a saved roll? By using ${manage}! That command lets you remove or edit a roll as
+        needed. See the help for ${manage} to learn more.
+      `,
+      heading("Advanced Usage: The Invocation", 2),
       oneLine`
         So far we've assumed that you're using both ${saved_set} and ${save_roll} in order to build your saved
         commands. But what if you're a power user in a rush? Well, ${saved_set} has you covered. It has an
@@ -83,16 +125,6 @@ module.exports = {
         are both valid invocations. When you supply an invocation along with a name and description, the
         saved roll is complete and you can use it right away.
       `,
-      "",
-      oneLine`
-        Finally, not all commands can be saved. Some do not roll actual dice (like
-        ${inlineCode("/table roll")} or ${inlineCode("/help")}), or don't have re-usable options (like
-        ${inlineCode("/coin")}), or are so simple that it's more effort to use a saved roll than to just use
-        the options (like ${inlineCode("/chop bomb:true")} vs ${inlineCode("/saved roll name:gimme ties")}).
-      `,
-      "",
-      "Here are the commands that can be saved:",
-      CommandNamePresenter.list(savable_commands),
     ].join("\n")
   },
 }
