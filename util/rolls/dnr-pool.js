73ce0905e5132598177c5a9fb734a118c026ae68
Paige Andrews <paige.andrews@modolabs.com>
2024-08-21 16:08:55 -0400

    Add class to get complex attrs of a dnr pool

---
+++ util/rolls/dnr-pool.js
@@ -0,0 +1,76 @@
+const {successes} = require("../../services/tally")
+
+/**
+ * Class representing a single dice pool in a Don't Rest Your Head roll
+ *
+ * Each roll in that system has up to four distinct pools. This class represents a single pool and has helper
+ * methods to generate the properties of that roll.
+ */
+class DnrPool {
+  /**
+   * Name of the pool
+   *
+   * @type {string}
+   */
+  name;
+
+  /**
+   * Raw roller results
+   *
+   * @type {Array<int[]>}
+   */
+  raw;
+
+  /**
+   * Simplified roller results
+   *
+   * This simply exposes the first entry of the raw results, since there is only ever one set of dice rolled.
+   *
+   * @type {int[]}
+   */
+  dice;
+
+  /**
+   * Number of dice in the pool
+   *
+   * Derived from the length of dice.
+   *
+   * @type {int}
+   */
+  size;
+
+  /**
+   * Number of successes per pool, as standard from the tally.successes function
+   *
+   * @type {int[]}
+   */
+  summed;
+
+  /**
+   * Number of successes
+   *
+   * This simply exposes the first entry of the summed results, since there is only ever one set of dice.
+   *
+   * @type {int}
+   */
+  successes;
+
+  constructor(name, raw) {
+    this.name = name
+    this.raw = raw
+
+    this.dice = raw[0]
+    this.size = this.dice.length
+    this.summed = successes(this.raw, 3, true)
+    this.successes = this.summed[0]
+
+    this.spread = Array(7).fill(0)
+    for (const die of this.dice) {
+      this.spread[die] += 1
+    }
+  }
+}
+
+module.exports = {
+  DnrPool
+}
---
+++ util/rolls/dnr-pool.test.js
@@ -0,0 +1,25 @@
+const { DnrPool } = require("./dnr-pool")
+
+describe("successes", () => {
+  it("tallies successes for 1-3", () => {
+    const pool = new DnrPool("test", [[2, 3, 4]])
+
+    expect(pool.successes).toEqual(2)
+  })
+})
+
+describe("size", () => {
+  it("returns number of dice rolled", () => {
+    const pool = new DnrPool("test", [[2, 3, 4]])
+
+    expect(pool.size).toEqual(3)
+  })
+})
+
+describe("spread", () => {
+  it("gets an array of die result counts", () => {
+    const pool = new DnrPool("test", [[2, 3, 4, 4, 5]])
+
+    expect(pool.spread).toEqual([0, 0, 1, 1, 2, 1, 0])
+  })
+})
