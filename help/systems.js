56e740b97a6ac155600220b4d2fd2013c7246010
Paige Andrews <paige.andrews@modolabs.com>
2024-08-26 17:12:26 -0400

    Refine systems help text

--- help/systems.js
+++ help/systems.js
@@ -9,22 +9,18 @@ module.exports = {
     return [
       oneLine`
         Although you ${italic("can")} use the ${inlineCode("/roll")} and ${inlineCode("/roll-formula")}
-        commands to roll dice for basically any system you can think of, it's much more convenient when Roll
-        It does the tallying and complex stuff for you. Here's the list of game systems whose dice mechanics
-        have a command in Roll It, along with any caveats. As always, the help info for a specific command
-        will have more detail than what's here.
+        commands to roll dice for basically any system you can think of, some systems have complex rules.
+        Counting successes, exploding dice, or comparing different tallies is doable but annoying with generic
+        rollers. It's much easier if all that math is handled for you, which is where Roll It's game-specific
+        commands come to the rescue.
+      `,
+      "",
+      oneLine`
+        Here's the list of game systems whose dice mechanics have a command in Roll It, along with any
+        caveats. As always, the help info for a specific command will have more detail than what's here.
       `,
       "",
       unorderedList([
-        oneLine`
-          Generic: ${inlineCode("/d10")}, ${inlineCode("/d100")}, and ${inlineCode("/d20")}, along with
-          ${inlineCode("/roll")} and ${inlineCode("/roll-formula")}. These commands don't do anything too
-          crazy and will give you a plain result. For many games, that's enough!
-        `,
-        oneLine`
-          Misc: ${inlineCode("/coin")} and ${inlineCode("/eightball")}. These don't really roll dice, but
-          they're fun.
-        `,
         oneLine`
           D&D 5e: ${inlineCode("/d20")}. Supports rolling with advantage or disadvantage.
         `,
@@ -45,17 +41,27 @@ module.exports = {
           ${inlineCode("bomb")} and static tests.
         `,
         oneLine`
-          New World of Darkness: ${inlineCode("/nwod")}. AKA Chronicles of Darkness 1e. Rolls and tallies with
-          correct exploding dice. Handles chance rolls, the rote benefit, and has an interactive teamwork
-          mode.
+          New World of Darkness / Chronicles of Darkness: ${inlineCode("/nwod")}. Works for both first and
+          second edition of Chronicles of Darkness. Rolls and tallies with correct exploding dice. Handles
+          chance rolls, the rote benefit, and has an interactive teamwork mode.
         `,
         oneLine`
-          Shadowrun 4e, 5e, 6e: ${inlineCode("/shadowrun")}. Handles glitches and rule of six.
+          Shadowrun 4e, 5e, 6e: ${inlineCode("/shadowrun")}. Handles glitches, the rule of six, and has an
+          interactive teamwork mode.
         `,
         oneLine`
           World of Darkness 20th Anniversary Edition: ${inlineCode("/wod20")}. Rolls and tallies, with support
           for specialties and an interactive teamwork mode.
-        `
+        `,
+        oneLine`
+          Generic: ${inlineCode("/d10")}, ${inlineCode("/d100")}, and ${inlineCode("/d20")}, along with
+          ${inlineCode("/roll")} and ${inlineCode("/roll-formula")}. These commands don't do anything too
+          crazy and will give you a plain result. For many games, that's enough!
+        `,
+        oneLine`
+          Misc: ${inlineCode("/coin")} and ${inlineCode("/eightball")}. These don't really roll dice, but
+          they're fun.
+        `,
       ])
     ].join("\n")
   },
