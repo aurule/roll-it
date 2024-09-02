1d2c076fac9a1a5911bbec5f1b4d3ce129da8d4b
Paige Andrews <pmandrews@gmail.com>
2024-09-02 16:09:43 -0400

    Log event when leaving a guild

---
+++ events/guildDelete.js
@@ -0,0 +1,8 @@
+const { logger } = require("../util/logger")
+
+module.exports = {
+  name: "guildDelete",
+  execute(guild) {
+    logger.info({id: guild.id, name: guild.name}, `Removed from guild`)
+  },
+}
