f9c8d4b1af45a6d978da21320181fe63af015e86
Paige Andrews <paige.andrews@modolabs.com>
2024-09-05 15:06:00 -0400

    Remove function from global namespace

--- docs/_scripts/inline-prism-plugin.js
+++ docs/_scripts/inline-prism-plugin.js
@@ -2,19 +2,17 @@
  * Adapted from the [docsify-code-inline](https://www.npmjs.com/package/@rakutentech/docsify-code-inline) package
  */
 
-function transform(markdown) {
-  const RE = /\[`(.*?)(?<!\\(\\{2})*)`\s+([a-z0-9-]+?)\](?!\()/g
-  return markdown.replace(RE, (_, code, __, lang) => {
-    const escaped = code.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
-    const langOrMarkup = Prism.languages[lang] || Prism.languages.markup
-    const highlighted = Prism.highlight(escaped, langOrMarkup, lang)
-    return `<code class="language-${lang}">${highlighted}</code>`
-  })
-}
-
 ;(function () {
   var inlinePrismPlugin = function (hook, vm) {
-    hook.beforeEach((content) => transform(content))
+    hook.beforeEach((markdown) => {
+      const RE = /\[`(.*?)(?<!\\(\\{2})*)`\s+([a-z0-9-]+?)\](?!\()/g
+      return markdown.replace(RE, (_, code, __, lang) => {
+        const escaped = code.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+        const langOrMarkup = Prism.languages[lang] || Prism.languages.markup
+        const highlighted = Prism.highlight(escaped, langOrMarkup, lang)
+        return `<code class="language-${lang}">${highlighted}</code>`
+      })
+    })
   }
 
   // Add plugin to docsify's plugin array
