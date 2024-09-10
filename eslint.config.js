fbc2ae0885f2de89a94564174f6b9950ea692da0
Paige Andrews <paige.andrews@modolabs.com>
2024-09-10 17:12:43 -0400

    Configure eslint to find unused vars

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  ignores: [
+    "**/*.test.js",
+    ".yarn/",
+    "testing/**",
+    "guides/**",
+    "docs/**",
+    ".pnp*",
+  ],
+  rules: {
+    "no-unused-vars": "error",
+    "no-undef": "error",
+  },
+  languageOptions: {
+    sourceType: "commonjs",
+    globals: {
+      module: "readonly",
+      fetch: "readonly",
+      __dirname: "readonly",
+      __filename: "readonly",
+      process: "readonly",
+      console: "readonly",
+    },
+  },
+}
+
