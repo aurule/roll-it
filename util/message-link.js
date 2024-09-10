09dddbfff3bf38018de8b67a44b90bcb1cd28418
Paige Andrews <paige.andrews@modolabs.com>
2024-09-10 13:35:32 -0400

    Document message link helper

--- util/message-link.js
+++ util/message-link.js
@@ -1,5 +1,12 @@
 const { messageLink, hideLinkEmbed } = require("discord.js")
 
+/**
+ * Generate a link to a specific message
+ *
+ * @param  {Message} message Discord.js message object
+ * @param  {Boolean} embed   Whether to show an embedded preview
+ * @return {str}             String with a correctly formatted link to the message
+ */
 function smartMessageLink(message, embed = true) {
   const raw_link = messageLink(message.channelId, message.id, message.guildId)
   if (embed) {
