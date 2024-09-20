6ff18131c9a2d171181531be0b7067caadcd1fd1
Paige Andrews <paige.andrews@modolabs.com>
2024-09-20 16:51:44 -0400

    Move the heavy logic to a dedicated class

--- commands/met/opposed.js
+++ commands/met/opposed.js
@@ -3,11 +3,12 @@ const { longReply } = require("../../util/long-reply")
 
 const commonOpts = require("../../util/common-options")
 const { throwChoices } = require("../../util/met-throw-options")
+const { MetOpposedManager } = require("../../services/met-opposed-manager")
 
 module.exports = {
   name: "opposed",
   parent: "met",
-  description: "Start an interactive MET rock-paper-scissors challenge",
+  description: "Start an interactive, opposed MET rock-paper-scissors test",
   data: () =>
     new SlashCommandSubcommandBuilder()
       .setName(module.exports.name)
@@ -15,13 +16,13 @@ module.exports = {
       .addUserOption((option) =>
         option
           .setName("opponent")
-          .setDescription("The user you are challenging")
+          .setDescription("User you are challenging")
           .setRequired(true)
       )
       .addStringOption(option =>
         option
           .setName("attribute")
-          .setDescription("The type of attribute this test uses")
+          .setDescription("Type of attribute this test uses")
           .setChoices(
             { name: "Mental", value: "mental" },
             { name: "Social", value: "social" },
@@ -32,16 +33,17 @@ module.exports = {
       .addStringOption(option =>
         option
           .setName("retest")
-          .setDescription("The standard ability to use for a retest")
+          .setDescription("Ability to use for a retest")
           .setRequired(true)
       )
       .addStringOption((option) =>
         option
           .setName("throw")
-          .setDescription("Choose what symbol you use")
+          .setDescription("The symbol you want to use for the first chop")
           .setChoices(...throwChoices(true))
           .setRequired(true),
       )
+      .addStringOption(commonOpts.description)
       .addBooleanOption(option =>
         option
           .setName("bomb")
@@ -51,41 +53,21 @@ module.exports = {
         option
           .setName("ties")
           .setDescription("Whether you win on ties. Defaults to false.")
-      )
-      .addStringOption(commonOpts.description),
+      ),
   async execute(interaction) {
-    // prompt opponent to respond
-    //  update with bomb and ties if clicked
-    // on timeout
-    //  simple message
-    // on Cancel
-    //  ensure clicked by user, remove message
-    // on Throw
-    //  enter.... THE LOOP
-    //
-    //
-    // Status message
-    // on timeout
-    //  show Complete message
-    // on Done
-    //  only for losing party
-    //  show Complete message
-    // on Retest
-    //  show Retest prompt
-    //
-    // Retest message
-    // on timeout
-    //  show Status message
-    // on Cancel
-    //  only for non-retest-initiator
-    //  show Status message
-    // on Throw
-    //  show Status message
-    //
-    //
-    // Complete message
-    //  show summary
-    return interaction.reply("WIP")
+    const manager = new MetOpposedManager({
+      interaction,
+      attackerId: interaction.user.id,
+      defenderId: interaction.options.getUser("opponent").id,
+      attribute: interaction.options.getString("attribute"),
+      retest_ability: interaction.options.getString("retest"),
+      description: interaction.options.getString("description")
+    })
+    manager.attacker.bomb = interaction.options.getBoolean("bomb") ?? false
+    manager.attacker.ties = interaction.options.getBoolean("ties") ?? false
+    manager.current_test.chop(manager.attacker, interaction.options.getString("throw"))
+
+    return manager.begin()
   },
   help({ command_name }) {
     return `undefined`
