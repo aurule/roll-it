0ce8fe8287a67186d1be5f4304d8ca78206c9b85
Paige Andrews <paige.andrews@modolabs.com>
2024-09-23 11:53:02 -0400

    Remove unused code

--- commands/roll-chooser.test.js
+++ commands/roll-chooser.test.js
@@ -1,13 +1,5 @@
 const chooser_command = require("./roll-chooser")
 
-const { Interaction } = require("../testing/interaction")
-
-var interaction
-
-beforeEach(() => {
-  interaction = new Interaction()
-})
-
 it("is global", () => {
   expect(chooser_command.global).toBeTruthy()
 })
