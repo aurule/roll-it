5aa4c6f577f140b59bcf0c822043daf867f6a800
Paige Andrews <paige.andrews@modolabs.com>
2024-10-17 15:32:47 -0400

    Add slash to command pretty helper

--- testing/command-pretty.js
+++ testing/command-pretty.js
@@ -7,7 +7,7 @@
  * @return {str}            Pretty-printed command name
  */
 function pretty(command) {
-  let presented = ""
+  let presented = "/"
   if (command.parent) presented += command.parent + " "
   presented += command.name
   return presented
