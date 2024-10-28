6f429e7ef08f5c3594e169c7af1099d6f3a9b7e4
Paige Andrews <paige.andrews@modolabs.com>
2024-10-28 17:04:34 -0400

    Add schema matcher

---
+++ testing/matchers/match-schema.js
@@ -0,0 +1,28 @@
+const {expect} = require('@jest/globals');
+
+/**
+ * Test whether the given value matches the given Joi schema
+ *
+ * @param  {any}     actual The value to test
+ * @param  {Joi.any} schema The schema to test against
+ * @return {obj}            Jest matcher result object
+ */
+function toMatchSchema(actual, schema) {
+  const result = schema.validate(actual, { abortEarly: true })
+
+  if (result.error === undefined) {
+    return {
+      message: () => `expected ${this.utils.printReceived(actual)} not to match the ${this.utils.printExpected("schema")}`,
+      pass: true,
+    }
+  } else {
+    return {
+      message: () => `expected ${this.utils.printReceived(actual)} to match the ${this.utils.printExpected("schema")}`,
+      pass: false,
+    }
+  }
+}
+
+module.exports = {
+  toMatchSchema,
+}
---
+++ testing/matchers/match-schema.test.js
@@ -0,0 +1,16 @@
+const {expect, it} = require('@jest/globals');
+const Joi = require("joi")
+
+const { toMatchSchema } = require("./match-schema")
+
+describe("toMatchSchema", () => {
+  const test_schema = Joi.string()
+
+  it("has a valid subject", () => {
+    expect("test").toMatchSchema(test_schema)
+  })
+
+  it("has an invalid subject", () => {
+    expect(5).not.toMatchSchema(test_schema)
+  })
+})
