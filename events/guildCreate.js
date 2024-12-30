141ed347096946a0294ce36e712eb8ffd51bc7a3
Paige Andrews <paige.andrews@modolabs.com>
2024-12-30 14:12:43 -0500

    Remove welcome message

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -18,24 +18,5 @@ module.exports = {
     if ((process.env.NODE_ENV != "development") == process.env.DEV_GUILDS.includes(guild.id)) {
       return
     }
-
-    const channel = guildAnnouncementChannel(guild)
-    try {
-      channel.send(
-        [
-          oneLine`
-          You've added Roll It! It's full of dice. ${italic("So many dice.")} Admins, please use the
-          ${inlineCode("/setup-roll-it")} command to set which dice rollers are availble on this server. You can
-          use ${inlineCode("/help topic:Dice Systems")} to see what Roll It supports if you aren't sure what
-          will suit your needs. If you want to know more about a command, use ${inlineCode("/help command")} to
-          read up on it.`,
-          "",
-          `Have fun! Now go roll some dice!`,
-        ].join("\n"),
-      )
-    } catch (error) {
-      // this is not a critical message, so just log that we couldn't send it
-      logger.warn(error, "Could not send welcome message")
-    }
   },
 }
