1095c69f6453d2854dc37e4eef47edd3c84a9dd9
Paige Andrews <pmandrews@gmail.com>
2025-02-01 22:18:02 -0500

    Add logging for network errors

---
+++ events/shard-error.js
@@ -0,0 +1,9 @@
+const { Events } = require("discord.js")
+const { logger } = require("../util/logger")
+
+module.exports = {
+  name: Events.ShardError,
+  execute(error) {
+    logger.error(error, 'Websocket error')
+  },
+}
