9bd27e0d5f5dfdd26d8cc369aa17b9d71aeb5dbc
Paige Andrews <paige.andrews@modolabs.com>
2025-03-17 13:20:32 -0400

    Add describe wrapper to fate tests

--- src/commands/fate.test.js
+++ src/commands/fate.test.js
@@ -3,56 +3,58 @@ const fate_command = require("./fate")
 const { Interaction } = require("../../testing/interaction")
 const { test_secret_option } = require("../../testing/shared/execute-secret")
 
-var interaction
+describe("fate command", () => {
+  var interaction
 
-beforeEach(() => {
-  interaction = new Interaction()
-})
+  beforeEach(() => {
+    interaction = new Interaction()
+  })
 
-describe("judge", () => {
-  describe("with dominant outcome", () => {
-    it("returns the correct message", () => {
-      const results = [4]
+  describe("judge", () => {
+    describe("with dominant outcome", () => {
+      it("returns the correct message", () => {
+        const results = [4]
 
-      const result = fate_command.judge(results, "en-US")
+        const result = fate_command.judge(results, "en-US")
 
-      expect(result).toMatch("pleases")
+        expect(result).toMatch("pleases")
+      })
+    })
+
+    describe("with no dominant outcome", () => {
+      it("returns the neutral message", () => {
+        const results = [-4, 0, 4]
+
+        const result = fate_command.judge(results, "en-US")
+
+        expect(result).toMatch("noted")
+      })
     })
   })
 
-  describe("with no dominant outcome", () => {
-    it("returns the neutral message", () => {
-      const results = [-4, 0, 4]
+  describe("perform", () => {
+    it("displays the description if present", () => {
+      const description_text = "this is a test"
+      const options = {
+        description: description_text,
+      }
 
-      const result = fate_command.judge(results, "en-US")
+      const result = fate_command.perform(options)
 
-      expect(result).toMatch("noted")
+      expect(result).toMatch(description_text)
+    })
+
+    it("displays the modifier", () => {
+      interaction.command_options.modifier = 8
+      const options = {
+        modifier: 8,
+      }
+
+      const result = fate_command.perform(options)
+
+      expect(result).toMatch("8")
     })
   })
+
+  test_secret_option(fate_command)
 })
-
-describe("perform", () => {
-  it("displays the description if present", () => {
-    const description_text = "this is a test"
-    const options = {
-      description: description_text,
-    }
-
-    const result = fate_command.perform(options)
-
-    expect(result).toMatch(description_text)
-  })
-
-  it("displays the modifier", () => {
-    interaction.command_options.modifier = 8
-    const options = {
-      modifier: 8,
-    }
-
-    const result = fate_command.perform(options)
-
-    expect(result).toMatch("8")
-  })
-})
-
-test_secret_option(fate_command)
