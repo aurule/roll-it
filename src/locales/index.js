5592284bc8dd93fbd5b26f899185088086ac73b0
Paige Andrews <paige.andrews@modolabs.com>
2025-04-21 11:08:17 -0400

    Add new teamwork strings

--- src/locales/index.js
+++ src/locales/index.js
@@ -13,7 +13,7 @@ i18next.use(Backend).init({
   defaultLng: "en-US",
   supportedLngs: ["en-US", "en"],
   initAsync: false,
-  ns: ["translation", "commands", "teamwork", "opposed", "help", "modals"],
+  ns: ["translation", "commands", "teamwork", "opposed", "help", "modals", "interactive"],
   backend: {
     loadPath: join(__dirname, "./{{lng}}/{{ns}}.yaml"),
     addPath: join(__dirname, "./{{lng}}/{{ns}}.missing.yaml"),
