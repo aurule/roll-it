b3d5b263f94b4008fa5028e826183e2c4df28009
Paige Andrews <paige.andrews@modolabs.com>
2025-05-09 10:56:56 -0400

    Add initial tables

--- src/db/interactive.setup.sql
+++ src/db/interactive.setup.sql
@@ -41,3 +41,75 @@ ON teamwork_helpers (teamwork_id, user_uid);
 
 CREATE INDEX IF NOT EXISTS interactive.teamwork_helper_requested
 ON teamwork_helpers (teamwork_id, requested);
+
+-- Met Opposed Tables
+
+CREATE TABLE IF NOT EXISTS interactive.opposed (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  locale TEXT NOT NULL,
+  attacker_uid TEXT NOT NULL,
+  attribute TEXT NOT NULL,
+  retests_allowed BOOLEAN DEFAULT true,
+  retest_ability TEXT NOT NULL,
+  carrier BOOLEAN DEFAULT false,
+  altering BOOLEAN DEFAULT false,
+  channel_uid TEXT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  expires_at DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS interactive.opposed_participants (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_uid TEXT NOT NULL,
+  mention TEXT NOT NULL,
+  bomb BOOLEAN DEFAULT false,
+  ties BOOLEAN DEFAULT false,
+  cancels BOOLEAN DEFAULT false,
+  opposed_id INTEGER NOT NULL,
+  FOREIGN KEY (opposed_id)
+    REFERENCES opposed (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS interactive.opposed_tests (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  retester_uid TEXT,
+  retest_reason TEXT,
+  canceller_uid TEXT,
+  cancelled_with TEXT,
+  opposed_id INTEGER NOT NULL,
+  done BOOLEAN DEFAULT false,
+  FOREIGN KEY (opposed_id)
+    REFERENCES opposed (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS interactive.opposed_test_chops (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  request TEXT NOT NULL,
+  result TEXT,
+  participant_id INTEGER NOT NULL,
+  test_id INTEGER NOT NULL,
+  FOREIGN KEY (participant_id)
+    REFERENCES opposed_participants (id)
+    ON DELETE CASCADE
+  FOREIGN KEY (test_id)
+    REFERENCES opposed_tests (id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS interactive.opposed_messages (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  message_uid TEXT NOT NULL,
+  opposed_id INTEGER NOT NULL,
+  test_id INTEGER NOT NULL,
+  FOREIGN KEY (opposed_id)
+    REFERENCES opposed (id)
+    ON DELETE CASCADE
+  FOREIGN KEY (test_id)
+    REFERENCES opposed_tests (id)
+    ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS interactive.opposed_message_id
+ON opposed_messages (message_uid);
