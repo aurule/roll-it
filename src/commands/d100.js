e3901fa3a08234819a87ad32a7e261882e50c923
Paige Andrews <paige.andrews@modolabs.com>
2025-05-12 11:33:44 -0400

    Add pool to d10 and d100

--- src/commands/d100.js
+++ src/commands/d100.js
@@ -2,7 +2,8 @@ const Joi = require("joi")
 
 const { LocalizedSlashCommandBuilder } = require("../util/localized-command")
 const { roll } = require("../services/base-roller")
-const { present } = require("../presenters/results/singleton-results-presenter")
+const { sum } = require("../services/tally")
+const { present } = require("../presenters/results/roll-results-presenter")
 const commonOpts = require("../util/common-options")
 const commonSchemas = require("../util/common-schemas")
 const { injectMention } = require("../util/formatters")
@@ -15,6 +16,7 @@ module.exports = {
     new LocalizedSlashCommandBuilder(command_name)
       .addStringOption(commonOpts.description)
       .addLocalizedIntegerOption("modifier")
+      .addIntegerOption(commonOpts.pool)
       .addIntegerOption(commonOpts.rolls)
       .addBooleanOption(commonOpts.secret),
   savable: true,
@@ -22,28 +24,35 @@ module.exports = {
   schema: Joi.object({
     description: commonSchemas.description,
     modifier: commonSchemas.modifier,
+    pool: commonSchemas.pool,
     rolls: commonSchemas.rolls,
   }),
-  perform({ rolls = 1, modifier = 0, description, locale = "en-US" } = {}) {
-    const raw_results = roll(1, 100, rolls)
+  perform({ rolls = 1, modifier = 0, pool = 1, description, locale = "en-US" } = {}) {
+    const raw_results = roll(pool, 100, rolls)
+    const summed_results = sum(raw_results)
 
     return present({
       rolls,
+      pool,
+      sides: 100,
       modifier,
       description,
       raw: raw_results,
+      summed: summed_results,
       locale,
     })
   },
   execute(interaction) {
     const modifier = interaction.options.getInteger("modifier") ?? 0
     const rolls = interaction.options.getInteger("rolls") ?? 1
+    const pool = interaction.options.getInteger("pool") ?? 1
     const roll_description = interaction.options.getString("description") ?? ""
     const secret = interaction.options.getBoolean("secret") ?? false
 
     const partial_message = module.exports.perform({
       rolls,
       modifier,
+      pool,
       description: roll_description,
       locale: interaction.locale,
     })
