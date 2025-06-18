4433aac320263b9a093888a46546ac1c122afc9e
Paige Andrews <paige.andrews@modolabs.com>
2025-06-18 16:45:13 -0400

    Improve drh pool tests

--- src/util/rolls/drh-pool.test.js
+++ src/util/rolls/drh-pool.test.js
@@ -1,25 +1,50 @@
 const { DrhPool } = require("./drh-pool")
 
-describe("successes", () => {
-  it("tallies successes for 1-3", () => {
+describe("DrhPool", () => {
+  it("saves the name", () => {
+    const pool = new DrhPool("test", [[2, 3, 4]])
+
+    expect(pool.name).toEqual("test")
+  })
+
+  it("saves the raw dice", () => {
+    const pool = new DrhPool("test", [[2, 3, 4]])
+
+    expect(pool.raw).toEqual([[2, 3, 4]])
+  })
+
+  it("extracts the first roll", () => {
+    const pool = new DrhPool("test", [[2, 3, 4]])
+
+    expect(pool.dice).toEqual([2, 3, 4])
+  })
+
+  it("sums successes for 1-3", () => {
+    const pool = new DrhPool("test", [[2, 3, 4]])
+
+    expect(pool.summed).toEqual([2])
+  })
+
+  it("extracts successes for first roll", () => {
     const pool = new DrhPool("test", [[2, 3, 4]])
 
     expect(pool.successes).toEqual(2)
   })
-})
 
-describe("size", () => {
-  it("returns number of dice rolled", () => {
-    const pool = new DrhPool("test", [[2, 3, 4]])
+  describe("size", () => {
+    it("returns number of dice rolled", () => {
+      const pool = new DrhPool("test", [[2, 3, 4]])
 
-    expect(pool.size).toEqual(3)
+      expect(pool.size).toEqual(3)
+    })
   })
-})
 
-describe("spread", () => {
-  it("gets an array of die result counts", () => {
-    const pool = new DrhPool("test", [[2, 3, 4, 4, 5]])
+  describe("spread", () => {
+    it("gets an array of die result counts", () => {
+      const pool = new DrhPool("test", [[2, 3, 4, 4, 5]])
 
-    expect(pool.spread).toEqual([0, 0, 1, 1, 2, 1, 0])
+      expect(pool.spread).toEqual([0, 0, 1, 1, 2, 1, 0])
+    })
   })
+
 })
