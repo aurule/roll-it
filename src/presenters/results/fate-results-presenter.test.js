176eb0cc7a7dfcad63cd99f43377e5df87c5a1a8
Paige Andrews <paige.andrews@modolabs.com>
2025-06-23 16:50:56 -0400

    Wrap fate presenter tests in describe block

--- src/presenters/results/fate-results-presenter.test.js
+++ src/presenters/results/fate-results-presenter.test.js
@@ -1,90 +1,92 @@
 const FateResultsPresenter = require("./fate-results-presenter")
 const { i18n } = require("../../locales")
 
-describe("presentOne", () => {
-  const defaultArgs = {
-    raw: [[1, 1, 2, 3]],
-    summed: [-1],
-    modifier: 2,
-    t: i18n.getFixedT("en-US", "commands", "fate"),
-  }
+describe("fate results presenter", () => {
+  describe("presentOne", () => {
+    const defaultArgs = {
+      raw: [[1, 1, 2, 3]],
+      summed: [-1],
+      modifier: 2,
+      t: i18n.getFixedT("en-US", "commands", "fate"),
+    }
 
-  it("includes the ladder word for the sum", () => {
-    const result = FateResultsPresenter.presentOne(defaultArgs)
+    it("includes the ladder word for the sum", () => {
+      const result = FateResultsPresenter.presentOne(defaultArgs)
 
-    expect(result).toMatch("Average")
-  })
-
-  it("shows the sum", () => {
-    const result = FateResultsPresenter.presentOne(defaultArgs)
-
-    expect(result).toMatch("+1")
-  })
-
-  it("includes description if present", () => {
-    const result = FateResultsPresenter.presentOne({
-      description: "test roll",
-      ...defaultArgs,
+      expect(result).toMatch("Average")
     })
 
-    expect(result).toMatch('"test roll"')
-  })
-})
+    it("shows the sum", () => {
+      const result = FateResultsPresenter.presentOne(defaultArgs)
 
-describe("presentMany", () => {
-  const defaultArgs = {
-    raw: [
-      [1, 2, 3, 3],
-      [2, 1, 3, 2],
-    ],
-    summed: [2, 0],
-    modifier: 5,
-    t: i18n.getFixedT("en-US", "commands", "fate"),
-  }
-
-  it("highlights final sum", () => {
-    const result = FateResultsPresenter.presentMany(defaultArgs)
-
-    expect(result).toMatch("+7")
-  })
-
-  it("includes the ladder word for the final sum", () => {
-    const result = FateResultsPresenter.presentMany(defaultArgs)
-
-    expect(result).toMatch("Epic")
-  })
-
-  it("includes description if present", () => {
-    const result = FateResultsPresenter.presentMany({
-      description: "test roll",
-      ...defaultArgs,
+      expect(result).toMatch("+1")
     })
 
-    expect(result).toMatch('"test roll"')
-  })
-})
+    it("includes description if present", () => {
+      const result = FateResultsPresenter.presentOne({
+        description: "test roll",
+        ...defaultArgs,
+      })
 
-describe("detail", () => {
-  const default_raw = [1, 1, 2, 3]
-  const default_modifier = 2
-
-  it("shows the breakdown of the dice", () => {
-    const result = FateResultsPresenter.detail(default_raw, default_modifier)
-
-    expect(result).toMatch("fateneg")
-    expect(result).toMatch("fatezero")
-    expect(result).toMatch("fatepos")
+      expect(result).toMatch('"test roll"')
+    })
   })
 
-  it("shows the modifier if non-zero", () => {
-    const result = FateResultsPresenter.detail(default_raw, default_modifier)
+  describe("presentMany", () => {
+    const defaultArgs = {
+      raw: [
+        [1, 2, 3, 3],
+        [2, 1, 3, 2],
+      ],
+      summed: [2, 0],
+      modifier: 5,
+      t: i18n.getFixedT("en-US", "commands", "fate"),
+    }
 
-    expect(result).toMatch(" + 2")
+    it("highlights final sum", () => {
+      const result = FateResultsPresenter.presentMany(defaultArgs)
+
+      expect(result).toMatch("+7")
+    })
+
+    it("includes the ladder word for the final sum", () => {
+      const result = FateResultsPresenter.presentMany(defaultArgs)
+
+      expect(result).toMatch("Epic")
+    })
+
+    it("includes description if present", () => {
+      const result = FateResultsPresenter.presentMany({
+        description: "test roll",
+        ...defaultArgs,
+      })
+
+      expect(result).toMatch('"test roll"')
+    })
   })
 
-  it("excludes modifier if zero", () => {
-    const result = FateResultsPresenter.detail(default_raw, 0)
+  describe("detail", () => {
+    const default_raw = [1, 1, 2, 3]
+    const default_modifier = 2
 
-    expect(result).not.toMatch(" + ")
+    it("shows the breakdown of the dice", () => {
+      const result = FateResultsPresenter.detail(default_raw, default_modifier)
+
+      expect(result).toMatch("fateneg")
+      expect(result).toMatch("fatezero")
+      expect(result).toMatch("fatepos")
+    })
+
+    it("shows the modifier if non-zero", () => {
+      const result = FateResultsPresenter.detail(default_raw, default_modifier)
+
+      expect(result).toMatch(" + 2")
+    })
+
+    it("excludes modifier if zero", () => {
+      const result = FateResultsPresenter.detail(default_raw, 0)
+
+      expect(result).not.toMatch(" + ")
+    })
   })
 })
