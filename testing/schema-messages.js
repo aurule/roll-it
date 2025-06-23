c10ff5b8eef245995aa86c2196ac4375d2a33cf4
Paige Andrews <paige.andrews@modolabs.com>
2025-06-23 11:14:32 -0400

    Remove src import

--- testing/schema-messages.js
+++ testing/schema-messages.js
@@ -1,12 +1,12 @@
-const { schemaErrors } = require("../util/schema-errors")
-
 module.exports = {
   /**
    * Small testing helper to turn schema error results into a single string
+   *
    * @param  {object} schema_result Joi validation object
    * @return {str}                  String of all error details
    */
   schemaMessages(schema_result) {
-    return schemaErrors(schema_result).join("\n")
+    const errors = schema_result.error ?? { details: [] }
+    return errors.details.map((d) => d.message).join("\n")
   },
 }
