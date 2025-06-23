4334e586740d812b2fabca732f9a80719c66d6e6
Paige Andrews <paige.andrews@modolabs.com>
2025-06-23 11:05:39 -0400

    Improve schema-errors tests

--- src/util/schema-errors.js
+++ src/util/schema-errors.js
@@ -1,4 +1,9 @@
 module.exports = {
+  /**
+   * Extract the readable error messages from Joi validation results
+   * @param  {object} schema_result Joi validation result
+   * @return {str[]}                Array of error descriptions
+   */
   schemaErrors(schema_result) {
     const errors = schema_result.error ?? { details: [] }
     return errors.details.map((d) => d.message)
--- src/util/schema-errors.test.js
+++ src/util/schema-errors.test.js
@@ -1,14 +1,40 @@
 const { schemaErrors } = require("./schema-errors")
 
-it("includes all errors", () => {
-  const fake_validation = {
-    error: {
-      details: [{ message: "test1" }, { message: "test2" }],
-    },
-  }
+describe("schemaErrors", () => {
+  describe("with errors", () => {
+    it("extracts the error message", () => {
+      const fake_validation = {
+        error: {
+          details: [{ message: "test1" }],
+        },
+      }
 
-  const result = schemaErrors(fake_validation)
+      const result = schemaErrors(fake_validation)
 
-  expect(result).toContain("test1")
-  expect(result).toContain("test2")
+      expect(result).toContain("test1")
+    })
+
+    it("includes all errors", () => {
+      const fake_validation = {
+        error: {
+          details: [{ message: "test1" }, { message: "test2" }],
+        },
+      }
+
+      const result = schemaErrors(fake_validation)
+
+      expect(result).toContain("test1")
+      expect(result).toContain("test2")
+    })
+  })
+
+  describe("with no errors", () => {
+    it("returns an empty array", () => {
+      const fake_validation = {}
+
+      const result = schemaErrors(fake_validation)
+
+      expect(result).toEqual([])
+    })
+  })
 })
