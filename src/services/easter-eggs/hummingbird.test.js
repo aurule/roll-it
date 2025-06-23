714c11186c8917e4c9db2d0b5af2b9d89b04a13d
Paige Andrews <paige.andrews@modolabs.com>
2025-06-23 11:58:28 -0400

    Add missing hummingbird test

--- src/services/easter-eggs/hummingbird.test.js
+++ src/services/easter-eggs/hummingbird.test.js
@@ -1,41 +1,51 @@
 const hummingbird = require("./hummingbird")
 
-describe("hasTrigger", () => {
-  it("returns true when one trigger appears", () => {
-    const message = "perception"
+describe("hummingbird", () => {
+  describe("hasTrigger", () => {
+    it("returns true when one trigger appears", () => {
+      const message = "perception"
 
-    const result = hummingbird.hasTrigger(message)
+      const result = hummingbird.hasTrigger(message)
 
-    expect(result).toEqual(true)
+      expect(result).toEqual(true)
+    })
+
+    it("returns true when multiple triggers appears", () => {
+      const message = "perceiving for a look"
+
+      const result = hummingbird.hasTrigger(message)
+
+      expect(result).toEqual(true)
+    })
+
+    it("returns false with no triggers", () => {
+      const message = "something else"
+
+      const result = hummingbird.hasTrigger(message)
+
+      expect(result).toEqual(false)
+    })
   })
 
-  it("returns true when multiple triggers appears", () => {
-    const message = "perceiving for a look"
+  describe("qualified", () => {
+    it("returns true with 11 successes", () => {
+      const result = hummingbird.qualified(11)
 
-    const result = hummingbird.hasTrigger(message)
+      expect(result).toEqual(true)
+    })
 
-    expect(result).toEqual(true)
+    it("returns false with other successes", () => {
+      const result = hummingbird.qualified(10)
+
+      expect(result).toEqual(false)
+    })
   })
 
-  it("returns false with no triggers", () => {
-    const message = "something else"
+  describe("spotted", () => {
+    it("returns the localized message", () => {
+      const result = hummingbird.spotted("en-US")
 
-    const result = hummingbird.hasTrigger(message)
-
-    expect(result).toEqual(false)
-  })
-})
-
-describe("qualified", () => {
-  it("returns true with 11 successes", () => {
-    const result = hummingbird.qualified(11)
-
-    expect(result).toEqual(true)
-  })
-
-  it("returns false with other successes", () => {
-    const result = hummingbird.qualified(10)
-
-    expect(result).toEqual(false)
+      expect(result).toMatch("You saw the hummingbird")
+    })
   })
 })
