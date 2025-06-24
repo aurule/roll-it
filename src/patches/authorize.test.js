232a8105f3d9f53003b3db9d24c89631e814d09e
Paige Andrews <paige.andrews@modolabs.com>
2025-06-24 11:07:05 -0400

    Add authorize helper tests

---
+++ src/patches/authorize.test.js
@@ -0,0 +1,57 @@
+const authorize = require("./authorize")
+
+const {
+  ButtonInteraction,
+  UserSelectMenuInteraction,
+  StringSelectMenuInteraction,
+} = require("discord.js")
+
+class PatchMeAuthorize {
+  user
+
+  constructor(user_id) {
+    this.user = {
+      id: user_id,
+    }
+  }
+
+  reply(args) {
+    return args
+  }
+}
+
+describe("authorize helper", () => {
+  describe("patch", () => {
+    it.each([
+      [ButtonInteraction],
+      [UserSelectMenuInteraction],
+      [StringSelectMenuInteraction],
+    ])("patches %p by default", (klass) => {
+      authorize.patch()
+
+      expect(klass.prototype.authorize).not.toBeUndefined()
+    })
+  })
+
+  describe("authorize", () => {
+    beforeAll(() => {
+      authorize.patch(PatchMeAuthorize)
+    })
+
+    describe("with an allowed user", () => {
+      it("is a noop", () => {
+        const fake = new PatchMeAuthorize("test user")
+
+        expect(() => fake.authorize("test user", "also me")).not.toThrow()
+      })
+    })
+
+    describe("with a non-allowed user", () => {
+      it("throws an error", () => {
+        const fake = new PatchMeAuthorize("test user")
+
+        expect(() => fake.authorize("only me")).toThrow("not allowed")
+      })
+    })
+  })
+})
