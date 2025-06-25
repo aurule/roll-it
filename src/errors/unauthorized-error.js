dae04aa84b897a749e35431c23caf550cdbafb3b
Paige Andrews <paige.andrews@modolabs.com>
2025-06-25 15:27:54 -0400

    Add allowed uids to unauthorized error

--- src/errors/unauthorized-error.js
+++ src/errors/unauthorized-error.js
@@ -3,10 +3,18 @@
  */
 class UnauthorizedError extends Error {
   interaction
+  allowed_uids
 
-  constructor(interaction) {
+  /**
+   * Create a new UnauthorizedError
+   * @param  {Interaction} interaction  Interaction that triggered the error
+   * @param  {Snowflake[]} allowed_uids Array of Discord user IDs that are allowed
+   * @return {UnauthorizedError}        New UnauthorizedError object
+   */
+  constructor(interaction, allowed_uids) {
     super(`User ${interaction.user.id} is not allowed`)
     this.interaction = interaction
+    this.allowed_uids = allowed_uids
   }
 }
 
