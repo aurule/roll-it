43194a0e010b20bf7afbb5814430e8079871f500
Paige Andrews <paige.andrews@modolabs.com>
2025-06-25 16:07:29 -0400

    Test core mention dispatching

--- src/mentions/index.js
+++ src/mentions/index.js
@@ -6,7 +6,21 @@ const { jsNoTests, noDotFiles } = require("../util/filters")
 const basename = path.basename(__filename)
 const handlersDir = __dirname
 
+/**
+ * Array of message mention handlers
+ *
+ * @type {Handler[]}
+ */
 const handlers = []
+
+/**
+ * Standard mention handler
+ *
+ * This is meant to be a fallback option if no other handler can take a mention. It implements some easter
+ * eggs for fun.
+ *
+ * @type {Handler}
+ */
 const fallbackHandler = require("./fallback")
 
 const contents = fs
@@ -25,8 +39,19 @@ contents.forEach((mention_file) => {
 module.exports = {
   handlers,
   fallbackHandler,
-  async handle(message) {
-    for (const handler of handlers) {
+  /**
+   * Handle a message mention
+   *
+   * This can come from someone replying to a message from Roll It, or by mentioning Roll It explicitly within
+   * a message's text.
+   *
+   * @param  {Message} message             Message mentioning the bot user
+   * @param  {Handler[]} handlers_override Array of mention handler objects
+   * @return {Promise}                     Promise resolving to the outcome of the handler, usually a Message object
+   */
+  async handle(message, handlers_override) {
+    const our_handlers = handlers_override ?? handlers
+    for (const handler of our_handlers) {
       if (handler.canHandle(message)) return handler.handle(message)
     }
     return fallbackHandler.handle(message)
---
+++ src/mentions/index.test.js
@@ -0,0 +1,44 @@
+const fallback_handler = require("./fallback")
+
+const { handle } = require("./index")
+
+describe("message mention dispatching", () => {
+  describe("handle", () => {
+    it("calls the first handler that can take the message", async () => {
+      const handler1 = {
+        canHandle: interaction => true,
+        handle: interaction => "one"
+      }
+      const handler2 = {
+        canHandle: interaction => true,
+        handle: interaction => "two"
+      }
+
+      const result = await handle({}, [handler2, handler2])
+
+      expect(result).toEqual("two")
+    })
+
+    it("calls the fallback handler if nothing else takes the message", async () => {
+      const handler1 = {
+        canHandle: interaction => false,
+        handle: interaction => "one"
+      }
+      const handler2 = {
+        canHandle: interaction => false,
+        handle: interaction => "two"
+      }
+      const fallback_spy = jest.spyOn(fallback_handler, "handle")
+
+      await handle({
+        author: {id: "test_user"},
+        mentions: {
+          users: []
+        },
+        reply: () => false,
+      }, [handler2, handler2])
+
+      expect(fallback_spy).toHaveBeenCalled()
+    })
+  })
+})
