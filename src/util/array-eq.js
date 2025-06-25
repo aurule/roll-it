e1f63387f2dc234574c02bebe902b535fd7fca55
Paige Andrews <paige.andrews@modolabs.com>
2025-06-25 10:43:24 -0400

    Document arrayeq

--- src/util/array-eq.js
+++ src/util/array-eq.js
@@ -1,4 +1,17 @@
 module.exports = {
+  /**
+   * Determine whether two arrays are equal
+   *
+   * Arrays `a` and `b` are considered equal if they are the same object, OR all of the following are true:
+   *
+   * - Neither one is `null`-ish
+   * - Both have the same length
+   * - Every element in `a` appears in `b` at the same index
+   *
+   * @param  {Array<any>?} a First array to compare
+   * @param  {Array<any>?} b Second array to compare
+   * @return {bool}          True if the arrays are equal, false if not
+   */
   arrayEq(a, b) {
     if (a === b) return true
     if (a == null || b == null) return false
