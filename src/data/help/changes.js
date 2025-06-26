25de4747da942818a72cb2858e420b99e60a63a9
Paige Andrews <paige.andrews@modolabs.com>
2025-06-26 10:53:58 -0400

    Test changes data

--- src/data/help/changes.js
+++ src/data/help/changes.js
@@ -1,9 +1,23 @@
+/**
+ * Provides data used to show the "changes" help topic
+ */
+
 const fs = require("fs")
 const path = require("path")
 
 const { version } = require("../../../package.json")
 const { i18n } = require("../../locales")
 
+/**
+ * Read the changelog file for a given version
+ *
+ * This looks in the root `/changelog` folder for files. If no file matches the given version, a translated
+ * string is returned instead.
+ *
+ * @param  {str} changelog_version Roll It version string
+ * @param  {str} locale            Locale key for the missing changelog string
+ * @return {Buffer|str}            Changelog text or missing changelog string
+ */
 function getChangelog(changelog_version, locale) {
   try {
     return fs.readFileSync(path.join(__dirname, "../../../changelog", `${changelog_version}.md`))
--- src/data/help/changes.test.js
+++ src/data/help/changes.test.js
@@ -1,16 +1,32 @@
 const changesTopic = require("./changes")
 const { version } = require("../../../package.json")
 
-describe("getChangelog", () => {
-  it("reads changelog file when present", () => {
-    const changelog = changesTopic.getChangelog("1.0.0", "en-US").toString()
+describe("changes help topic", () => {
+  describe("getChangelog", () => {
+    it("reads changelog file when present", () => {
+      const changelog = changesTopic.getChangelog("1.0.0", "en-US").toString()
 
-    expect(changelog).toMatch("official stable release")
+      expect(changelog).toMatch("official stable release")
+    })
+
+    it("returns no changelog message when no changelog exists", () => {
+      const changelog = changesTopic.getChangelog("0.5.0", "en-US")
+
+      expect(changelog).toMatch("No changelog available for 0.5.0")
+    })
   })
 
-  it("returns no changelog message when no changelog exists", () => {
-    const changelog = changesTopic.getChangelog("0.5.0", "en-US")
+  describe("help_data", () => {
+    it("includes the current version", () => {
+      const data = changesTopic.help_data("en-US")
 
-    expect(changelog).toMatch("No changelog available for 0.5.0")
+      expect(data.version).toEqual(version)
+    })
+
+    it("includes the latest changelog", () => {
+      const data = changesTopic.help_data("en-US")
+
+      expect(data.changelog.toString()).toMatch("Changelog for Roll It")
+    })
   })
 })
