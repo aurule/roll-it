5ac23c1ec1f988ae39c94def6d048a8589eaa459
Paige Andrews <paige.andrews@modolabs.com>
2025-06-26 11:48:36 -0400

    Add basic met test

---
+++ src/commands/met.test.js
@@ -0,0 +1,21 @@
+const { Interaction } = require("../../testing/interaction")
+
+const met_command = require("./met")
+
+let interaction
+
+beforeEach(() => {
+  interaction = new Interaction()
+})
+
+describe("/met command", () => {
+  describe("execute", () => {
+    it("delegates to subcommand", async () => {
+      interaction.command_options.subcommand_name = "static"
+
+      await met_command.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("rolled")
+    })
+  })
+})
