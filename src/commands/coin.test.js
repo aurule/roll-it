b858336fdce251f970856809d64c3e79c642b41b
Paige Andrews <paige.andrews@modolabs.com>
2025-06-26 11:05:52 -0400

    Add coin sacrifice test

--- src/commands/coin.test.js
+++ src/commands/coin.test.js
@@ -4,95 +4,107 @@ const { Interaction } = require("../../testing/interaction")
 const { schemaMessages } = require("../../testing/schema-messages")
 const { test_secret_option } = require("../../testing/shared/execute-secret")
 
-describe("execute", () => {
-  let interaction
+describe("/coin command", () => {
+  describe("execute", () => {
+    let interaction
 
-  beforeEach(() => {
-    interaction = new Interaction()
-  })
-
-  it("performs the roll", async () => {
-    const result = await coin_command.execute(interaction)
-
-    expect(result.content).toMatch("flipped a coin")
-  })
-
-  test_secret_option(coin_command)
-})
-
-describe("judge", () => {
-  it("returns empty string with no call", () => {
-    const result = coin_command.judge([[1]], "", "en-US")
-
-    expect(result).toEqual("")
-  })
-
-  it("returns good message when call matches result", () => {
-    const result = coin_command.judge([[1]], "1", "en-US")
-
-    expect(result).toMatch("accepted")
-  })
-
-  it("returns bad message when call does not match result", () => {
-    const result = coin_command.judge([[1]], "2", "en-US")
-
-    expect(result).toMatch("inadequate")
-  })
-})
-
-describe("perform", () => {
-  it("displays the description if present", () => {
-    const options = {
-      description: "this is a test",
-    }
-
-    const result = coin_command.perform(options)
-
-    expect(result).toMatch(options.description)
-  })
-
-  it("displays the call if present", () => {
-    const options = {
-      call: "1",
-    }
-
-    const result = coin_command.perform(options)
-
-    expect(result).toMatch("called *heads*")
-  })
-})
-
-describe("schema", () => {
-  describe("call", () => {
-    it("is optional", () => {
-      const options = {}
-      const result = coin_command.schema.validate(options, {
-        abortEarly: false,
-      })
-
-      expect(schemaMessages(result)).not.toMatch("call")
+    beforeEach(() => {
+      interaction = new Interaction()
     })
 
-    it.each([["1"], ["2"]])("allows %s", (call_value) => {
-      const options = {
-        call: call_value,
-      }
-      const result = coin_command.schema.validate(options, {
-        abortEarly: false,
-      })
+    it("performs the roll", async () => {
+      const result = await coin_command.execute(interaction)
 
-      expect(schemaMessages(result)).not.toMatch("call")
+      expect(result.content).toMatch("flipped a coin")
     })
 
-    it("disallows other values", () => {
+    test_secret_option(coin_command)
+  })
+
+  describe("judge", () => {
+    it("returns empty string with no call", () => {
+      const result = coin_command.judge([[1]], "", "en-US")
+
+      expect(result).toEqual("")
+    })
+
+    it("returns good message when call matches result", () => {
+      const result = coin_command.judge([[1]], "1", "en-US")
+
+      expect(result).toMatch("accepted")
+    })
+
+    it("returns bad message when call does not match result", () => {
+      const result = coin_command.judge([[1]], "2", "en-US")
+
+      expect(result).toMatch("inadequate")
+    })
+  })
+
+  describe("perform", () => {
+    it("displays the description if present", () => {
       const options = {
-        call: "nopealope",
+        description: "this is a test",
       }
-      const result = coin_command.schema.validate(options, {
-        abortEarly: false,
+
+      const result = coin_command.perform(options)
+
+      expect(result).toMatch(options.description)
+    })
+
+    it("displays the call if present", () => {
+      const options = {
+        call: "1",
+      }
+
+      const result = coin_command.perform(options)
+
+      expect(result).toMatch("called *heads*")
+    })
+
+    it("displays sacrifice easter egg", () => {
+      const options = {
+        description: "sacrificing a chicken",
+      }
+
+      const result = coin_command.perform(options)
+
+      expect(result).toMatch("Your sacrifice")
+    })
+  })
+
+  describe("schema", () => {
+    describe("call", () => {
+      it("is optional", () => {
+        const options = {}
+        const result = coin_command.schema.validate(options, {
+          abortEarly: false,
+        })
+
+        expect(schemaMessages(result)).not.toMatch("call")
       })
 
-      expect(schemaMessages(result)).toMatch("must be")
+      it.each([["1"], ["2"]])("allows %s", (call_value) => {
+        const options = {
+          call: call_value,
+        }
+        const result = coin_command.schema.validate(options, {
+          abortEarly: false,
+        })
+
+        expect(schemaMessages(result)).not.toMatch("call")
+      })
+
+      it("disallows other values", () => {
+        const options = {
+          call: "nopealope",
+        }
+        const result = coin_command.schema.validate(options, {
+          abortEarly: false,
+        })
+
+        expect(schemaMessages(result)).toMatch("must be")
+      })
     })
   })
 })
