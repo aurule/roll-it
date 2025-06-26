1c1ff83f0df8b1ee45ebb6fa751d025103e6c7bc
Paige Andrews <paige.andrews@modolabs.com>
2025-06-26 11:40:41 -0400

    Wrap help tests in describe block

--- src/commands/help.test.js
+++ src/commands/help.test.js
@@ -3,43 +3,45 @@ const eightball_command = require("./8ball")
 
 const { Interaction } = require("../../testing/interaction")
 
-describe("execute", () => {
-  describe("topic subcommand", () => {
-    it("shows the topic", async () => {
-      const interaction = new Interaction()
-      interaction.command_options.subcommand_name = "topic"
-      interaction.command_options.topic = "about"
+describe("/help command", () => {
+  describe("execute", () => {
+    describe("topic subcommand", () => {
+      it("shows the topic", async () => {
+        const interaction = new Interaction()
+        interaction.command_options.subcommand_name = "topic"
+        interaction.command_options.topic = "about"
 
-      await help_command.execute(interaction)
+        await help_command.execute(interaction)
 
-      expect(interaction.replyContent).toMatch("passion project")
+        expect(interaction.replyContent).toMatch("passion project")
+      })
+    })
+
+    describe("command subcommand", () => {
+      it("shows the command", async () => {
+        const interaction = new Interaction()
+        interaction.command_options.subcommand_name = "command"
+        interaction.command_options.command = "8ball"
+        interaction.client.commands.set("8ball", eightball_command)
+
+        await help_command.execute(interaction)
+
+        expect(interaction.replyContent).toMatch("Get an answer")
+      })
     })
   })
 
-  describe("command subcommand", () => {
-    it("shows the command", async () => {
-      const interaction = new Interaction()
-      interaction.command_options.subcommand_name = "command"
-      interaction.command_options.command = "8ball"
-      interaction.client.commands.set("8ball", eightball_command)
+  describe("help_data", () => {
+    it("includes topic names", () => {
+      const help_data = help_command.help_data({ locale: "en-US" })
 
-      await help_command.execute(interaction)
-
-      expect(interaction.replyContent).toMatch("Get an answer")
+      expect(help_data.topics.some((t) => t.includes("About Roll It"))).toBeTruthy()
     })
-  })
-})
 
-describe("help", () => {
-  it("includes topic names", () => {
-    const help_data = help_command.help_data({ locale: "en-US" })
+    it("includes command names", () => {
+      const help_data = help_command.help_data({ locale: "en-US" })
 
-    expect(help_data.topics.some((t) => t.includes("About Roll It"))).toBeTruthy()
-  })
-
-  it("includes command names", () => {
-    const help_data = help_command.help_data({ locale: "en-US" })
-
-    expect(help_data.commands.some((c) => c.includes("8ball"))).toBeTruthy()
+      expect(help_data.commands.some((c) => c.includes("8ball"))).toBeTruthy()
+    })
   })
 })
