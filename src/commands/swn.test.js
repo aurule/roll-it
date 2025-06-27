25491eef39a07b4eacf2296dc631b1e74100fc4b
Paige Andrews <paige.andrews@modolabs.com>
2025-06-27 11:17:55 -0400

    Add swn sacrifice tests

--- src/commands/swn.test.js
+++ src/commands/swn.test.js
@@ -10,70 +10,89 @@ beforeEach(() => {
   interaction = new Interaction()
 })
 
-describe("schema", () => {
-  describe("pool", () => {
-    const pool_schema = swn_command.schema.extract("pool")
+describe("swn command", () => {
+  describe("schema", () => {
+    describe("pool", () => {
+      const pool_schema = swn_command.schema.extract("pool")
 
-    it("has a min of 2", () => {
-      const result = pool_schema.validate(1)
+      it("has a min of 2", () => {
+        const result = pool_schema.validate(1)
 
-      expect(result.error).toBeTruthy()
-    })
-  })
-})
-
-describe("judge", () => {
-  describe("with dominant outcome", () => {
-    it("returns the correct message", () => {
-      const results = [11]
-
-      const result = swn_command.judge(results, "en-US")
-
-      expect(result).toMatch("pleases")
+        expect(result.error).toBeTruthy()
+      })
     })
   })
 
-  describe("with no dominant outcome", () => {
-    it("returns the neutral message", () => {
-      const results = [2, 7, 12]
+  describe("judge", () => {
+    describe("with dominant outcome", () => {
+      it.each([
+        [11, "pleases"],
+        [9, "accepted"],
+        [6, "noted"],
+        [4, "inadequate"],
+        [2, "angers"],
+      ])("returns correct text for %i", (die, text) => {
+        const results = [die]
 
-      const result = swn_command.judge(results, "en-US")
+        const result = swn_command.judge(results, "en-US")
 
-      expect(result).toMatch("noted")
-    })
-  })
-})
-
-describe("perform", () => {
-  it("displays the description if present", () => {
-    const options = {
-      description: "this is a test",
-      rolls: 1,
-    }
-
-    const result = swn_command.perform(options)
-
-    expect(result).toMatch("this is a test")
-  })
-})
-
-describe("execute", () => {
-  describe("with one roll", () => {
-    test_secret_option(swn_command, { rolls: 1 })
-  })
-
-  describe("with multiple rolls", () => {
-    beforeEach(() => {
-      interaction.command_options.rolls = 2
+        expect(result).toMatch(text)
+      })
     })
 
+    describe("with no dominant outcome", () => {
+      it("returns the neutral message", () => {
+        const results = [2, 7, 12]
+
+        const result = swn_command.judge(results, "en-US")
+
+        expect(result).toMatch("noted")
+      })
+    })
+  })
+
+  describe("perform", () => {
     it("displays the description if present", () => {
-      const description_text = "this is a test"
-      interaction.command_options.description = description_text
+      const options = {
+        description: "this is a test",
+        rolls: 1,
+      }
 
-      swn_command.execute(interaction)
+      const result = swn_command.perform(options)
 
-      expect(interaction.replyContent).toMatch(description_text)
+      expect(result).toMatch("this is a test")
+    })
+
+    it("displays the sacrifice easter egg if present", () => {
+      const options = {
+        description: "sacrificing",
+        rolls: 1,
+      }
+
+      const result = swn_command.perform(options)
+
+      expect(result).toMatch("Your sacrifice")
+    })
+  })
+
+  describe("execute", () => {
+    describe("with one roll", () => {
+      test_secret_option(swn_command, { rolls: 1 })
+    })
+
+    describe("with multiple rolls", () => {
+      beforeEach(() => {
+        interaction.command_options.rolls = 2
+      })
+
+      it("displays the description if present", () => {
+        const description_text = "this is a test"
+        interaction.command_options.description = description_text
+
+        swn_command.execute(interaction)
+
+        expect(interaction.replyContent).toMatch(description_text)
+      })
     })
   })
 })
