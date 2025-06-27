5fcd1c4b958b21bfd398917cb89f11de36540e10
Paige Andrews <paige.andrews@modolabs.com>
2025-06-27 16:37:42 -0400

    Test the opposed test object

--- src/db/opposed/optest.js
+++ src/db/opposed/optest.js
@@ -8,22 +8,115 @@ const { Collection } = require("discord.js")
  * @class
  */
 class OpTest {
+  /**
+   * Database ID of our corresponding test record
+   * @type {int}
+   */
   id
+
+  /**
+   * Database ID of our parent challenge record
+   * @type {int}
+   */
   challenge_id
+
+  /**
+   * Locale code for rendering strings
+   * @type {str}
+   */
   locale
+
+  /**
+   * Database ID of the participant who retested this test
+   *
+   * @type {int}
+   */
   retester_id = null
+
+  /**
+   * Retest code given
+   * @type {str}
+   */
   retest_reason = null
+
+  /**
+   * Whether this test has been retested
+   * @type {Boolean}
+   */
   retested = false
+
+  /**
+   * Database ID of the participant who cancelled this test's retest
+   *
+   * @type {int}
+   */
   canceller_id = null
+
+  /**
+   * Cancel code given
+   * @type {str}
+   */
   cancelled_with = null
+
+  /**
+   * Whether this test's retest was cancelled
+   * @type {Boolean}
+   */
   cancelled = false
+
+  /**
+   * Displayable history text of this test
+   * @type {str}
+   */
   history = null
+
+  /**
+   * Displayable short breakdown of the test's result
+   * @type {str}
+   */
   breakdown = null
+
+  /**
+   * Database ID of the participant who is currently the winner of the test
+   *
+   * @type {int}
+   */
   leader_id = null
 
+  /**
+   * Database handler
+   * @type {Opposed}
+   */
   opposed_db
+
+  /**
+   * Collection of related database records
+   *
+   * This is used as a cache for lazy-loading related records.
+   *
+   * @type {Collection<str, obj>}
+   */
   records
 
+  /**
+   * Create a new OpTest
+   *
+   * @param  {object}  options
+   * @param  {int}     options.id             ID of the test record
+   * @param  {int}     options.challenge_id   ID of the parent challenge
+   * @param  {str}     options.locale         Locale code
+   * @param  {int}     options.retester_id    ID of the retesting participant
+   * @param  {str}     options.retest_reason  Retest code
+   * @param  {Boolean} options.retested       Whether the initial result was retested
+   * @param  {int}     options.canceller_id   ID of the cancelling participant
+   * @param  {str}     options.cancelled_with Cancel code
+   * @param  {Boolean} options.cancelled      Whether the retest was cancelled
+   * @param  {str}     options.history        Displayable test history
+   * @param  {str}     options.breakdown      Displayable result details
+   * @param  {int}     options.leader_id      ID of the winning participant
+   * @param  {Opposed} options.opposed_db     DB object
+   * @return {OpTest}                         New OpTest object
+   */
   constructor({
     id,
     challenge_id,
@@ -37,7 +130,7 @@ class OpTest {
     history = null,
     breakdown = null,
     leader_id = null,
-    opposed_db,
+    opposed_db
   } = {}) {
     this.id = id
     this.challenge_id = challenge_id
@@ -57,6 +150,12 @@ class OpTest {
     this.records = new Collection()
   }
 
+  /**
+   * Lazily load the related participant records
+   *
+   * @param  {str} key Key of the participant record to return
+   * @return {Participant|undefined} The named participant record, or undefined if not found
+   */
   populateParticipants(key) {
     const { ParticipantRoles } = require("../opposed")
     const participants = this.opposed_db.getParticipants(this.challenge_id, true)
@@ -78,30 +177,58 @@ class OpTest {
     return this.records.get(key)
   }
 
+  /**
+   * Participant who is currently winning this test
+   * @return {Participant} Leading participant record
+   */
   get leader() {
     return this.records.ensure("leader", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Participant who is currently losing this test
+   * @return {Participant} Trailing participant record
+   */
   get trailer() {
     return this.records.ensure("trailer", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Participant who retested the initial result
+   * @return {Participant} Retesting participant record
+   */
   get retester() {
     return this.records.ensure("retester", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Participant who cancelled the retest
+   * @return {Participant} Cancelling participant record
+   */
   get canceller() {
     return this.records.ensure("canceller", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Participant who initiated the overall challenge
+   * @return {Participant} Attacking participant record
+   */
   get attacker() {
     return this.records.ensure("attacker", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Participant who is the opponent of the overall challenge
+   * @return {Participant} Defending participant record
+   */
   get defender() {
     return this.records.ensure("defender", (key) => this.populateParticipants(key))
   }
 
+  /**
+   * Parent challenge record
+   * @return {Challenge} Challenge record
+   */
   get challenge() {
     return this.records.ensure("challenge", () => this.opposed_db.getChallenge(this.challenge_id))
   }
---
+++ src/db/opposed/optest.test.js
@@ -0,0 +1,179 @@
+const { Opposed, ParticipantRoles, ChallengeStates } = require("../opposed")
+
+const { OpTest } = require("./optest")
+
+describe("OpTest", () => {
+  let opposed_db
+
+  beforeEach(() => {
+    opposed_db = new Opposed()
+  })
+
+  describe("constructor", () => {
+    it("leaves associated records for lazy loading", () => {
+      const test_record = new OpTest({
+        id: 1,
+        challenge_id: 1,
+        locale: "en-US",
+        opposed_db,
+      })
+
+      expect(test_record.records.size).toEqual(0)
+    })
+  })
+
+  describe("related records", () => {
+    let challenge_id
+    let test_record
+    let attacker
+    let defender
+    const attacker_uid = "atk"
+    const defender_uid = "def"
+
+    beforeAll(() => {
+      let result = opposed_db.addChallenge({
+        locale: "en-US",
+        description: "testing challenge",
+        attacker_uid,
+        attribute: "mental",
+        retest_ability: "occult",
+        state: ChallengeStates.AdvantagesAttacker,
+        channel_uid: "testchan",
+        timeout: 1000,
+      })
+      challenge_id = result.lastInsertRowid
+
+      result = opposed_db.addParticipant({
+        challenge_id,
+        user_uid: attacker_uid,
+        mention: `<@${attacker_uid}>`,
+        role: ParticipantRoles.Attacker,
+        advantages: ["hi", "there"],
+      })
+      let attacker_id = result.lastInsertRowid
+      attacker = opposed_db.getParticipant(attacker_id)
+
+      result = opposed_db.addParticipant({
+        challenge_id,
+        user_uid: "def",
+        mention: `<@${defender_uid}>`,
+        role: ParticipantRoles.Defender,
+        advantages: ["oh", "no"],
+      })
+      let defender_id = result.lastInsertRowid
+      defender = opposed_db.getParticipant(defender_id)
+    })
+
+    beforeEach(() => {
+      let result = opposed_db.addTest({
+        challenge_id,
+        locale: "en-US",
+        attacker_ready: true,
+        defender_ready: true,
+        leader_id: attacker.id,
+        retester_id: defender.id,
+        canceller_id: attacker.id,
+      })
+      test_id = result.lastInsertRowid
+      test_record = opposed_db.getTest(test_id)
+    })
+
+    describe("populateParticipants", () => {
+      it("sets leader from own leader ID", () => {
+        expect(test_record.records.get("leader")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("leader").id).toEqual(attacker.id)
+      })
+
+      it("sets trailer from non-leader participant", () => {
+        expect(test_record.records.get("trailer")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("trailer").id).toEqual(defender.id)
+      })
+
+      it("sets retester from own retester ID", () => {
+        expect(test_record.records.get("retester")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("retester").id).toEqual(defender.id)
+      })
+
+      it("sets canceller from own canceller ID", () => {
+        expect(test_record.records.get("canceller")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("canceller").id).toEqual(attacker.id)
+      })
+
+      it("sets attacker based on role", () => {
+        expect(test_record.records.get("attacker")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("attacker").id).toEqual(attacker.id)
+      })
+
+      it("sets defender based on role", () => {
+        expect(test_record.records.get("defender")).toBeUndefined()
+
+        test_record.populateParticipants()
+
+        expect(test_record.records.get("defender").id).toEqual(defender.id)
+      })
+
+      it("returns the named participant record if asked", () => {
+        const participant = test_record.populateParticipants("attacker")
+
+        expect(participant.id).toEqual(attacker.id)
+      })
+    })
+
+    describe("leader", () => {
+      it("gets the leader record", () => {
+        expect(test_record.leader.id).toEqual(attacker.id)
+      })
+    })
+
+    describe("trailer", () => {
+      it("gets the trailer record", () => {
+        expect(test_record.trailer.id).toEqual(defender.id)
+      })
+    })
+
+    describe("retester", () => {
+      it("gets the retester record", () => {
+        expect(test_record.retester.id).toEqual(defender.id)
+      })
+    })
+
+    describe("canceller", () => {
+      it("gets the canceller record", () => {
+        expect(test_record.canceller.id).toEqual(attacker.id)
+      })
+    })
+
+    describe("attacker", () => {
+      it("gets the attacker record", () => {
+        expect(test_record.attacker.id).toEqual(attacker.id)
+      })
+    })
+
+    describe("defender", () => {
+      it("gets the defender record", () => {
+        expect(test_record.defender.id).toEqual(defender.id)
+      })
+    })
+
+    describe("challenge", () => {
+      it("gets the challenge record", () => {
+        expect(test_record.challenge.id).toEqual(challenge_id)
+      })
+    })
+  })
+})
