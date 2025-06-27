6c535610865c35004672a0763c0414f1b630100b
Paige Andrews <paige.andrews@modolabs.com>
2025-06-27 10:17:24 -0400

    Add tests for pba roll judging

--- src/commands/pba.test.js
+++ src/commands/pba.test.js
@@ -9,57 +9,65 @@ beforeEach(() => {
   interaction = new Interaction()
 })
 
-describe("judge", () => {
-  describe("with dominant outcome", () => {
-    it("returns the correct message", () => {
-      const results = [11]
+describe("pba command", () => {
+  describe("judge", () => {
+    describe("with dominant outcome", () => {
+      it.each([
+        [11, "pleases"],
+        [9, "accepted"],
+        [6, "noted"],
+        [4, "inadequate"],
+        [2, "angers"],
+      ])("returns correct text for %i", (die, text) => {
+        const results = [die]
 
-      const result = pba_command.judge(results, "en-US")
+        const result = pba_command.judge(results, "en-US")
 
-      expect(result).toMatch("pleases")
+        expect(result).toMatch(text)
+      })
+    })
+
+    describe("with no dominant outcome", () => {
+      it("returns the neutral message", () => {
+        const results = [2, 7, 12]
+
+        const result = pba_command.judge(results, "en-US")
+
+        expect(result).toMatch("noted")
+      })
     })
   })
 
-  describe("with no dominant outcome", () => {
-    it("returns the neutral message", () => {
-      const results = [2, 7, 12]
-
-      const result = pba_command.judge(results, "en-US")
-
-      expect(result).toMatch("noted")
-    })
-  })
-})
-
-describe("perform", () => {
-  it("displays the description if present", () => {
-    const options = {
-      description: "this is a test",
-    }
-
-    const result = pba_command.perform(options)
-
-    expect(result).toMatch("this is a test")
-  })
-})
-
-describe("execute", () => {
-  describe("with one roll", () => {
-    test_secret_option(pba_command, { rolls: 1 })
-  })
-
-  describe("with multiple rolls", () => {
-    beforeEach(() => {
-      interaction.command_options.rolls = 2
-    })
-
+  describe("perform", () => {
     it("displays the description if present", () => {
-      const description_text = "this is a test"
-      interaction.command_options.description = description_text
+      const options = {
+        description: "this is a test",
+      }
 
-      pba_command.execute(interaction)
+      const result = pba_command.perform(options)
 
-      expect(interaction.replyContent).toMatch(description_text)
+      expect(result).toMatch("this is a test")
+    })
+  })
+
+  describe("execute", () => {
+    describe("with one roll", () => {
+      test_secret_option(pba_command, { rolls: 1 })
+    })
+
+    describe("with multiple rolls", () => {
+      beforeEach(() => {
+        interaction.command_options.rolls = 2
+      })
+
+      it("displays the description if present", () => {
+        const description_text = "this is a test"
+        interaction.command_options.description = description_text
+
+        pba_command.execute(interaction)
+
+        expect(interaction.replyContent).toMatch(description_text)
+      })
     })
   })
 })
