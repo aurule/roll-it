05aade404a6a7b5aa03bb00e0a349f9afe2866f5
Paige Andrews <paige.andrews@modolabs.com>
2025-06-30 15:29:21 -0400

    Fix bad references in retest cancel button

--- src/components/opposed/cancel-button.js
+++ src/components/opposed/cancel-button.js
@@ -1,7 +1,7 @@
 const { ButtonBuilder, ButtonStyle } = require("discord.js")
 const { i18n } = require("../../locales")
 const { Opposed, ChallengeStates } = require("../../db/opposed")
-const { logger } = require("../../util/logger")
+const { makeHistory } = require("../../services/opposed/history")
 
 module.exports = {
   name: "opposed_cancel",
@@ -23,7 +23,6 @@ module.exports = {
       return interaction.ensure("whisper", t("missing"), {
         component: "opposed_cancel",
         test: test,
-        challenge: challenge,
         detail: "failed to whisper about missing cancel reason ",
       })
     }
@@ -37,38 +36,36 @@ module.exports = {
 
     const cancelling_message = require("../../messages/opposed/cancelling")
     await interaction
-      .ensure("edit", cancelling_message.inert(challenge.id), {
+      .ensure("edit", cancelling_message.inert(test.challenge_id), {
         component: "opposed_cancel",
         test: test,
-        challenge: challenge,
         detail: "failed to update cancelling message to remove controls",
       })
-      .catch((error) => {
+      .catch((_error) => {
         // suppress all errors so we can send other messages
         return
       })
 
     let next_message
     if (test.leader) {
-      opposed_db.setChallengeState(challenge.id, ChallengeStates.Winning)
+      opposed_db.setChallengeState(test.challenge_id, ChallengeStates.Winning)
       next_message = require("../../messages/opposed/winning")
     } else {
-      opposed_db.setChallengeState(challenge.id, ChallengeStates.Tying)
+      opposed_db.setChallengeState(test.challenge_id, ChallengeStates.Tying)
       next_message = require("../../messages/opposed/tying")
     }
 
     return interaction
-      .ensure("reply", next_message.data(challenge.id), {
+      .ensure("reply", next_message.data(test.challenge_id), {
         component: "opposed_cancel",
         test: test,
-        challenge: challenge,
         detail: `failed to send ${next_message.state} prompt`,
       })
       .then((reply_result) => {
         const message_uid = reply_result.resource.message.id ?? reply_result.id
 
         opposed_db.addMessage({
-          challenge_id: challenge.id,
+          challenge_id: test.challenge_id,
           message_uid,
           test_id: test.id,
         })
