48804163e97a7e495ba90a3dbd06ec093e30e8d3
Paige Andrews <paige.andrews@modolabs.com>
2025-07-09 11:25:35 -0400

    Wrap array eq tests in a describe block

--- src/util/array-eq.test.js
+++ src/util/array-eq.test.js
@@ -1,44 +1,47 @@
 const { arrayEq } = require("./array-eq")
 
-it("returns true for self", () => {
-  const arr1 = [1, 2, 3]
+describe("arrayEq helper", () => {
+  it("returns true for self", () => {
+    const arr1 = [1, 2, 3]
 
-  const result = arrayEq(arr1, arr1)
+    const result = arrayEq(arr1, arr1)
 
-  expect(result).toBeTruthy()
-})
+    expect(result).toBeTruthy()
+  })
 
-it("returns false if one is null", () => {
-  const arr1 = [1, 2, 3]
+  it("returns false if one is null", () => {
+    const arr1 = [1, 2, 3]
 
-  const result = arrayEq(arr1)
+    const result = arrayEq(arr1)
 
-  expect(result).toBeFalsy()
-})
+    expect(result).toBeFalsy()
+  })
 
-it("returns false if mismatched lengths", () => {
-  const arr1 = [1, 2, 3]
-  const arr2 = [1, 2, 3, 4]
+  it("returns false if mismatched lengths", () => {
+    const arr1 = [1, 2, 3]
+    const arr2 = [1, 2, 3, 4]
 
-  const result = arrayEq(arr1, arr2)
+    const result = arrayEq(arr1, arr2)
 
-  expect(result).toBeFalsy()
-})
+    expect(result).toBeFalsy()
+  })
 
-it("returns false if non-shared item", () => {
-  const arr1 = [1, 2, 3]
-  const arr2 = [1, 2, 4]
+  it("returns false if non-shared item", () => {
+    const arr1 = [1, 2, 3]
+    const arr2 = [1, 2, 4]
 
-  const result = arrayEq(arr1, arr2)
+    const result = arrayEq(arr1, arr2)
 
-  expect(result).toBeFalsy()
-})
+    expect(result).toBeFalsy()
+  })
 
-it("returns true with matching items", () => {
-  const arr1 = [1, 2, 3]
-  const arr2 = [1, 2, 3]
+  it("returns true with matching items", () => {
+    const arr1 = [1, 2, 3]
+    const arr2 = [1, 2, 3]
 
-  const result = arrayEq(arr1, arr2)
+    const result = arrayEq(arr1, arr2)
 
-  expect(result).toBeTruthy()
+    expect(result).toBeTruthy()
+  })
+
 })
