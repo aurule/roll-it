dacfe712a784288e6045aa60956df1fd3cc44243
Paige Andrews <paige.andrews@modolabs.com>
2025-07-15 16:13:08 -0400

    Add basic tests for opposed messages

---
+++ src/messages/opposed/index.test.js
@@ -0,0 +1,32 @@
+const Joi = require("joi")
+const { Challenge } = require("../../db/opposed/challenge")
+
+const messages = require("./index")
+
+const message_schema = Joi.object({
+  state: Joi.string().required().valid(...Object.values(Challenge.States)),
+  data: Joi.function().required(),
+  inert: Joi.function().optional(),
+  afterRetry: Joi.function().optional(),
+  handleReply: Joi.function().optional(),
+})
+
+describe("opposed messages", () => {
+  it("loads message files", () => {
+    expect(messages.size).toBeGreaterThan(0)
+  })
+
+  it("indexes messages by state name", () => {
+    expect(messages.get("withdrawn").state).toEqual("withdrawn")
+  })
+
+  it("excludes the index file", () => {
+    expect(messages.has(undefined)).toBeFalsy()
+  })
+
+  it.each(
+    messages.map(m => [m.state, m]),
+  )("%s message matches schema", (_state, message) => {
+    expect(message).toMatchSchema(message_schema)
+  })
+})
