314a01bc675865dec33197aad1dc76f5c7764c3d
Paige Andrews <paige.andrews@modolabs.com>
2025-07-15 16:22:39 -0400

    Report all schema errors

--- testing/matchers/match-schema.js
+++ testing/matchers/match-schema.js
@@ -6,18 +6,18 @@
  * @return {obj}            Jest matcher result object
  */
 function toMatchSchema(actual, schema) {
-  const result = schema.validate(actual, { errors: { wrap: { label: "`" } } })
+  const result = schema.validate(actual, { abortEarly: false, errors: { wrap: { label: "`" } } })
 
   if (result.error === undefined) {
     return {
       message: () =>
-        `expected ${this.utils.printReceived(actual)} not to match the schema. Errors:\n${result.error.details.map(d => `- ${this.utils.printExpected(d.message)}`)}`,
+        `expected ${this.utils.printReceived(actual)} not to match the schema. Errors:${result.error.details.map(d => `\n- ${this.utils.printExpected(d.message)}`)}`,
       pass: true,
     }
   } else {
     return {
       message: () =>
-        `expected ${this.utils.printReceived(actual)} to match the schema. Errors:\n${result.error.details.map(d => `- ${this.utils.printExpected(d.message)}`)}`,
+        `expected ${this.utils.printReceived(actual)} to match the schema. Errors:${result.error.details.map(d => `\n- ${this.utils.printExpected(d.message)}`)}`,
       pass: false,
     }
   }
