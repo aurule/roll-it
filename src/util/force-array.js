dd070d1c7f8418fad12f9a962dbd231976a5b737
Paige Andrews <paige.andrews@modolabs.com>
2025-07-23 13:28:09 -0400

    Improve force-array helper

--- src/util/force-array.js
+++ src/util/force-array.js
@@ -1,6 +1,15 @@
 module.exports = {
-  forceArray(obj) {
-    if (obj === null) return []
-    return Array.isArray(obj) ? obj : [obj]
+  /**
+   * Coerce a value into an array
+   *
+   * If `thing` is already an array, it is returned as-is. Null and undefined are turned into an empty array.
+   * All other non-array values are turned into an array containing `thing`.
+   *
+   * @param  {any}                 thing The thing to turn into an array
+   * @return {Array<typeof thing>}       Array of the thing
+   */
+  forceArray(thing) {
+    if (thing === null || thing === undefined) return []
+    return Array.isArray(thing) ? thing : [thing]
   },
 }
--- src/util/force-array.test.js
+++ src/util/force-array.test.js
@@ -1,19 +1,27 @@
 const { forceArray } = require("./force-array")
 
-it("converts a single entity into an array containing that entity", () => {
-  const result = forceArray(1)
+describe("forceArray", () => {
+  it("converts a single entity into an array containing that entity", () => {
+    const result = forceArray(1)
 
-  expect(result).toEqual([1])
-})
+    expect(result).toEqual([1])
+  })
 
-it("passes through an array", () => {
-  const result = forceArray([1])
+  it("passes through an array", () => {
+    const result = forceArray([1])
 
-  expect(result).toEqual([1])
-})
+    expect(result).toEqual([1])
+  })
 
-it("returns an empty array with null arg", () => {
-  const result = forceArray(null)
+  it("returns an empty array with null arg", () => {
+    const result = forceArray(null)
 
-  expect(result).toEqual([])
+    expect(result).toEqual([])
+  })
+
+  it("returns an empty array with undefined arg", () => {
+    const result = forceArray(undefined)
+
+    expect(result).toEqual([])
+  })
 })
