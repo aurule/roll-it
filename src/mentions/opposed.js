5ad8af978c68355205fd8bef6a3ebf2cae7d33f6
Paige Andrews <paige.andrews@modolabs.com>
2025-08-01 11:51:24 -0400

    Get retry keyword from i18n file

--- src/mentions/opposed.js
+++ src/mentions/opposed.js
@@ -1,8 +1,10 @@
 const { userMention } = require("discord.js")
 const { Opposed } = require("../db/opposed")
-const { i18n } = require("../locales")
+const { i18n, available_locales } = require("../locales")
 const { logger } = require("../util/logger")
 const message_contents = require("../messages/opposed")
+
+const RETRY_KEYWORDS = available_locales.map(locale => i18n.t("opposed.retry", { lng: locale, ns: "interactive" }))
 
 module.exports = {
   /**
@@ -17,6 +19,7 @@ module.exports = {
     const opposed_db = new Opposed()
     return opposed_db.hasMessage(interaction.reference?.messageId)
   },
+
   /**
    * Handle a message
    *
@@ -31,10 +34,10 @@ module.exports = {
     const opposed_db = new Opposed()
     const challenge = opposed_db.findChallengeByMessage(interaction.reference.messageId)
 
-    if (interaction.content === "retry") {
+    if (RETRY_KEYWORDS.includes(interaction.content)) {
       const message_file = message_contents.get(challenge.state)
       const message_data = message_file.data(challenge.id)
-      const afterReply = message_file.afterReply
+      const afterRetry = message_file.afterRetry
       return interaction
         .ensure("reply", message_data, {
           challenge_id: challenge.id,
@@ -48,8 +51,8 @@ module.exports = {
             test_id: opposed_db.findTestByMessage(interaction.reference.messageId)?.id ?? null,
           }
           opposed_db.addMessage(message_props)
-          if (afterReply !== undefined) {
-            afterReply(reply_interaction)
+          if (afterRetry !== undefined) {
+            afterRetry(reply_interaction)
           }
         })
     }
