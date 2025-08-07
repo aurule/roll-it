1b276f32d02ed6dc647240e9aa3ae3f9b9e30b60
Paige Andrews <paige.andrews@modolabs.com>
2025-08-07 11:59:58 -0400

    Fix incorrect type description

--- src/util/extract-number.js
+++ src/util/extract-number.js
@@ -2,7 +2,7 @@
  * Pull the first number out of the given content
  *
  * @param  {string}  content String to parse
- * @return {number?}         Extracted number, or null if one is not found
+ * @return {number?}         Extracted number, or undefined if one is not found
  */
 function extractNumber(content) {
   const match = content.replace(/\s/g, "").match(/-?\d+/)
