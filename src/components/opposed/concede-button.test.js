ead5f64ab9cc30b938d5c3f6d48f6434c1d6f472
Paige Andrews <paige.andrews@modolabs.com>
2025-08-28 15:20:51 -0400

    Test concede button

---
+++ src/components/opposed/concede-button.test.js
@@ -0,0 +1,68 @@
+jest.mock("../../util/message-builders")
+
+const { ChallengeFixture } = require("../../../testing/challenge-fixture")
+const { Interaction } = require("../../../testing/interaction")
+const { Challenge } = require("../../db/opposed/challenge")
+
+const concedeButton = require("./concede-button")
+
+describe("opposed test concede button", () => {
+  describe("data", () => {
+    it("has a sensible id", () => {
+      const button = concedeButton.data("en-US")
+
+      expect(button.data.custom_id).toMatch("concede")
+    })
+
+    it("has a label", () => {
+      const button = concedeButton.data("en-US")
+
+      expect(button.data.label).toMatch("Concede")
+    })
+  })
+
+  describe("execute", () => {
+    let interaction
+    let challenge
+
+    beforeEach(() => {
+      interaction = new Interaction()
+      challenge = new ChallengeFixture().withParticipants().attachMessage(interaction.message.id)
+      challenge.addDefenderWin()
+      interaction.user.id = challenge.attacker.uid
+      interaction.customId = "opposed_concede"
+    })
+
+    describe("authorization", () => {
+      describe("authorization", () => {
+        it("allows attacking user", async () => {
+          await expect(concedeButton.execute(interaction)).resolves.not.toThrow()
+        })
+
+        it("disallows all others", async () => {
+          interaction.user.id = "other"
+
+          await expect(concedeButton.execute(interaction)).rejects.toThrow()
+        })
+      })
+    })
+
+    it("edits message to winning inert", async () => {
+      await concedeButton.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("<@def> is currently winning")
+    })
+
+    it("sets state to conceded", async () => {
+      await concedeButton.execute(interaction)
+
+      expect(challenge.record.state).toEqual(Challenge.States.Conceded)
+    })
+
+    it("replies with concession message", async () => {
+      await concedeButton.execute(interaction)
+
+      expect(interaction.replyContent).toMatch("<@atk> conceded to <@def>")
+    })
+  })
+})
