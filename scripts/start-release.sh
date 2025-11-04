17983eb5546c1b57748bcecd5206116c7d199bfa
Paige Andrews <pmandrews@gmail.com>
2025-11-04 12:29:05 -0500

    Fix release script step to update docs coverpage

--- scripts/start-release.sh
+++ scripts/start-release.sh
@@ -7,7 +7,7 @@ git branch release/$1
 git checkout release/$1
 npm --no-git-tag-version version $1
 echo "...updated package.json"
-sed -i "" -E "s/<small>.*<\/small>/<small>$1<\/small>/" docs/_coverpage.md
+sed "s/<small>.*<\/small>/<small>$1<\/small>/" docs/_coverpage.md -i
 echo "...updated docs coverpage"
 git commit package.json package-lock.json docs/_coverpage.md -m "Update version to $1"
 yarn changelog:build
