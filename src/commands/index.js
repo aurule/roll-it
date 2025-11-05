2a9b01e85ebe9728c95193dd8d5488b768bd8182
Paige Andrews <pmandrews@gmail.com>
2025-11-05 16:31:01 -0500

    Remove unused filter from command loading

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,7 +4,7 @@ const { Collection } = require("@discordjs/collection")
 const { available_locales } = require("../locales")
 const { comparator } = require("../util/command-sorter")
 
-const { jsNoTests, noDotFiles } = require("../util/filters")
+const { jsNoTests } = require("../util/filters")
 
 const basename = path.basename(__filename)
 const commandsDir = __dirname
@@ -35,7 +35,6 @@ commands.sorted.teamworkable = new Collection()
 const contents = fs
   .readdirSync(commandsDir)
   .filter(jsNoTests)
-  .filter(noDotFiles)
   .filter((file) => {
     return file !== basename
   })
