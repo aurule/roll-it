4cba6f4a8e0839e1d0d2eaac4e3763caf53e2a74
Paige Andrews <pmandrews@gmail.com>
2025-11-10 15:55:56 -0500

    Do not log timing for guild add/remove events

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -3,6 +3,10 @@ const { logger } = require("../util/logger")
 
 module.exports = {
   name: Events.GuildCreate,
+  logMe: false,
+  logContext(_args) {
+    return {}
+  },
   async execute(guild) {
     logger.info({ id: guild.id, name: guild.name }, `Added to guild`)
   },
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -3,6 +3,10 @@ const { logger } = require("../util/logger")
 
 module.exports = {
   name: Events.GuildDelete,
+  logMe: false,
+  logContext(_args) {
+    return {}
+  },
   execute(guild) {
     logger.info({ id: guild.id, name: guild.name }, `Removed from guild`)
   },
