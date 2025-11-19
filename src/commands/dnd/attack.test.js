bea2b64be9e1bf3eb46daac80489fd1973e0d825
Paige Andrews <pmandrews@gmail.com>
2025-11-18 19:58:57 -0500

    Add dnd attack command tests

---
+++ src/commands/dnd/attack.test.js
@@ -0,0 +1,41 @@
+const { Interaction } = require("../../../testing/interaction")
+const { test_secret_option } = require("../../../testing/shared/execute-secret")
+
+const dnd_attack_command = require("./attack")
+
+describe("/dnd save", () => {
+  describe("perform", () => {
+    it("rolls a single result", () => {
+      const result = dnd_attack_command.perform({})
+
+      expect(result).toMatch("1d20")
+    })
+
+    it("rolls multiple results", () => {
+      const result = dnd_attack_command.perform({
+        rolls: 2,
+      })
+
+      expect(result).toMatch("2 attacks")
+    })
+
+    it("shows description if present", () => {
+      const result = dnd_attack_command.perform({
+        description: "a test",
+      })
+
+      expect(result).toMatch("a test")
+    })
+  })
+
+  describe("execute", () => {
+    var interaction
+
+    beforeEach(() => {
+      interaction = new Interaction()
+      interaction.command_options.subcommand_name = "attack"
+    })
+
+    test_secret_option(dnd_attack_command)
+  })
+})
