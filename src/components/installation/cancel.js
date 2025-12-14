32302bd2cc4631ace4764c14486fcf6923f599b8
Paige Andrews <pmandrews@gmail.com>
2025-12-13 22:14:53 -0500

    Add outline of cancel button

---
+++ src/components/installation/cancel.js
@@ -0,0 +1,17 @@
+const { ButtonBuilder, ButtonStyle } = require("discord.js")
+const { i18n } = require("../../locales")
+
+module.exports = {
+  name: "install_cancel",
+  valid_states: ["summary"],
+  data: (locale) => {
+    const t = i18n.getFixedT(locale, "install", "shared.components.cancel")
+    return new ButtonBuilder()
+      .setCustomId("install_cancel")
+      .setLabel(t("text"))
+      .setStyle(ButtonStyle.Danger)
+  },
+  async execute(interaction) {
+    //
+  }
+}
