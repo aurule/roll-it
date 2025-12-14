4bce46dd3c3c56c2f2b3990fc83f7f3b69d6cf64
Paige Andrews <pmandrews@gmail.com>
2025-12-13 22:07:05 -0500

    Add basic installation db methods

--- src/db/installation.js
+++ src/db/installation.js
@@ -1,7 +1,124 @@
+const { oneLine } = require("common-tags");
 const { CachedDb } = require("./cached-db");
 
 class Installation extends CachedDb {
-  // x
+  /**
+   * Create a new installation record
+   *
+   * @param  {object}    options
+   * @param  {string}    options.locale    Locale code
+   * @param  {Snowflake} options.guild_uid Discord guild ID
+   * @param  {Snowflake} options.user_uid  Discord user ID
+   * @param  {string}    options.state     State of the installation
+   * @param  {object}    options.old_deets Object of existing systems, features, and commands
+   * @param  {object}    options.new_deets Object of to-set systems, features, and commands
+   * @param  {number}    options.timeout   Number of seconds before the installation times out
+   * @return {Info}      Query info object with `changes` and `lastInsertRowid` properties
+   */
+  addInstallation({
+    locale,
+    guild_uid,
+    user_uid,
+    state,
+    old_deets = {},
+    new_deets = {},
+    timeout,
+  } = {}) {
+    const insert = this.prepared(
+      "addInstallation",
+      oneLine`
+      INSERT INTO interactive.installation_processes (
+        locale,
+        guild_uid,
+        user_uid,
+        state,
+        old_deets,
+        new_deets,
+        expires_at
+      ) VALUES (
+        @locale,
+        @guild_uid,
+        @user_uid,
+        @state,
+        JSONB(@old_deets),
+        JSONB(@new_deets),
+        DATETIME('now', @timeout || ' seconds')
+      )
+    `,
+    )
+
+    return insert.run({
+      locale,
+      guild_uid,
+      user_uid,
+      state,
+      old_deets: JSON.stringify(old_deets),
+      new_deets: JSON.stringify(new_deets),
+      timeout,
+    })
+  }
+
+  /**
+   * Get the total number of installation records
+   * @return {number} Total number of installation records
+   */
+  installationCount() {
+    const select = this.prepared(
+      "installationCount",
+      oneLine`
+        SELECT COUNT(1)
+        FROM   interactive.installation_processes
+      `,
+      true)
+
+    return select.get()
+  }
+
+  /**
+   * Remove an installation record
+   *
+   * @param  {number} id Internal ID of the installation record
+   * @return {Info}      Query info object with `changes` and `lastInsertRowid` properties
+   */
+  destroy(id) {
+    const destroy = this.prepared(
+      "destroy",
+      oneLine`
+        DELETE FROM interactive.installation_processes WHERE id = ?
+      `,
+    )
+
+    return destroy.run(id)
+  }
+
+  /**
+   * Get an installation record
+   *
+   * @param  {number} id Internal ID of the installation record
+   * @return {object}    Installation object
+   */
+  getInstallation(id) {
+    const select = this.prepared(
+      "getInstallation",
+      oneLine`
+        SELECT *,
+               JSON_EXTRACT(old_deets, '$') AS old_deets,
+               JSON_EXTRACT(new_deets, '$') AS new_deets,
+               DATETIME('now') > DATETIME(expires_at) AS expired
+        FROM   interactive.installation_processes
+        WHERE  id = ?
+      `)
+
+    const raw_out = select.get(id)
+
+    if (raw_out === undefined) return undefined
+
+    // this should probably be a specialty object
+    return {
+      ...raw_out,
+      expired: !!raw_out.expired,
+    }
+  }
 }
 
 module.exports = {
--- src/db/installation.test.js
+++ src/db/installation.test.js
@@ -3,6 +3,9 @@ const { makeDB } = require("./index")
 const { Installation } = require("./installation")
 
 describe("Installation DB", () => {
+  /**
+   * @type Installation
+   */
   let installation
   let db
 
@@ -11,5 +14,102 @@ describe("Installation DB", () => {
     installation = new Installation(db)
   })
 
-  it.todo("has methods")
+  describe("addInstallation", () => {
+    it("creates a record", () => {
+      installation.addInstallation({
+        locale: "en-US",
+        guild_uid: "guild",
+        user_uid: "user",
+        state: "start",
+        timeout: 1000,
+      })
+
+      expect(installation.installationCount()).toEqual(1)
+    })
+  })
+
+  describe("installationCount", () => {
+    it("counts total installation records", () => {
+      installation.addInstallation({
+        locale: "en-US",
+        guild_uid: "guild",
+        user_uid: "user",
+        state: "start",
+        timeout: 1000,
+      })
+      installation.addInstallation({
+        locale: "en-US",
+        guild_uid: "guild",
+        user_uid: "user",
+        state: "start",
+        timeout: 1000,
+      })
+
+      expect(installation.installationCount()).toEqual(2)
+    })
+  })
+
+  describe("destroy", () => {
+    it("removes the record", () => {
+      const installation_id = installation.addInstallation({
+        locale: "en-US",
+        guild_uid: "guild",
+        user_uid: "user",
+        state: "start",
+        timeout: 1000,
+      }).lastInsertRowid
+
+      installation.destroy(installation_id)
+
+      expect(installation.installationCount()).toEqual(0)
+    })
+  })
+
+  describe("getInstallation", () => {
+    let installation_id
+
+    beforeEach(() => {
+      installation_id = installation.addInstallation({
+        locale: "en-US",
+        guild_uid: "guild",
+        user_uid: "user",
+        state: "start",
+        timeout: 1000,
+      }).lastInsertRowid
+    })
+
+    it("returns undefined for bad id", () => {
+      const result = installation.getInstallation(789)
+
+      expect(result).toBeUndefined()
+    })
+
+    it("gets the record", () => {
+      const result = installation.getInstallation(installation_id)
+
+      expect(result.guild_uid).toEqual("guild")
+    })
+
+    describe("expired flag", () => {
+      it("is false when expires_at is in future", () => {
+        const result = installation.getInstallation(installation_id)
+
+        expect(result.expired).toEqual(false)
+      })
+
+      it("is true when expires_at is in past", () => {
+        installation_id = installation.addInstallation({
+          locale: "en-US",
+          guild_uid: "guild",
+          user_uid: "user",
+          state: "start",
+          timeout: -1000,
+        }).lastInsertRowid
+
+        const result = installation.getInstallation(installation_id)
+
+        expect(result.expired).toEqual(true)
+      })
+    })
+  })
 })
