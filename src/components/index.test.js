2a8bc16fdfb0f7b6d5c476690f1732fac7049bb0
Paige Andrews <pmandrews@gmail.com>
2025-12-13 22:49:23 -0500

    Test that all components have a unique name

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,4 +1,4 @@
-const { handle } = require("./index")
+const { handle, handlers } = require("./index")
 
 describe("component dispatching", () => {
   describe("handle", () => {
@@ -30,6 +30,15 @@ describe("component dispatching", () => {
       const result = await handle({}, [handler1, handler2])
 
       expect(result).toEqual(false)
+    })
+  })
+
+  describe("components", () => {
+    it("each has a unique ID", () => {
+      const all_handler_names = handlers.flatMap(h => h.components.map(c => c.name))
+      const unique_handler_names = new Set(all_handler_names)
+
+      expect(all_handler_names).toHaveLength(unique_handler_names.size)
     })
   })
 })
