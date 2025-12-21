a331c2772927e5a89928e33a55736b179ec837a0
Paige Andrews <pmandrews@gmail.com>
2025-12-21 17:33:19 -0500

    Add system finder service

---
+++ src/services/system-helpers.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * Find systems that use the given commands
+   *
+   * Systems are returned only if the given command_names are a superset of that system's required commands.
+   *
+   * @param  {...string} command_names Array of command names
+   * @return {System[]}                Array of system objects
+   */
+  findByCommands(...command_names) {
+    const name_set = new Set(command_names)
+    const { systems } = require("../data")
+
+    return systems.filter(s => name_set.isSupersetOf(new Set(s.commands.required)))
+  }
+}
---
+++ src/services/system-helpers.test.js
@@ -0,0 +1,90 @@
+const systemHelpers = require("./system-helpers")
+
+describe("system helpers", () => {
+  describe("findbyCommands", () => {
+    describe("system with one required command", () => {
+      it("includes if command is in array", () => {
+        const command_names = ["dnd", "d20"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).toContain("dnd35")
+      })
+
+      it("excludes if command is not in array", () => {
+        const command_names = ["fate"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("dnd35")
+      })
+
+      it("excludes if only recommended is in array", () => {
+        const command_names = ["d10"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("dnd35")
+      })
+
+      it("excludes if only optional is in array", () => {
+        const command_names = ["table"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("dnd35")
+      })
+    })
+
+    describe("system with multiple required commands", () => {
+      it("includes if all commands are in array", () => {
+        const command_names = ["ffrpg", "d10", "formula", "d20"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).toContain("ffrpg")
+      })
+
+      it("excludes if only some commands are in array", () => {
+        const command_names = ["ffrpg", "d10"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("ffrpg")
+      })
+
+      it("excludes if no commands are in array", () => {
+        const command_names = ["d20"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("ffrpg")
+      })
+
+      it("excludes if only recommended is in array", () => {
+        const command_names = ["d100"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("ffrpg")
+      })
+
+      it("excludes if only optional is in array", () => {
+        const command_names = ["table"]
+
+        const result = systemHelpers.findByCommands(...command_names)
+
+        const system_names = result.map(r => r.name)
+        expect(system_names).not.toContain("generic")
+      })
+    })
+  })
+})
