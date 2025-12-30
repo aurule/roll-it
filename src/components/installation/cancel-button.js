a5341d44d9b6ce620a0a8ff6dee745e3218755d4
Paige Andrews <pmandrews@gmail.com>
2025-12-30 16:00:51 -0500

    Implement install cancel button

--- src/components/installation/cancel-button.js
+++ src/components/installation/cancel-button.js
@@ -1,5 +1,7 @@
 const { ButtonBuilder, ButtonStyle } = require("discord.js")
 const { i18n } = require("../../locales")
+const { Installation } = require("../../db/installation")
+const cancelled = require("../../messages/installation/cancelled")
 
 module.exports = {
   name: "install_cancel",
@@ -11,6 +13,27 @@ module.exports = {
       .setStyle(ButtonStyle.Danger)
   },
   async execute(interaction) {
-    //
+    const install_db = new Installation()
+    const install = install_db.findInstallationByMessage(interaction.message.id)
+
+    interaction.authorize(install.user_uid)
+
+    install_db.finishInstallation(install.id)
+
+    interaction.message
+      .delete()
+      .catch((_error) => {
+        // suppress all errors so we can send other messages
+        return
+      })
+
+    interaction.ensure(
+      "reply",
+      cancelled.data(install.id),
+      {
+        install,
+        detail: "failed to send cancellation message",
+      }
+    )
   }
 }
---
+++ src/messages/installation/cancelled.js
@@ -0,0 +1,18 @@
+const { Installation } = require("../../db/installation")
+const { i18n } = require("../../locales")
+const build = require("../../util/message-builders")
+
+/**
+ * Message shown upon cancelling an install process
+ */
+module.exports = {
+  data: (installation_id) => {
+    const install_db = new Installation()
+    const install = install_db.getInstallation(installation_id)
+    const locale = install.locale
+
+    const t = i18n.getFixedT(locale, "install")
+
+    return build.textMessage(t("cancelled"))
+  }
+}
