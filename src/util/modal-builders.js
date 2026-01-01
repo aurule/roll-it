d033bf10e291b0fd9de09e79b85a8c861bc4dffb
Paige Andrews <pmandrews@gmail.com>
2026-01-01 16:46:07 -0500

    Add new modal builders

---
+++ src/util/modal-builders.js
@@ -0,0 +1,64 @@
+const {
+  TextDisplayBuilder,
+  LabelBuilder,
+  ModalBuilder,
+} = require("discord.js")
+
+/**
+ * Module with helpers to more fluidly build modals, their components, and related json structures
+ *
+ * These helpers make use of the discord.js builder classes under the hood, while providing a more concise api
+ * for most use cases.
+ */
+module.exports = {
+  /**
+   * Create modal data
+   *
+   * Modals are limited to five top-level components.
+   *
+   * @param  {string}             customId   ID of the modal
+   * @param  {string}             title      Title to show in the modal's header bar
+   * @param  {ComponentBuilder[]} components Array of components to include in the modal
+   * @return {Object}             Object of modal data
+   */
+  modal(customId, title, components) {
+    return new ModalBuilder({
+      customId,
+      title,
+      components,
+    })
+  },
+
+  /**
+   * Create a text display component
+   *
+   * @param  {string}             content The text to display
+   * @return {TextDisplayBuilder}         Text builder object
+   */
+  text(content) {
+    return new TextDisplayBuilder().setContent(content)
+  },
+
+  /**
+   * Create a labelled input component
+   *
+   * Modals need all input objects wrapped in a label to provide a title and
+   * description. The fields are both tightly limited in length. If there's
+   * more to say, use a text display for it.
+   *
+   * @param  {ComponentBuilder} component   The component to wrap
+   * @param  {string}           label_text  Label to show for the component. Max 45 characters.
+   * @param  {string}           description Descriptive text for the component. Max 100 characters.
+   * @return {LabelBuilder}                 Label builder object
+   */
+  label(component, label_text, description) {
+    const label = new LabelBuilder({
+      label: label_text,
+      component,
+    })
+
+    if (description) label.setDescription(description)
+
+    return label
+  }
+}
