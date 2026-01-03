867f8a0f86f152d2559f588b0c495b1e4b9a1e4d
Paige Andrews <pmandrews@gmail.com>
2026-01-03 14:20:41 -0500

    Remove duplicate command from curv system

--- src/data/systems/curv.js
+++ src/data/systems/curv.js
@@ -3,6 +3,6 @@ module.exports = {
   commands: {
     required: ["curv"],
     recommended: ["formula", "d4", "d6", "d8", "d10", "d12", "d20", "d100"],
-    optional: ["curv", "table"],
+    optional: ["table"],
   },
 }
