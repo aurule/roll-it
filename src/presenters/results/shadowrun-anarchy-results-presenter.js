f181320d0080d1ec20847117b229f872e97ec484
Paige Andrews <pmandrews@gmail.com>
2026-01-10 20:15:54 -0500

    Add sra presenter

---
+++ src/presenters/results/shadowrun-anarchy-results-presenter.js
@@ -0,0 +1,226 @@
+const { strikethrough, bold } = require("discord.js")
+
+const { i18n } = require("../../locales")
+
+/**
+ * Class to more conveniently handle the complex presentation logic for a shadowrun anarchy roll
+ *
+ * The presenter is designed to handle a single roll or set of identical rolls. It must not be reused for
+ * different rolls.
+ */
+class ShadowrunAnarchyPresenter {
+  _glitches = undefined
+
+  /**
+   * Create a new ShadowrunAnarchyPresenter object
+   *
+   * @param {number}     options.pool        Total dice in each roll
+   * @param {number}     options.threshold   Threshold for a success
+   * @param {number}     options.risk        Number of dice in the pool that are risked
+   * @param {number}     options.rolls       Total number of rolls made
+   * @param {number}     options.until       Target number of successes from multiple rolls
+   * @param {string}     options.description Text describing the roll
+   * @param {number[][]} options.raw         Array of one array with ints representing raw dice rolls
+   * @param {number[]}   options.summed      Array of one int, summing the rolled dice
+   * @param {string}     options.locale      Name of the locale to use to look up stings
+   */
+  constructor({
+    pool,
+    threshold,
+    risk,
+    rolls,
+    until,
+    description,
+    raw,
+    summed,
+    locale = "en-US",
+  } = {}) {
+    this.pool = pool
+    this.threshold = threshold
+    this.risk = risk
+    this.rolls = rolls
+    this.until = until
+    this.description = description
+    this.raw = raw
+    this.summed = summed
+    this.t = i18n.getFixedT(locale, "commands", "sra")
+  }
+
+  /**
+   * Present the rolled results
+   * @return {string} String of described results
+   */
+  presentResults() {
+    const t_args = {
+      count: this.raw.length,
+      context: this.description ? "description" : undefined,
+      description: this.description,
+      pool: this.explainPool(),
+    }
+
+    let layout
+    if (this.until) {
+      layout = this.rolls ? "until.max" : "until.plain"
+      t_args.results = this.raw.map((_roll, idx) => {
+        const o_args = {
+          count: this.summed[idx],
+          context: this.glitches[idx] ? "glitch" : undefined,
+          glitch: this.glitchString(this.glitches[idx]),
+        }
+        const r_args = {
+          outcome: this.t("response.outcome", o_args),
+          detail: this.detail(idx),
+        }
+        return this.t("response.layout.result", r_args)
+      })
+      t_args.target = this.t("response.layout.until.target", { count: this.until })
+      t_args.sum = this.t("response.layout.until.sum", {
+        sum: this.summed.reduce((acc, sum) => acc + sum, 0),
+        count: this.until,
+      })
+      t_args.rolls = this.t("response.layout.until.rolls", { count: this.raw.length })
+    } else if (this.rolls > 1) {
+      layout = "many"
+      t_args.results = this.raw.map((_roll, idx) => {
+        const o_args = {
+          count: this.summed[idx],
+          context: this.glitches[idx] ? "glitch" : undefined,
+          glitch: this.glitchString(this.glitches[idx]),
+        }
+        const r_args = {
+          outcome: this.t("response.outcome", o_args),
+          detail: this.detail(idx),
+        }
+        return this.t("response.layout.result", r_args)
+      })
+    } else {
+      layout = "single"
+      const o_args = {
+        count: this.summed[0],
+        context: this.glitches[0] ? "glitch" : undefined,
+        glitch: this.glitchString(this.glitches[0]),
+      }
+      t_args.outcome = this.t("response.outcome", o_args)
+      t_args.detail = this.detail(0)
+    }
+
+    return this.t(`response.layout.${layout}`, t_args)
+  }
+
+  /**
+   * Explain the dice pool
+   * @return {string} Breakdown of the pool's parameters
+   */
+  explainPool() {
+    const key = this.risk ? "risking" : "plain"
+
+    const t_args = {
+      context: this.advantage,
+      dice: this.t("response.pool.dice", { count: this.pool }),
+      risk: this.t("response.pool.risk", { count: this.risk }),
+    }
+
+    return this.t(`response.pool.${key}`, t_args)
+  }
+
+  /**
+   * Get a string describing (dis)advantage state
+   * @return {string?} One of "advantage" or "disadvantage", or undefined
+   */
+  get advantage() {
+    switch(this.threshold) {
+      case 4:
+        return "advantage"
+      case 6:
+        return "disadvantage"
+      default:
+        return undefined
+    }
+  }
+
+  /**
+   * Get the glitch total for all rolls
+   * @return {number[]} Array of glitch totals
+   */
+  get glitches() {
+    if (this._glitches === undefined) {
+      this._glitches = this.raw.map((roll) => roll.slice(0, this.risk).filter((r) => r === 1).length)
+    }
+
+    return this._glitches
+  }
+
+  /**
+   * Convert a glitch count into a string
+   *
+   * Always empty for zero glitches.
+   *
+   * @param  {number} glitch Total number of glitch dice
+   * @return {string}        Translated glitch string
+   */
+  glitchString(glitch) {
+    switch(glitch) {
+      case 0:
+        return ""
+      case 1:
+        return this.t("response.glitch.minor")
+      case 2:
+        return this.t("response.glitch.major")
+      default:
+        return this.t("response.glitch.critical")
+    }
+  }
+
+  /**
+   * [detail description]
+   *
+   * The roll [1, 6, 3, 1, 5] is formatted as
+   * ```md
+   * __*1*, **6!**__, 3, 1, **5**
+   * ```
+   *
+   * This makes use of the non-standard underline formatting that Discord
+   * supplies to ensure that the risked dice are highlighted.
+   *
+   * @param  {number} roll_idx Index of the roll to format
+   * @return {string}          Formatted dice
+   */
+  detail(roll_idx) {
+    const risked = []
+    const regular = []
+
+    for (const [idx, die] of this.raw[roll_idx].entries()) {
+        if (idx < this.risk) {
+            switch(true) {
+                case die >= this.threshold:
+                    risked.push(`**${die}!**`)
+                    break
+                case die === 1:
+                    risked.push(`*1*`)
+                    break
+                default:
+                    risked.push(`${die}`)
+                    break
+            }
+            continue
+        }
+        if (die >= this.threshold) {
+            regular.push(`**${die}**`)
+        } else {
+            regular.push(`${die}`)
+        }
+    }
+    switch (true) {
+        case !this.risk:
+            return regular.join(", ")
+        case this.risk === this.pool:
+            return `__${risked.join(", ")}__`
+        default:
+            return `__${risked.join(", ")}__, ${regular.join(", ")}`
+    }
+  }
+}
+
+module.exports = {
+  ShadowrunAnarchyPresenter,
+}
---
+++ src/presenters/results/shadowrun-anarchy-results-presenter.test.js
@@ -0,0 +1,456 @@
+const { ShadowrunAnarchyPresenter } = require("./shadowrun-anarchy-results-presenter")
+
+describe("Shadowrun Anarchy presenter", () => {
+  const defaults = {
+    pool: 5,
+    threshold: 5,
+    raw: [[1, 2, 3, 4, 5]],
+    summed: [1],
+    risk: 0,
+    rolls: 1,
+    until: 0,
+    description: "",
+  }
+
+  describe("presentResults", () => {
+    describe("rolling until a target", () => {
+      const until_defaults = {
+        ...defaults,
+        until: 1,
+      }
+
+      it("shows the successes for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("- **1 success**")
+      })
+
+      it("shows the dice for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("3, 4")
+      })
+
+      it("shows the glitch for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...until_defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("minor glitch")
+      })
+
+      it("includes the user", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("userMention")
+      })
+
+      it("includes description if present", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...until_defaults,
+          description: "desc",
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("desc")
+      })
+
+      it("describes the pool", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("5 dice")
+      })
+
+      it("describes total rolls made", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("1 roll")
+      })
+
+      it("describes risked dice", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...until_defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("risking 2")
+      })
+
+      it("describes target", () => {
+        const presenter = new ShadowrunAnarchyPresenter(until_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("until 1 success")
+      })
+
+      it("describes max rolls if given", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...until_defaults,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("up to 1 time")
+      })
+    })
+    describe("with multiple rolls", () => {
+      const multi_defaults = {
+        ...defaults,
+        raw: [[2, 4, 6], [1, 6, 5]],
+        summed: [1, 2],
+        rolls: 2,
+        pool: 3,
+      }
+
+      it("shows the successes for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter(multi_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("- **1 success**")
+        expect(result).toMatch("- **2 successes**")
+      })
+
+      it("shows the dice for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter(multi_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("2, 4")
+        expect(result).toMatch("1, **6**")
+      })
+
+      it("shows the glitch for each roll", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...multi_defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("minor glitch")
+      })
+
+      it("includes the user", () => {
+        const presenter = new ShadowrunAnarchyPresenter(multi_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("userMention")
+      })
+
+      it("includes the description if present", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...multi_defaults,
+          description: "desc"
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("desc")
+      })
+
+      it("describes the pool", () => {
+        const presenter = new ShadowrunAnarchyPresenter(multi_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("3 dice")
+      })
+
+      it("describes the total rolls", () => {
+        const presenter = new ShadowrunAnarchyPresenter(multi_defaults)
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("2 times")
+      })
+
+      it("describes risked dice", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...multi_defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("risking 2")
+      })
+    })
+
+    describe("with a single roll", () => {
+      it("shows the successes", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("1 success")
+      })
+
+      it("shows the dice", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("2, 3")
+      })
+
+      it("includes the user", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("{{userMention}}")
+      })
+
+      it("includes a glitch if present", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("minor glitch")
+      })
+
+      it("includes description if present", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+          description: "desc",
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("desc")
+      })
+
+      it("describes the dice pool", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("5 dice")
+      })
+
+      it("describes the risked dice", () => {
+        const presenter = new ShadowrunAnarchyPresenter({
+          ...defaults,
+          risk: 2,
+        })
+
+        const result = presenter.presentResults()
+
+        expect(result).toMatch("risking 2")
+      })
+    })
+  })
+
+  describe("explainPool", () => {
+    it("shows dice total", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+      })
+
+      const result = presenter.explainPool()
+
+      expect(result).toMatch("5 dice")
+    })
+
+    it("shows risked dice", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        risk: 2,
+      })
+
+      const result = presenter.explainPool()
+
+      expect(result).toMatch("risking 2")
+    })
+
+    it("shows advantage", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        threshold: 4,
+      })
+
+      const result = presenter.explainPool()
+
+      expect(result).toMatch("with advantage")
+    })
+
+    it("shows disadvantage", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        threshold: 6,
+      })
+
+      const result = presenter.explainPool()
+
+      expect(result).toMatch("with disadvantage")
+    })
+  })
+
+  describe("detail", () => {
+    it("adds exclamation to success die", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 4]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("**6!**")
+    })
+
+    it("italicizes glitch die", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 4]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("*1*")
+    })
+
+    it("includes normal die", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 4]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("3,")
+    })
+
+    it("bolds success die", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 5]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("**5**")
+    })
+
+    it("underlines risked dice", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 5]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("__**6!**, *1*__")
+    })
+
+    it("does not format non-risked 1s", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 1, 5]],
+        risk: 2,
+      })
+
+      const result = presenter.detail(0)
+
+      expect(result).toMatch("1,")
+    })
+  })
+
+  describe("glitches", () => {
+    it("includes 1s below the risk threshold", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 1, 2, 3, 5]],
+        risk: 2,
+      })
+
+      expect(presenter.glitches).toEqual([1])
+    })
+
+    it("ignores 1s after the risk threshold", () => {
+      const presenter = new ShadowrunAnarchyPresenter({
+        ...defaults,
+        raw: [[6, 2, 2, 3, 1]],
+        risk: 2,
+      })
+
+      expect(presenter.glitches).toEqual([0])
+    })
+  })
+
+  describe("glitchString", () => {
+    it("returns empty string with zero glitches", () => {
+      const presenter = new ShadowrunAnarchyPresenter(defaults)
+
+      const result = presenter.glitchString(0)
+
+      expect(result).toEqual("")
+    })
+
+    it("returns minor glitch for 1", () => {
+      const presenter = new ShadowrunAnarchyPresenter(defaults)
+
+      const result = presenter.glitchString(1)
+
+      expect(result).toMatch("minor")
+    })
+
+    it("returns major glitch for 1", () => {
+      const presenter = new ShadowrunAnarchyPresenter(defaults)
+
+      const result = presenter.glitchString(2)
+
+      expect(result).toMatch("major")
+    })
+
+    it("returns critical glitch for 3", () => {
+      const presenter = new ShadowrunAnarchyPresenter(defaults)
+
+      const result = presenter.glitchString(3)
+
+      expect(result).toMatch("critical")
+    })
+
+    it("returns critical glitch for above 3", () => {
+      const presenter = new ShadowrunAnarchyPresenter(defaults)
+
+      const result = presenter.glitchString(4)
+
+      expect(result).toMatch("critical")
+    })
+  })
+})
