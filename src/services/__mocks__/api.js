8a86fae7f1253444347a3ee98851867da26cc490
Paige Andrews <paige.andrews@outschool.com>
2026-01-29 14:42:56 -0500

    Convert api mocks

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -1,11 +1,15 @@
-module.exports = {
-  sendMessage: jest.fn(async (channelId, payload) => {
-    return {
-      channel_id: channelId,
-      payload: payload,
-      detail: `sent message to channel ${channelId}`,
-    }
-  }),
-  getGuildCommands: jest.fn(async (_guildId) => []),
-  setGuildCommands: jest.fn(async (_guildId, _commandNames) => true),
-}
+/**
+ * API overrides for testing
+ */
+
+export const sendMessage = jest.fn(async (channelId, payload) => {
+  return {
+    channel_id: channelId,
+    payload: payload,
+    detail: `sent message to channel ${channelId}`,
+  }
+})
+
+export const getGuildCommands = jest.fn(async (_guildId) => [])
+
+export const setGuildCommands = jest.fn(async (_guildId, _commandNames) => true)
