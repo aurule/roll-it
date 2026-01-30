f49e419bdef502359d6f4d7bf0125ea7b20ec16b
Paige Andrews <paige.andrews@outschool.com>
2026-01-29 21:04:42 -0500

    Allow jest to work with esm

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-module.exports = {
+export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -175,10 +175,10 @@ module.exports = {
   fakeTimers: {
     enableGlobally: true,
     advanceTimers: true,
-  }
+  },
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
