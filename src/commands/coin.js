79eae3f84d6ac77edc24b549937b1eab19725215
Paige Andrews <pmandrews@gmail.com>
2026-01-30 19:21:38 -0500

    Initial conversion of coin command

--- src/commands/coin.js
+++ src/commands/coin.js
@@ -1,63 +1,92 @@
 import Joi from "joi"
 
-import { LocalizedSlashCommandBuilder } from "../util/localized-command.js"
-import { roll } from "../services/base-roller.js"
-import { present } from "../presenters/results/coin-results-presenter.js"
+import * as sacrifice from "../services/easter-eggs/sacrifice.js"
+import { Command } from "./abstract/command.js"
 import { descriptionOption, secretOption} from "../util/common-options.js"
 import { descriptionSchema } from "../util/common-schemas.js"
-import { injectMention } from "../util/formatters/inject-user.js.js"
-import * as sacrifice from "../services/easter-eggs/sacrifice.js"
+import { present } from "../presenters/results/coin-results-presenter.js"
+import { roll } from "../services/base-roller.js"
 
-const command_name = "coin"
-
-module.exports = {
-  name: command_name,
-  data: () =>
-    new LocalizedSlashCommandBuilder(command_name)
-      .addStringOption(descriptionOption)
-      .addLocalizedStringOption("call", (option) => option.setLocalizedChoices("1", "2"))
-      .addBooleanOption(secretOption),
-  schema: Joi.object({
+/**
+ * Class for the /coin command
+ */
+export class Coin extends Command {
+  static name = "coin"
+  static schema = Joi.object({
     description: descriptionSchema,
     call: Joi.string().optional().valid("1", "2").messages({
       "any.only": 'Call must be either "1" or "2".',
     }),
-  }),
-  judge(raw_results, call, locale) {
-    if (call === "") return ""
+  })
 
-    const result = raw_results[0][0]
-    if (call == result) return sacrifice.good(locale)
-    return sacrifice.bad(locale)
-  },
-  perform({ description, call, locale = "en-US" }) {
-    const raw_results = roll(1, 2, 1)
+  /**
+   * The coin face called by the user
+   *
+   * One of "heads" or "tails", translated appropriately.
+   *
+   * @type string
+   */
+  call
+
+  /**
+   * Description for the coin toss
+   * @type string
+   */
+  description
+
+  /**
+   * Raw dice results
+   * @type number[][]
+   */
+  raw_results
+
+  static data() {
+    return this.builder
+      .addStringOption(descriptionOption)
+      .addLocalizedStringOption("call", (option) => option.setLocalizedChoices("1", "2"))
+      .addBooleanOption(secretOption),
+  }
+
+  /**
+   * Create a new Coin object
+   * @param  {Interaction} interaction Discord interaction object
+   * @return {Command}                 New Command object
+   */
+  constructor(interaction) {
+    super(interaction)
+
+    this.description = this.options.getString("description") ?? ""
+    this.call = this.options.getString("call") ?? ""
+    this.secret = this.options.getBoolean("secret") ?? false
+  }
+
+  perform() {
+    this.raw_results = roll(1, 2, 1)
 
     const presented_result = present({
-      call,
-      description,
-      raw: raw_results,
-      locale,
+      call: this.call,
+      description: this.description,
+      raw: this.raw_results,
+      locale: this.locale,
     })
 
-    if (sacrifice.hasTrigger(description, locale)) {
-      const sacrifice_message = module.exports.judge(raw_results, call, locale)
+    if (sacrifice.hasTrigger(this.description, this.locale)) {
+      const sacrifice_message = this.judge()
       return `${presented_result}\n-# ${sacrifice_message}`
     }
 
     return presented_result
-  },
-  async execute(interaction) {
-    const roll_description = interaction.options.getString("description") ?? ""
-    const call = interaction.options.getString("call") ?? ""
-    const secret = interaction.options.getBoolean("secret") ?? false
+  }
 
-    const partial_message = module.exports.perform({
-      call,
-      description: roll_description,
-      locale: interaction.locale,
-    })
+  /**
+   * Pass judgement on the coin toss, as per `sacrifice` easter egg
+   * @return {string} Empty for no judgement, otherwise a sacrifice message
+   */
+  judge() {
+    if (this.call === "") return ""
 
-    return interaction.rollReply(injectMention(partial_message, interaction.user.id), secret)
-  },
+    const result = this.raw_results[0][0]
+    if (call == result) return sacrifice.good(locale)
+    return sacrifice.bad(locale)
+  }
 }
