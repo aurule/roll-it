bcb428d4e4acc2a4d286c597035fc59fd066f215
Paige Andrews <pmandrews@gmail.com>
2026-02-01 15:47:12 -0500

    Add class for savable commands

---
+++ src/commands/abstract/savable-command.js
@@ -0,0 +1,46 @@
+import { Command } from "./command.js";
+
+/**
+ * Class for commands which can be saved
+ *
+ * This covers the majority of dice rolling commands.
+ */
+export class SavableCommand extends Command {
+  /**
+   * Whether this command can be saved
+   *
+   * Always true for savable commands.
+   *
+   * @type {boolean}
+   */
+  static savable = true
+
+  /**
+   * Array of option names which can be altered after saving
+   *
+   * Each _must_ be a numeric option. Used by `/saved grow` and `/saved roll`.
+   * The first option name will be changed by default when the user fails to
+   * pick a specific option.
+   *
+   * @type {string[]}
+   */
+  static changeable = []
+
+  /**
+   * Create a new SavableCommand
+   *
+   * Unlike simple commands, savable commands can be invoked with a set of saved
+   * options.
+   *
+   * @param  {Interaction}     interaction Discord interaction object
+   * @param  {CommandOptions?} options     Object of option overrides
+   * @return {SavableCommand}              New SavableCommand object
+   */
+  constructor(interaction, options) {
+    super(interaction)
+
+    if (options) {
+      this.options = options
+    }
+  }
+}
