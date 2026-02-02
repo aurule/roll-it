c561607112f87a675ddaa5290ba1a035871dc582
Paige Andrews <paige.andrews@outschool.com>
2026-02-02 16:57:52 -0500

    Convert shadowrun command

--- src/commands/shadowrun.js
+++ src/commands/shadowrun.js
@@ -1,23 +1,30 @@
 import Joi from "joi"
 
-import { LocalizedSlashCommandBuilder } from "../util/localized-command.js"
 import { rollUntil } from "../services/until-roller.js"
 import { rollExplode } from "../services/base-roller.js"
 import { successes } from "../services/tally.js"
 import { ShadowrunPresenter, present } from "../presenters/results/shadowrun-results-presenter.js"
-import { teamworkBegin } from "../interactive/teamwork.js"
 import { descriptionOption, rollsOption, teamworkOption, secretOption } from "../util/common-options.js"
 import { poolSchema, rollsSchema, untilSchema, descriptionSchema } from "../util/common-schemas.js"
-import { injectMention } from "../util/formatters/inject-user.js.js"
-import { i18n } from "../locales/index.js"
 import * as sacrifice from "../services/easter-eggs/sacrifice.js"
+import { TeamworkableCommand } from "./abstract/teamworkable-command.js"
 
-const command_name = "shadowrun"
+/**
+ * Class for the shadowrun command
+ */
+export class Shadowrun extends TeamworkableCommand {
+  static name = "shadowrun"
+  static changeable = ["pool"]
 
-module.exports = {
-  name: command_name,
-  data: () =>
-    new LocalizedSlashCommandBuilder(command_name)
+  pool = 1
+  description = ""
+  edge = false
+  explode = 7
+  rolls = 1
+  until = 0
+
+  static data() {
+    return this.builder
       .addLocalizedIntegerOption("pool", (option) =>
         option.setMinValue(1).setMaxValue(1000).setRequired(true),
       )
@@ -26,31 +33,29 @@ module.exports = {
       .addIntegerOption(rollsOption)
       .addBooleanOption(teamworkOption)
       .addLocalizedIntegerOption("until", (option) => option.setMinValue(1))
-      .addBooleanOption(secretOption),
-  savable: true,
-  changeable: ["pool"],
-  schema: Joi.object({
+      .addBooleanOption(secretOption)
+  }
+
+  static schema = Joi.object({
     pool: poolSchema,
     edge: Joi.boolean().optional(),
     rolls: rollsSchema,
     until: untilSchema,
     description: descriptionSchema,
-  }),
-  teamwork: {
-    roller: (final_pool, { explode }) => rollExplode(final_pool, 6, explode, 1),
-    summer: (raw_results) => successes(raw_results, 5),
-    presenter: (final_pool, raw_results, summed_results, locale, { edge, description }) =>
-      present({
-        rolls: 1,
-        pool: final_pool,
-        edge,
-        until: 0,
-        description,
-        raw: raw_results,
-        summed: summed_results,
-        locale,
-      }),
-  },
+  })
+
+  constructor(interaction, options) {
+    super(interaction, options)
+
+    this.saveOption("pool")
+    this.saveOption("description")
+    this.saveOption("edge")
+    this.saveOption("rolls")
+    this.saveOption("until")
+
+    if (this.edge) this.explode = 6
+  }
+
   judge(presenter) {
     const buckets = [0, 0, 0, 0, 0]
     const expected = Math.round(presenter.pool / 3)
@@ -80,101 +85,81 @@ module.exports = {
     const dominating = buckets.findIndex((b) => b >= presenter.summed.length / 2)
     switch (dominating) {
       case 0:
-        return sacrifice.great(presenter.locale)
+        return sacrifice.great(this.locale)
       case 1:
-        return sacrifice.good(presenter.locale)
+        return sacrifice.good(this.locale)
       case 2:
       default:
-        return sacrifice.neutral(presenter.locale)
+        return sacrifice.neutral(this.locale)
       case 3:
-        return sacrifice.bad(presenter.locale)
+        return sacrifice.bad(this.locale)
       case 4:
-        return sacrifice.awful(presenter.locale)
+        return sacrifice.awful(this.locale)
     }
-  },
-  perform({ pool, edge, rolls = 1, until, description, locale = "en-US" } = {}) {
-    let raw_results
-    let summed_results
+  }
 
-    const explode = edge ? 6 : 7
-
-    if (until) {
-      ;({ raw_results, summed_results } = rollUntil({
-        roll: () => rollExplode(pool, 6, explode),
-        tally: (currentResult) => successes(currentResult, 5),
-        max: rolls === 1 ? 0 : rolls,
-        target: until,
-      }))
-    } else {
-      raw_results = rollExplode(pool, 6, explode, rolls)
-      summed_results = successes(raw_results, 5)
-    }
-
+  performTeamwork(final_pool) {
+    const raw_results = rollExplode(final_pool, 6, this.explode, 1)
+    const summed_results = successes(raw_results, 5)
     const presenter = new ShadowrunPresenter({
-      rolls,
-      pool,
-      edge,
-      until,
-      description,
+      rolls: 1,
+      pool: final_pool,
+      edge: this.edge,
+      until: 0,
+      description: this.description,
       raw: raw_results,
       summed: summed_results,
-      locale,
+      locale: this.locale,
     })
     const result_lines = [presenter.presentResults()]
 
-    if (sacrifice.hasTrigger(description, locale)) {
-      const sacrifice_message = module.exports.judge(presenter)
+    if (sacrifice.hasTrigger(this.description, this.locale)) {
+      const sacrifice_message = this.judge(presenter)
       result_lines.push(`-# ${sacrifice_message}`)
     }
 
     return result_lines.join("\n")
-  },
-  async execute(interaction) {
-    const pool = interaction.options.getInteger("pool")
-    const edge = interaction.options.getBoolean("edge") ?? false
-    const rolls = interaction.options.getInteger("rolls") ?? 1
-    const until = interaction.options.getInteger("until") ?? 0
-    const description = interaction.options.getString("description") ?? ""
-    const secret = interaction.options.getBoolean("secret") ?? false
-    const is_teamwork = interaction.options.getBoolean("teamwork") ?? false
+  }
 
-    const t = i18n.getFixedT(interaction.locale, "commands", "shadowrun")
+  perform() {
+    let raw_results
+    let summed_results
 
-    const userFlake = interaction.user.id
-
-    if (is_teamwork) {
-      if (rolls > 1 || until > 0 || secret) {
-        return interaction.whisper(t("options.teamwork.validation.conflict"))
-      }
-
-      const explode = edge ? 6 : 7
-      const teamwork_options = {
-        roller: { explode },
-        summer: {},
-        presenter: { edge, description },
-      }
-
-      return teamworkBegin({
-        interaction,
-        description,
-        command: command_name,
-        options: teamwork_options,
-        pool,
-      })
+    if (this.until) {
+      ;({ raw_results, summed_results } = rollUntil({
+        roll: () => rollExplode(this.pool, 6, this.explode),
+        tally: (currentResult) => successes(currentResult, 5),
+        max: this.rolls === 1 ? 0 : this.rolls,
+        target: this.until,
+      }))
+    } else {
+      raw_results = rollExplode(this.pool, 6, this.explode, this.rolls)
+      summed_results = successes(raw_results, 5)
     }
 
-    const partial_message = module.exports.perform({
-      rolls,
-      pool,
-      edge,
-      until,
-      description,
-      locale: interaction.locale,
+    const presenter = new ShadowrunPresenter({
+      rolls: this.rolls,
+      pool: this.pool,
+      edge: this.edge,
+      until: this.until,
+      description: this.description,
+      raw: raw_results,
+      summed: summed_results,
+      locale: this.locale,
     })
-    const full_text = injectMention(partial_message, userFlake)
-    return interaction.paginate({
-      content: full_text,
-      secret,
-    })
-  },
+    const result_lines = [presenter.presentResults()]
+
+    if (sacrifice.hasTrigger(this.description, this.locale)) {
+      const sacrifice_message = this.judge(presenter)
+      result_lines.push(`-# ${sacrifice_message}`)
+    }
+
+    return result_lines.join("\n")
+  }
+
+  validate() {
+    if (this.teamwork && (this.rolls > 1 || this.until > 0 || this.secret)) {
+      return this.t("options.teamwork.validation.conflict")
+    }
+  }
 }
