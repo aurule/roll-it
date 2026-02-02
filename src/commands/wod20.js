ee3977bb2608664cb59fb1fb0a05a58fbb9e0095
Paige Andrews <paige.andrews@outschool.com>
2026-02-02 17:04:33 -0500

    Convert wod20 command

--- src/commands/wod20.js
+++ src/commands/wod20.js
@@ -1,24 +1,31 @@
 import Joi from "joi"
 
-import { LocalizedSlashCommandBuilder } from "../util/localized-command.js"
 import { rollUntil } from "../services/until-roller.js"
 import { roll } from "../services/base-roller.js"
 import { wod20 } from "../services/tally.js"
 import { present } from "../presenters/results/wod20-results-presenter.js"
-import { teamworkBegin } from "../interactive/teamwork.js"
 import { descriptionOption, rollsOption, teamworkOption, secretOption } from "../util/common-options.js"
 import { poolSchema, rollsSchema, untilSchema, descriptionSchema } from "../util/common-schemas.js"
-import { injectMention } from "../util/formatters/inject-user.js.js"
-import { i18n } from "../locales/index.js"
 import * as hummingbird from "../services/easter-eggs/hummingbird.js"
 import * as sacrifice from "../services/easter-eggs/sacrifice.js"
+import { TeamworkableCommand } from "./abstract/teamworkable-command.js"
 
-const command_name = "wod20"
+/**
+ * Class for the wod20 command
+ */
+export class Wod20 extends TeamworkableCommand {
+  static name = "wod20"
+  static changeable = ["pool", "difficulty"]
 
-module.exports = {
-  name: command_name,
-  data: () =>
-    new LocalizedSlashCommandBuilder(command_name)
+  pool = 1
+  description = ""
+  difficulty = 6
+  specialty = false
+  rolls = 1
+  until = 0
+
+  static data() {
+    return this.builder
       .addLocalizedIntegerOption("pool", (option) =>
         option.setMinValue(1).setMaxValue(1000).setRequired(true),
       )
@@ -28,20 +35,32 @@ module.exports = {
       .addIntegerOption(rollsOption)
       .addBooleanOption(teamworkOption)
       .addLocalizedIntegerOption("until", (option) => option.setMinValue(1))
-      .addBooleanOption(secretOption),
-  savable: true,
-  changeable: ["pool", "difficulty"],
-  schema: Joi.object({
+      .addBooleanOption(secretOption)
+  }
+
+  static schema = Joi.object({
     pool: poolSchema,
     difficulty: Joi.number().optional().integer().min(2).max(10),
     specialty: Joi.boolean().optional(),
     rolls: rollsSchema,
     until: untilSchema,
     description: descriptionSchema,
-  }),
-  judge(results, pool, difficulty, locale) {
-    const factor = 10 / (11 - difficulty)
-    const expected = Math.round(pool / factor)
+  })
+
+  constructor(interaction, options) {
+    super(interaction, options)
+
+    this.saveOption("pool")
+    this.saveOption("description")
+    this.saveOption("difficulty")
+    this.saveOption("specialty")
+    this.saveOption("rolls")
+    this.saveOption("until")
+  }
+
+  judge(results) {
+    const factor = 10 / (11 - this.difficulty)
+    const expected = Math.round(this.pool / factor)
 
     const buckets = [0, 0, 0, 0, 0]
     for (const result of results) {
@@ -68,139 +87,92 @@ module.exports = {
     const dominating = buckets.findIndex((b) => b >= results.length / 2)
     switch (dominating) {
       case 0:
-        return sacrifice.great(locale)
+        return sacrifice.great(this.locale)
       case 1:
-        return sacrifice.good(locale)
+        return sacrifice.good(this.locale)
       case 2:
       default:
-        return sacrifice.neutral(locale)
+        return sacrifice.neutral(this.locale)
       case 3:
-        return sacrifice.bad(locale)
+        return sacrifice.bad(this.locale)
       case 4:
-        return sacrifice.awful(locale)
+        return sacrifice.awful(this.locale)
     }
-  },
-  teamwork: {
-    roller: (final_pool) => roll(final_pool, 10, 1),
-    summer: (raw_results, { difficulty, specialty }) => wod20(raw_results, difficulty, specialty),
-    presenter: (
-      final_pool,
-      raw_results,
-      summed_results,
-      locale,
-      { difficulty, specialty, description },
-    ) =>
+  }
+
+  performTeamwork(final_pool) {
+    const raw_results = roll(final_pool, 10, 1)
+    const summed_results = wod20(raw_results, this.difficulty, this.specialty)
+    const result_lines = [
       present({
         rolls: 1,
         pool: final_pool,
-        difficulty,
-        specialty,
+        difficulty: this.difficulty,
+        specialty: this.specialty,
         until: 0,
-        description: roll_description,
+        description: this.description,
         raw: raw_results,
         summed: summed_results,
-        locale,
+        locale: this.locale,
       }),
-  },
-  perform({
-    pool,
-    difficulty = 7,
-    specialty,
-    rolls = 1,
-    until,
-    description,
-    locale = "en-US",
-  } = {}) {
+    ]
+
+    if (sacrifice.hasTrigger(this.description, this.locale)) {
+      const sacrifice_message = this.judge(summed_results)
+      result_lines.push(`-# ${sacrifice_message}`)
+    }
+
+    return result_lines.join("\n")
+  }
+
+  perform() {
     let raw_results
     let summed_results
 
-    if (until) {
+    if (this.until) {
       ;({ raw_results, summed_results } = rollUntil({
-        roll: () => roll(pool, 10),
-        tally: (currentResult) => wod20(currentResult, difficulty, specialty),
-        max: rolls === 1 ? 0 : rolls,
-        target: until,
+        roll: () => roll(this.pool, 10),
+        tally: (currentResult) => wod20(currentResult, this.difficulty, this.specialty),
+        max: this.rolls === 1 ? 0 : this.rolls,
+        target: this.until,
       }))
     } else {
-      raw_results = roll(pool, 10, rolls)
-      summed_results = wod20(raw_results, difficulty, specialty)
+      raw_results = roll(this.pool, 10, this.rolls)
+      summed_results = wod20(raw_results, this.difficulty, this.specialty)
     }
 
     const result_lines = [
       present({
-        rolls,
-        pool,
-        difficulty,
-        specialty,
-        until,
-        description,
+        rolls: this.rolls,
+        pool: this.pool,
+        difficulty: this.difficulty,
+        specialty: this.specialty,
+        until: this.until,
+        description: this.description,
         raw: raw_results,
         summed: summed_results,
-        locale,
+        locale: this.locale,
       }),
     ]
 
-    if (sacrifice.hasTrigger(description, locale)) {
-      const sacrifice_message = module.exports.judge(summed_results, pool, difficulty, locale)
+    if (sacrifice.hasTrigger(this.description, this.locale)) {
+      const sacrifice_message = this.judge(summed_results)
       result_lines.push(`-# ${sacrifice_message}`)
     }
 
-    if (hummingbird.hasTrigger(description, locale)) {
+    if (hummingbird.hasTrigger(this.description, this.locale)) {
       if (summed_results.some(hummingbird.qualified)) {
-        const hummingbird_message = hummingbird.spotted(locale)
+        const hummingbird_message = hummingbird.spotted(this.locale)
         result_lines.push(`-# ${hummingbird_message}`)
       }
     }
 
     return result_lines.join("\n")
-  },
-  async execute(interaction) {
-    const pool = interaction.options.getInteger("pool")
-    const difficulty = interaction.options.getInteger("difficulty") ?? 6
-    const specialty = interaction.options.getBoolean("specialty") ?? false
-    const rolls = interaction.options.getInteger("rolls") ?? 1
-    const until = interaction.options.getInteger("until") ?? 0
-    const description = interaction.options.getString("description") ?? ""
-    const secret = interaction.options.getBoolean("secret") ?? false
-    const is_teamwork = interaction.options.getBoolean("teamwork") ?? false
+  }
 
-    const t = i18n.getFixedT(interaction.locale, "commands", "wod20")
-
-    const userFlake = interaction.user.id
-
-    if (is_teamwork) {
-      if (rolls > 1 || until > 0 || secret) {
-        return interaction.whisper(t("options.teamwork.validation.conflict"))
-      }
-
-      const teamwork_options = {
-        roller: {},
-        summer: { difficulty, specialty },
-        presenter: { difficulty, specialty, description },
-      }
-
-      return teamworkBegin({
-        interaction,
-        description,
-        command: command_name,
-        options: teamwork_options,
-        pool,
-      })
+  validate() {
+    if (this.teamwork && (this.rolls > 1 || this.until > 0 || this.secret)) {
+      return this.t("options.teamwork.validation.conflict")
     }
-
-    const partial_message = module.exports.perform({
-      rolls,
-      pool,
-      difficulty,
-      specialty,
-      until,
-      description,
-      locale: interaction.locale,
-    })
-    const full_text = injectMention(partial_message, userFlake)
-    return interaction.paginate({
-      content: full_text,
-      secret,
-    })
-  },
+  }
 }
