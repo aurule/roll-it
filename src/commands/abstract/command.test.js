6b2a8d99539925106ce9f9c898a51e2c8b3e89eb
Paige Andrews <paige.andrews@outschool.com>
2026-02-06 16:26:55 -0500

    Test more command bits

--- src/commands/abstract/command.test.js
+++ src/commands/abstract/command.test.js
@@ -1,8 +1,14 @@
+import { MessageFlags } from "discord.js"
+import { Interaction } from "../../../testing/interaction.js"
 import { LocalizedSlashCommandBuilder } from "../../util/localized-command.js"
 import { Command } from "./command.js"
 
 class TestCommand extends Command {
   static name = "roll"
+
+  perform() {
+    return "test"
+  }
 }
 
 describe("Command base class", () => {
@@ -23,6 +29,54 @@ describe("Command base class", () => {
   describe("builder", () => {
     it("is a localized slash command builder", () => {
       expect(TestCommand.builder).toBeInstanceOf(LocalizedSlashCommandBuilder)
+    })
+  })
+
+  describe("options", () => {
+    let interaction
+
+    beforeEach(() => {
+      interaction = new Interaction()
+    })
+
+    describe("secret", () => {
+      it("defaults to false", () => {
+        const cmd = new TestCommand(interaction)
+
+        expect(cmd.secret).toBe(false)
+      })
+
+      it("accepts true", () => {
+        interaction.command_options = {
+          secret: true
+        }
+
+        const cmd = new TestCommand(interaction)
+
+        expect(cmd.secret).toBe(true)
+      })
+
+      it("false makes response non-ephemeral", async () => {
+        interaction.command_options = {
+          secret: false
+        }
+        const cmd = new TestCommand(interaction)
+
+        const result = await cmd.execute()
+
+        expect(result.message.flags).not.toHaveFlag(MessageFlags.Ephemeral)
+      })
+
+      it("true makes response ephemeral", async () => {
+        interaction.command_options = {
+          secret: true
+        }
+        const cmd = new TestCommand(interaction)
+
+        const result = await cmd.execute()
+
+        expect(result.message.flags).toHaveFlag(MessageFlags.Ephemeral)
+      })
     })
   })
 })
