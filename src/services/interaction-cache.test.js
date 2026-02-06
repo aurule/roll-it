ec2f2e5531db5d18ab887559daa6d3881db17648
Paige Andrews <paige.andrews@outschool.com>
2026-02-06 16:32:19 -0500

    Fix interaction cache test

--- src/services/interaction-cache.test.js
+++ src/services/interaction-cache.test.js
@@ -1,6 +1,6 @@
 import { Interaction } from "../../testing/interaction.js"
 
-const interactionCache = require("./interaction-cache")
+import interactionCache from "./interaction-cache.js"
 
 describe("interaction cache", () => {
   describe("set", () => {
@@ -17,23 +17,14 @@ describe("interaction cache", () => {
     it("stores the command options", async () => {
       const interaction = new Interaction()
       interaction.commandName = "d10"
-      interaction.options.data = [{ name: "modifier", value: 3 }]
+      interaction.command_options = {
+        modifier: 3
+      }
 
       await interactionCache.set(interaction)
 
       const cached = await interactionCache.getInteraction(interaction)
       expect(cached.options.modifier).toEqual(3)
-    })
-
-    it("does not choke on an interaction with no options", async () => {
-      const interaction = new Interaction()
-      interaction.commandName = "d10"
-      delete interaction.options.data
-
-      await interactionCache.set(interaction)
-
-      const cached = await interactionCache.getInteraction(interaction)
-      expect(cached).toBeUndefined()
     })
   })
 })
