7df37e38b74ac0d431aceae4a9925ddd2d73e040
Paige Andrews <paige.andrews@outschool.com>
2026-02-06 16:36:32 -0500

    Fix curv tests

--- src/commands/curv.test.js
+++ src/commands/curv.test.js
@@ -1,6 +1,6 @@
 vitest.mock("../util/message-builders")
 
-const curv_command = require("./curv")
+import { Curv } from "./curv.js"
 
 import { Interaction } from "../../testing/interaction.js"
 
@@ -11,134 +11,134 @@ describe("/curv command", () => {
     interaction = new Interaction()
   })
 
-  describe("/curv command", () => {
-    describe("schema", () => {
-      describe("keep", () => {
-        const keep_schema = curv_command.schema.extract("keep")
+  describe("schema", () => {
+    describe("keep", () => {
+      const keep_schema = Curv.schema.extract("keep")
 
-        it("is optional", () => {
-          const result = keep_schema.validate()
-
-          expect(result.error).toBeFalsy()
-        })
-
-        it("rejects unknown values", () => {
-          const result = keep_schema.validate("nothing")
-
-          expect(result.error).toBeTruthy()
-        })
-
-        it.concurrent.each([["all"], ["highest"], ["lowest"]])("accepts '%s'", async (value) => {
-          const result = keep_schema.validate(value)
-
-          expect(result.error).toBeFalsy()
-        })
-      })
-
-      describe("with", () => {
-        const with_schema = curv_command.schema.extract("with")
-
-        it("is optional", () => {
-          const result = with_schema.validate()
-
-          expect(result.error).toBeFalsy()
-        })
-
-        it("rejects unknown values", () => {
-          const result = with_schema.validate("nothing")
-
-          expect(result.error).toBeTruthy()
-        })
-
-        it.concurrent.each([["advantage"], ["disadvantage"]])("accepts '%s'", async (value) => {
-          const result = with_schema.validate(value)
-
-          expect(result.error).toBeFalsy()
-        })
-      })
-
-      it("does not allow 'keep' and 'with'", () => {
-        const options = {
-          keep: "all",
-          with: "advantage",
-        }
-
-        const result = curv_command.schema.validate(options, { abortEarly: false })
-
-        expect(result.error.message).toMatch("exclusive peers")
-      })
-
-      it("allows keep alone", () => {
-        const options = {
-          keep: "all",
-        }
-
-        const result = curv_command.schema.validate(options, { abortEarly: false })
+      it("is optional", () => {
+        const result = keep_schema.validate()
 
         expect(result.error).toBeFalsy()
       })
 
-      it("allows with alone", () => {
-        const options = {
-          with: "advantage",
-        }
+      it("rejects unknown values", () => {
+        const result = keep_schema.validate("nothing")
 
-        const result = curv_command.schema.validate(options, { abortEarly: false })
+        expect(result.error).toBeTruthy()
+      })
+
+      it.concurrent.each([["all"], ["highest"], ["lowest"]])("accepts '%s'", async (value) => {
+        const result = keep_schema.validate(value)
 
         expect(result.error).toBeFalsy()
       })
     })
 
-    describe("perform", () => {
-      it("displays the description if present", () => {
-        const options = {
-          description: "this is a test",
-          rolls: 1,
-        }
+    describe("with", () => {
+      const with_schema = Curv.schema.extract("with")
 
-        const result = curv_command.perform(options)
+      it("is optional", () => {
+        const result = with_schema.validate()
 
-        expect(result).toMatch("this is a test")
+        expect(result.error).toBeFalsy()
       })
 
-      it("overrides `keep` using `with`", () => {
-        const options = {
-          rolls: 1,
-          keep: "all",
-          with: "advantage",
-        }
+      it("rejects unknown values", () => {
+        const result = with_schema.validate("nothing")
 
-        const result = curv_command.perform(options)
-
-        expect(result).toMatch("advantage")
+        expect(result.error).toBeTruthy()
       })
 
-      it("allows disadvantage", () => {
-        const options = {
-          rolls: 1,
-          with: "disadvantage",
-        }
+      it.concurrent.each([["advantage"], ["disadvantage"]])("accepts '%s'", async (value) => {
+        const result = with_schema.validate(value)
 
-        const result = curv_command.perform(options)
-
-        expect(result).toMatch("disadvantage")
+        expect(result.error).toBeFalsy()
       })
     })
 
-    describe("execute", () => {
-      describe("with multiple rolls", () => {
-        beforeEach(() => {
-          interaction.command_options.rolls = 2
-        })
+    it("does not allow 'keep' and 'with'", () => {
+      const options = {
+        keep: "all",
+        with: "advantage",
+      }
 
-        it("displays the description if present", () => {
-          const description_text = "this is a test"
-          interaction.command_options.description = description_text
+      const result = Curv.schema.validate(options, { abortEarly: false })
 
-          curv_command.execute(interaction)
+      expect(result.error.message).toMatch("exclusive peers")
+    })
 
-          expect(interaction.replyContent).toMatch(description_text)
-        })
+    it("allows keep alone", () => {
+      const options = {
+        keep: "all",
+      }
+
+      const result = Curv.schema.validate(options, { abortEarly: false })
+
+      expect(result.error).toBeFalsy()
+    })
+
+    it("allows with alone", () => {
+      const options = {
+        with: "advantage",
+      }
+
+      const result = Curv.schema.validate(options, { abortEarly: false })
+
+      expect(result.error).toBeFalsy()
+    })
+  })
+
+  describe("perform", () => {
+    it("displays the description if present", () => {
+      interaction.command_options = {
+        description: "this is a test",
+        rolls: 1,
+      }
+      const curv_command = new Curv(interaction)
+
+      const result = curv_command.perform()
+
+      expect(result).toMatch("this is a test")
+    })
+
+    it("overrides `keep` using `with`", () => {
+      interaction.command_options = {
+        rolls: 1,
+        keep: "all",
+        with: "advantage",
+      }
+      const curv_command = new Curv(interaction)
+
+      const result = curv_command.perform()
+
+      expect(result).toMatch("advantage")
+    })
+
+    it("allows disadvantage", () => {
+      interaction.command_options = {
+        rolls: 1,
+        with: "disadvantage",
+      }
+      const curv_command = new Curv(interaction)
+
+      const result = curv_command.perform()
+
+      expect(result).toMatch("disadvantage")
+    })
+
+    describe("with multiple rolls", () => {
+      beforeEach(() => {
+        interaction.command_options.rolls = 2
+      })
+
+      it("displays the description if present", async () => {
+        const description_text = "this is a test"
+        interaction.command_options.description = description_text
+        const curv_command = new Curv(interaction)
+
+        await curv_command.execute()
+
+        expect(interaction.replyContent).toMatch(description_text)
       })
     })
   })
