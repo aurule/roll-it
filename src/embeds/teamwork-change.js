d7cd58f135f6f1967f877d4d44d8fa12ac67fd46
Paige Andrews <paige.andrews@outschool.com>
2026-02-19 16:18:47 -0500

    Add all change fields at once

--- src/embeds/teamwork-change.js
+++ src/embeds/teamwork-change.js
@@ -31,8 +31,7 @@ export class TeamworkChangeEmbed {
     this.builder.setColor(0x03b199)
     this.builder.setTitle(this.t("title"))
     this.builder.setDescription(this.description)
-    this.builder.addFields(this.helper_names)
-    this.builder.addFields(this.helper_bonuses)
+    this.builder.addFields(this.helper_names, this.helper_bonuses)
   }
 
   /**
