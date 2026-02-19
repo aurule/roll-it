14e8ad6c58c3352fc9562c28430c2a503074102a
Paige Andrews <paige.andrews@outschool.com>
2026-02-19 16:35:40 -0500

    Fix command completers tests

--- src/completers/command-completers.test.js
+++ src/completers/command-completers.test.js
@@ -1,6 +1,23 @@
 import { all } from "./command-completers.js"
 
 describe("command name completers", () => {
+  beforeAll(() => {
+    vitest.mock(import("../commands/index.js"), async (importOriginal) => {
+      const all_choices = new Array(30).fill({ name: "test", value: "test" })
+      all_choices.push({ name: "wod20", value: "wod20" }, { name: "chop", value: "chop" })
+
+      const actual = await importOriginal()
+      return {
+        ...actual,
+        all_choices
+      }
+    })
+  })
+
+  afterAll(() => {
+    vitest.restoreAllMocks()
+  })
+
   describe("all", () => {
     it("searches command names by lowercase", () => {
       const result = all("WOD")
