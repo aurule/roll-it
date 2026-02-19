f6573481c30e4c5783df1f80588d0c11ddb7800e
Paige Andrews <paige.andrews@outschool.com>
2026-02-19 16:42:15 -0500

    Simplify component dispatch testing

--- src/components/index.test.js
+++ src/components/index.test.js
@@ -1,38 +1,6 @@
-import { handle, handlers } from "./index.js"
+import { handlers } from "./index.js"
 
 describe("component dispatching", () => {
-  describe("handle", () => {
-    it("calls first handler that can accept the interaction", async () => {
-      const handler1 = {
-        canHandle: (_interaction) => false,
-        handle: (_interaction) => "one",
-      }
-      const handler2 = {
-        canHandle: (_interaction) => true,
-        handle: (_interaction) => "two",
-      }
-
-      const result = await handle({}, [handler1, handler2])
-
-      expect(result).toEqual("two")
-    })
-
-    it("returns false if no handler takes the interaction", async () => {
-      const handler1 = {
-        canHandle: (_interaction) => false,
-        handle: (_interaction) => "one",
-      }
-      const handler2 = {
-        canHandle: (_interaction) => false,
-        handle: (_interaction) => "two",
-      }
-
-      const result = await handle({}, [handler1, handler2])
-
-      expect(result).toEqual(false)
-    })
-  })
-
   describe("components", () => {
     it("each has a unique ID", () => {
       const all_handler_names = handlers.flatMap((h) => h.components.map((c) => c.name))
